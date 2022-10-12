b5d7afe659f1caf448f59d5a2842162f02ff6cf0
Luc Wollants <lucwollants@gmail.com>
2022-10-12 16:05:49 +0200

    Add Address Nova resource (hidden from navigation)

---
+++ app/Nova/Address.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova;
+
+use App\Domain\Organizations\Models\AddressModel;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+final class Address extends Resource
+{
+    public static string $model = AddressModel::class;
+
+    public static $title = 'street';
+
+    public static $displayInNavigation = false;
+
+    /**
+     * @var array<string>
+     */
+    public static $search = [
+        'street',
+        'zip',
+        'city',
+        'country',
+    ];
+
+    /**
+     * @return array<Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            Text::make('Id')
+                ->readonly(),
+
+            Text::make('Street')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Zip')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('City')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Country')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            BelongsTo::make('Organization'),
+        ];
+    }
+}
