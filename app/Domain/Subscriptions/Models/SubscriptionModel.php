f03a4149d51fd5f1137853579048cc361aa76f77
Luc Wollants <lucwollants@gmail.com>
2022-10-12 12:34:29 +0200

    Add casting for price and fee

--- app/Domain/Subscriptions/Models/SubscriptionModel.php
+++ app/Domain/Subscriptions/Models/SubscriptionModel.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Subscriptions\Models;
 
 use App\Models\UuidModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 final class SubscriptionModel extends UuidModel
@@ -22,4 +23,20 @@ final class SubscriptionModel extends UuidModel
         'billing_interval',
         'fee',
     ];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: static fn ($value) => (float) $value / 100,
+            set: static fn ($value) => (int) ($value * 100),
+        );
+    }
+
+    protected function fee(): Attribute
+    {
+        return Attribute::make(
+            get: static fn ($value) => (float) $value / 100,
+            set: static fn ($value) => (int) ($value * 100),
+        );
+    }
 }
