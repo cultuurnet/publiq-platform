fc8312f4d6b3144eb80ff910b94fdaa689da1fce
Luc Wollants <lucwollants@gmail.com>
2022-10-12 16:26:48 +0200

    Add Organization Nova resource

---
+++ app/Nova/Organization.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova;
+
+use App\Domain\Organizations\Models\OrganizationModel;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\HasOne;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+final class Organization extends Resource
+{
+    public static string $model = OrganizationModel::class;
+
+    public static $title = 'name';
+
+    /**
+     * @var array<string>
+     */
+    public static $search = [
+        'name',
+        'vat',
+        'address.street',
+        'address.zip',
+        'address.city',
+        'address.country',
+    ];
+
+    /**
+     * @return array<Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            Text::make('Id')
+                ->readonly(),
+
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Vat')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make(
+                'Address',
+                fn () => $this->address->street . ', ' . $this->address->zip . ' ' . $this->address->city)
+                ->onlyOnIndex(),
+
+            HasOne::make('Address'),
+        ];
+    }
+}
