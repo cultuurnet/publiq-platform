138f9d27f2464bd60df1af4c7cef74e8e55aeee6
Luc Wollants <lucwollants@gmail.com>
2022-10-13 09:37:48 +0200

    Add belongsTo on Integration

--- app/Domain/Integrations/Models/IntegrationModel.php
+++ app/Domain/Integrations/Models/IntegrationModel.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Domain\Integrations\Models;
 
 use App\Domain\Contacts\Models\ContactModel;
+use App\Domain\Subscriptions\Models\SubscriptionModel;
 use App\Models\UuidModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -29,5 +31,13 @@ final class IntegrationModel extends UuidModel
     public function contacts(): HasMany
     {
         return $this->hasMany(ContactModel::class, 'integration_id');
+    }
+
+    /**
+     * @return BelongsTo<SubscriptionModel, IntegrationModel>
+     */
+    public function subscription(): belongsTo
+    {
+        return $this->belongsTo(SubscriptionModel::class, 'subscription_id');
     }
 }
