3637e9ada9bdc6b33ae331d91152b182edea9f1e
Luc Wollants <lucwollants@gmail.com>
2022-10-24 19:44:11 +0200

    Improve typehinting

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -10,32 +10,21 @@ use Inertia\Middleware;
 final class HandleInertiaRequests extends Middleware
 {
     /**
-     * The root template that's loaded on the first page visit.
-     *
-     * @see https://inertiajs.com/server-side-setup#root-template
      * @var string
      */
     protected $rootView = 'layouts/default';
 
-    /**
-     * Determines the current asset version.
-     *
-     * @see https://inertiajs.com/asset-versioning
-     */
     public function version(Request $request): ?string
     {
         return parent::version($request);
     }
 
     /**
-     * Defines the props that are shared by default.
-     *
-     * @see https://inertiajs.com/shared-data
+     * @return array<string, mixed>
      */
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-
         ]);
     }
 }
