0189ce984bf8dd24f78b7956e0f174913358bdc0
Luc Wollants <lucwollants@gmail.com>
2022-10-24 16:34:41 +0200

    Add default blade layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ])
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>publiq-platform</title>
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+</html>
