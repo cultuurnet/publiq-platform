d2033a6a3067a24ad77dddf68921bd99a4b65036
Luc Wollants <lucwollants@gmail.com>
2022-10-25 10:06:27 +0200

    Add Controller for the Integration

---
+++ app/Domain/Integrations/Controllers/IntegrationController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Integrations\Controllers;
+
+use App\Domain\Integrations\IntegrationType;
+use App\Domain\Subscriptions\Repositories\SubscriptionRepository;
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+use Inertia\Response;
+
+final class IntegrationController extends Controller
+{
+    private SubscriptionRepository $subscriptionRepository;
+
+    public function __construct(SubscriptionRepository $subscriptionRepository)
+    {
+        $this->subscriptionRepository = $subscriptionRepository;
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('Integrations/Create', [
+            'integrationTypes' => IntegrationType::cases(),
+            'subscriptions' => $this->subscriptionRepository->all(),
+        ]);
+    }
+}
