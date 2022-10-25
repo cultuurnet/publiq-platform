f4c98d9441702c26a3fcbab2681a1178304344ba
Luc Wollants <lucwollants@gmail.com>
2022-10-25 08:52:26 +0200

    Use `resolvePageComponent` to load pages

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,11 +2,10 @@ import './bootstrap';
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/inertia-react'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-  resolve: async (name) => {
-    return (await import(/* @vite-ignore */`./Pages/${name}`)).default
-  },
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx")),
   setup({ el, App, props }) {
     const root = createRoot(el);
     root.render(<App {...props} />);
