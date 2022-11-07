667c06e3f2287c4dfa14e8485cd1b3386e82a687
Luc Wollants <lucwollants@gmail.com>
2022-11-07 13:13:06 +0100

    Update UserModel to also implement the Stateful User

--- app/Domain/Users/Models/UserModel.php
+++ app/Domain/Users/Models/UserModel.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace App\Domain\Users\Models;
 
 use App\Models\UuidModel;
+use Auth0\Laravel\Contract\Model\Stateful\User as StatefulUser;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-final class UserModel extends UuidModel implements AuthenticatableContract
+final class UserModel extends UuidModel implements AuthenticatableContract, StatefulUser
 {
     use Authenticatable;
 
     protected $table = 'users';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+    ];
 }
