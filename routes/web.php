161fd7b371feae2f5b2c01269f950ac660dddcb0
Luc Wollants <lucwollants@gmail.com>
2022-11-08 08:57:31 +0100

    Cleanup auth route binding

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Domain\Auth\Controllers\Login;
 use App\Domain\Integrations\Controllers\IntegrationController;
 use App\Domain\Subscriptions\Controllers\SubscriptionController;
+use Auth0\Laravel\Http\Controller\Stateful\Callback;
+use Auth0\Laravel\Http\Controller\Stateful\Logout;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -20,10 +23,10 @@ use Inertia\Inertia;
 
 Route::get('/', static fn () => Inertia::render('Index'));
 
-Route::get('/login', \Auth0\Laravel\Http\Controller\Stateful\Login::class)->name('login');
-Route::get('/admin/login', static fn() => redirect('/login'));
-Route::get('/logout', \Auth0\Laravel\Http\Controller\Stateful\Logout::class)->name('logout');
-Route::get('/auth/callback', \Auth0\Laravel\Http\Controller\Stateful\Callback::class)->name('auth0.callback');
+Route::get('/login', Login::class)->name('login');
+Route::get('/admin/login', static fn () => redirect('/login'));
+Route::get('/logout', Logout::class)->name('logout');
+Route::get('/auth/callback', Callback::class)->name('auth.callback');
 
 Route::get('/subscriptions', [SubscriptionController::class, 'index']);
 
