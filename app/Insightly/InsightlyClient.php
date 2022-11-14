a6f0a3c0e40f624648bc16194eda825b7dc1cbd1
Luc Wollants <lucwollants@gmail.com>
2022-11-14 11:52:33 +0100

    Add `InsightlyClient`

---
+++ app/Insightly/InsightlyClient.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Insightly;
+
+use App\Insightly\Exceptions\AuthenticationFailed;
+use App\Insightly\Exceptions\BadRequest;
+use App\Insightly\Exceptions\DeleteFailed;
+use App\Insightly\Exceptions\RecordLimitReached;
+use App\Insightly\Exceptions\RecordNotFound;
+use App\Insightly\Resources\ContactResource;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class InsightlyClient
+{
+    public function __construct(
+        private readonly ClientInterface $httpClient,
+        private readonly string $apiKey
+    ) {
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $requestWithHeaders = $request
+            ->withAddedHeader(
+                'Authorization',
+                'Basic ' . base64_encode($this->apiKey . ':')
+            )
+            ->withAddedHeader(
+                'Content-Type',
+                'application/json'
+            );
+
+        $response = $this->httpClient->sendRequest($requestWithHeaders);
+        $this->validateResponse($response);
+
+        return $response;
+    }
+
+    private function validateResponse(ResponseInterface $response): void
+    {
+        switch ($response->getStatusCode()) {
+            case 400:
+                throw new BadRequest($response->getReasonPhrase());
+            case 401:
+                throw new AuthenticationFailed($response->getReasonPhrase());
+            case 402:
+                throw new RecordLimitReached($response->getReasonPhrase());
+            case 404:
+                throw new RecordNotFound($response->getReasonPhrase());
+            case 417:
+                throw new DeleteFailed($response->getReasonPhrase());
+        }
+    }
+}
