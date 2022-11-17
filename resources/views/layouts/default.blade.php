0a3ee3bacf47891fb2f8c59ebd04956bbc321c15
simon-debruijn <simon.debruijn@hotmail.com>
2022-11-17 17:39:38 +0100

    Fix path entrypoint in blade file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite([
             'resources/css/app.css',
-            'resources/js/app.jsx',
+            'resources/ts/app.tsx',
         ])
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>publiq-platform</title>
