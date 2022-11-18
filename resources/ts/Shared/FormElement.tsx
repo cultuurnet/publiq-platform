23354a43aa20ef725d7d51393a6bbb20c569db02
simon-debruijn <simon.debruijn@hotmail.com>
2022-11-18 17:28:26 +0100

    Add FormElement component

---
+++ resources/ts/Shared/FormElement.tsx
@@ -0,0 +1,79 @@
+import React, { FC, useId } from 'react';
+import { classNames } from '../utils/classnames';
+
+type LabelPosition = 'top' | 'left' | 'right';
+
+type LabelSize = 'base' | 'lg' | 'xl';
+
+type WrapperProps = {
+  labelPosition?: LabelPosition;
+  children: JSX.Element;
+};
+
+const Wrapper = ({ labelPosition, children }: WrapperProps) => {
+  if (labelPosition !== 'right') {
+    return children;
+  }
+
+  return <div className="flex">{children}</div>;
+};
+
+const getFlexDirection = (labelPosition: LabelPosition | undefined) => {
+  if (!labelPosition) return 'flex-col';
+  if (labelPosition === 'top') return 'flex-col';
+  if (labelPosition === 'left') return 'flex-row';
+  if (labelPosition === 'right') return 'flex-row-reverse';
+};
+
+const getAlignItems = (labelPosition: LabelPosition | undefined) => {
+  if (labelPosition === 'right') return 'items-center';
+  return;
+};
+
+type Props = {
+  label?: string;
+  labelPosition?: LabelPosition;
+  labelSize?: LabelSize;
+  error?: string;
+  Component: FC<{ id: string }>;
+};
+
+export const FormElement: FC<Props> = ({
+  label,
+  labelPosition,
+  labelSize,
+  Component,
+  error,
+}) => {
+  const id = useId();
+
+  return (
+    <div className="flex flex-col">
+      <Wrapper labelPosition={labelPosition}>
+        <div
+          className={classNames(
+            'flex gap-1',
+            getFlexDirection(labelPosition),
+            getAlignItems(labelPosition),
+          )}
+        >
+          {label && (
+            <label
+              htmlFor={id}
+              className={labelSize ? `text-${labelSize}` : ''}
+            >
+              {label}
+            </label>
+          )}
+          <Component id={id} />
+        </div>
+      </Wrapper>
+      {error && <span className="text-red-500">{error}</span>}
+    </div>
+  );
+};
+
+FormElement.defaultProps = {
+  labelPosition: 'top',
+  labelSize: 'base',
+};
