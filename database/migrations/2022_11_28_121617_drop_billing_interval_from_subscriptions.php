558222b76b122fbd9a4865aed7ce7030202b5974
JonasVHG <jonas@cultuurnet.be>
2022-11-28 14:34:15 +0100

    implement down() in migration file

--- database/migrations/2022_11_28_121617_drop_billing_interval_from_subscriptions.php
+++ database/migrations/2022_11_28_121617_drop_billing_interval_from_subscriptions.php
@@ -17,6 +17,7 @@ return new class () extends Migration {
     public function down(): void
     {
         Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('billing_interval');
         });
     }
 };
