daa779c754fc18e6ba118d306cd73add7200855b
Luc Wollants <lucwollants@gmail.com>
2022-11-30 11:09:08 +0100

    Add property type hinting to `UserModel`

--- app/Domain/Users/Models/UserModel.php
+++ app/Domain/Users/Models/UserModel.php
@@ -9,6 +9,11 @@ use Auth0\Laravel\Contract\Model\Stateful\User as StatefulUser;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ */
 final class UserModel extends UuidModel implements AuthenticatableContract, StatefulUser
 {
     use Authenticatable;
