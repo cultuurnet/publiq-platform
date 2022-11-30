4f236f75465bddcf2fb42b482193746498eb0b53
JonasVHG <jonas@cultuurnet.be>
2022-11-30 16:51:05 +0100

    use a length of 255 to be futureproof

--- database/migrations/2022_11_30_145724_update_action_events_table.php
+++ database/migrations/2022_11_30_145724_update_action_events_table.php
@@ -10,7 +10,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('action_events', function (Blueprint $table) {
-            $table->string('user_id', 42)->change();
+            $table->string('user_id', 255)->change();
         });
     }
 
