b5a7954cdbc0c3fbf0804aa932025801155453ff
Luc Wollants <lucwollants@gmail.com>
2022-12-02 14:30:05 +0100

    Instantiate a Auth0\SDK\API\Management singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ namespace App\Providers;
 
 use App\Insightly\InsightlyClient;
 use App\Insightly\Pipelines;
+use App\Json;
+use Auth0\SDK\API\Management;
+use Auth0\SDK\Auth0;
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +27,24 @@ final class AppServiceProvider extends ServiceProvider
                 config('insightly.api_key'),
                 new Pipelines(config('insightly.pipelines'))
             );
+        });
+
+        $this->app->singleton(Management::class, function () {
+            $auth0 = new Auth0([
+                'domain' => config('auth0.management.domain'),
+                'clientId' => config('auth0.management.clientId'),
+                'clientSecret' => config('auth0.management.clientSecret'),
+                'cookieSecret' => config('auth0.management.cookieSecret'),
+            ]);
+
+            $response = $auth0->authentication()->clientCredentials([
+                'audience' => config('auth0.management.audience'),
+            ]);
+
+            $response = Json::decodeAssociatively((string) $response->getBody());
+            $auth0->configuration()->setManagementToken($response['access_token']);
+
+            return $auth0->management();
         });
     }
 
