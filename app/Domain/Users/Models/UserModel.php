a035d065100d3ea929a3030b9c787003447019b9
Luc Wollants <lucwollants@gmail.com>
2022-12-06 11:35:34 +0100

    Add extra type hints

--- app/Domain/Users/Models/UserModel.php
+++ app/Domain/Users/Models/UserModel.php
@@ -9,6 +9,11 @@ use Auth0\Laravel\Contract\Model\Stateful\User as StatefulUser;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
+/**
+ * @property string $id
+ * @property string $email
+ * @property string $name
+ */
 final class UserModel extends UuidModel implements AuthenticatableContract, StatefulUser
 {
     use Authenticatable;
@@ -17,7 +22,7 @@ final class UserModel extends UuidModel implements AuthenticatableContract, Stat
 
     protected $fillable = [
         'id',
-        'name',
         'email',
+        'name',
     ];
 }
