84282a5925d24f3137355bb9044097a48cd0b724
Luc Wollants <lucwollants@gmail.com>
2022-12-08 09:27:26 +0100

    Improve subscription seeder by checking existence of plan

--- database/seeders/SubscriptionsSeeder.php
+++ database/seeders/SubscriptionsSeeder.php
@@ -8,6 +8,7 @@ use App\Domain\Integrations\IntegrationType;
 use App\Domain\Subscriptions\Currency;
 use App\Domain\Subscriptions\Repositories\SubscriptionRepository;
 use App\Domain\Subscriptions\Subscription;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Seeder;
 use Ramsey\Uuid\Uuid;
 
@@ -15,8 +16,17 @@ final class SubscriptionsSeeder extends Seeder
 {
     public function run(SubscriptionRepository $subscriptionRepository): void
     {
+        $subscriptionId = Uuid::fromString('b46745a1-feb5-45fd-8fa9-8e3ef25aac26');
+
+        try {
+            $subscriptionRepository->getById($subscriptionId);
+            $this->command->info('Subscription already exists');
+            return;
+        } catch (ModelNotFoundException) {
+        }
+
         $subscription = new Subscription(
-            Uuid::uuid4(),
+            $subscriptionId,
             'Basic Plan - Search API - Monthly',
             'Basic Plan for integrating with Search API, billed monthly and with a onetime fee.',
             IntegrationType::SearchApi,
