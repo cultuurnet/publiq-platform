d010964444bd3ec6d7d2b484e77ec4088b91cf2c
Luc Wollants <lucwollants@gmail.com>
2022-12-08 09:26:29 +0100

    Add `toDomain` conversion method on `SubscriptionModel`

--- app/Domain/Subscriptions/Models/SubscriptionModel.php
+++ app/Domain/Subscriptions/Models/SubscriptionModel.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Domain\Subscriptions\Models;
 
+use App\Domain\Integrations\IntegrationType;
+use App\Domain\Subscriptions\Currency;
+use App\Domain\Subscriptions\Subscription;
 use App\Models\UuidModel;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 /**
  * @property string $id
@@ -46,6 +50,19 @@ final class SubscriptionModel extends UuidModel
         return Attribute::make(
             get: static fn ($value) => (float) $value / 100,
             set: static fn ($value) => (int) ($value * 100),
+        );
+    }
+
+    public function toDomain(): Subscription
+    {
+        return new Subscription(
+            Uuid::fromString($this->id),
+            $this->name,
+            $this->description,
+            IntegrationType::from($this->integration_type),
+            Currency::from($this->currency),
+            $this->price,
+            $this->fee
         );
     }
 }
