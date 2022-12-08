1f108f7e1f40eea8561598fc928a000614de96f0
JonasVHG <jonas@cultuurnet.be>
2022-12-08 13:10:46 +0100

    migration

--- database/migrations/2022_12_07_144000_update_integrations_table.php
+++ database/migrations/2022_12_07_144000_update_integrations_table.php
@@ -1,15 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::table('integrations', function (Blueprint $table) {
-            $table->string('status');
+            $table->string('status')->default('draft');
             $table->dropSoftDeletes();
         });
     }
