4790fb64f459e66142e60acc4be564c75646f1a6
Bert Ramakers <bertramakers@me.com>
2022-12-08 15:27:23 +0100

    Add migration to create table for auth0 client details

---
+++ database/migrations/2022_12_08_152100_create_auth0_clients_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('auth0_clients', static function (Blueprint $table) {
+            $table->uuid('integration_id')->primary();
+            $table->string('auth0_client_id')->index();
+            $table->string('auth0_client_secret');
+            $table->string('auth0_tenant');
+            $table->unique(['integration_id', 'auth0_tenant']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('auth0_clients');
+    }
+};
