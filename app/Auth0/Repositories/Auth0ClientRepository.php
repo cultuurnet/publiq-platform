593797397a013de1f8205afc7a7721bbb21194e2
Bert Ramakers <bertramakers@me.com>
2022-12-09 13:59:52 +0100

    Add method to save an Auth0Client
    It will either update or create the client based on the client id and tenant

--- app/Auth0/Repositories/Auth0ClientRepository.php
+++ app/Auth0/Repositories/Auth0ClientRepository.php
@@ -4,12 +4,29 @@ declare(strict_types=1);
 
 namespace App\Auth0\Repositories;
 
+use App\Auth0\Auth0Client;
 use App\Auth0\Auth0ClientsForIntegration;
 use App\Auth0\Models\Auth0ClientModel;
 use Ramsey\Uuid\UuidInterface;
 
 final class Auth0ClientRepository
 {
+    public function save(Auth0Client $auth0Client): void
+    {
+        Auth0ClientModel::query()->updateOrCreate(
+            [
+                'auth0_client_id' => $auth0Client->clientId,
+                'auth0_tenant' => $auth0Client->tenant->value,
+            ],
+            [
+                'integration_id' => $auth0Client->integrationId->toString(),
+                'auth0_client_id' => $auth0Client->clientId,
+                'auth0_client_secret' => $auth0Client->clientSecret,
+                'auth0_tenant' => $auth0Client->tenant->value,
+            ]
+        );
+    }
+
     public function getByIntegrationId(UuidInterface $integrationId): Auth0ClientsForIntegration
     {
         $auth0Clients = Auth0ClientModel::query()
