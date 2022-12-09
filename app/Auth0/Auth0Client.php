25708240f96a42bb667c630ae4d045e1db69806c
Bert Ramakers <bertramakers@me.com>
2022-12-09 12:51:51 +0100

    Rename $id to $integrationId

--- app/Auth0/Auth0Client.php
+++ app/Auth0/Auth0Client.php
@@ -9,7 +9,7 @@ use Ramsey\Uuid\UuidInterface;
 final class Auth0Client
 {
     public function __construct(
-        public readonly UuidInterface $id,
+        public readonly UuidInterface $integrationId,
         public readonly string $clientId,
         public readonly string $clientSecret,
         public readonly Auth0Tenant $tenant
