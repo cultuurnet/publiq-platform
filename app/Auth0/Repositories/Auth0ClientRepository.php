65bc2fc20d910e2c27a0b629feba7aeb3122e59a
Bert Ramakers <bertramakers@me.com>
2022-12-09 14:51:50 +0100

    Update Auth0ClientRepository::save() to allow multiple clients to be saved at once

--- app/Auth0/Repositories/Auth0ClientRepository.php
+++ app/Auth0/Repositories/Auth0ClientRepository.php
@@ -7,24 +7,34 @@ namespace App\Auth0\Repositories;
 use App\Auth0\Auth0Client;
 use App\Auth0\Auth0ClientsForIntegration;
 use App\Auth0\Models\Auth0ClientModel;
+use Illuminate\Support\Facades\DB;
 use Ramsey\Uuid\UuidInterface;
 
 final class Auth0ClientRepository
 {
-    public function save(Auth0Client $auth0Client): void
+    public function save(Auth0Client ...$auth0Clients): void
     {
-        Auth0ClientModel::query()->updateOrCreate(
-            [
-                'auth0_client_id' => $auth0Client->clientId,
-                'auth0_tenant' => $auth0Client->tenant->value,
-            ],
-            [
-                'integration_id' => $auth0Client->integrationId->toString(),
-                'auth0_client_id' => $auth0Client->clientId,
-                'auth0_client_secret' => $auth0Client->clientSecret,
-                'auth0_tenant' => $auth0Client->tenant->value,
-            ]
-        );
+        if (count($auth0Clients) === 0) {
+            return;
+        }
+
+        DB::transaction(static function () use ($auth0Clients) {
+            foreach ($auth0Clients as $auth0Client) {
+                Auth0ClientModel::query()
+                    ->updateOrCreate(
+                        [
+                            'auth0_client_id' => $auth0Client->clientId,
+                            'auth0_tenant' => $auth0Client->tenant->value,
+                        ],
+                        [
+                            'integration_id' => $auth0Client->integrationId->toString(),
+                            'auth0_client_id' => $auth0Client->clientId,
+                            'auth0_client_secret' => $auth0Client->clientSecret,
+                            'auth0_tenant' => $auth0Client->tenant->value,
+                        ]
+                    );
+            }
+        });
     }
 
     public function getByIntegrationId(UuidInterface $integrationId): Auth0ClientsForIntegration
