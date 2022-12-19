e25cabfdead2ec26115d78da4bee9687005bb6ec
Luc Wollants <lucwollants@gmail.com>
2022-12-19 13:39:57 +0100

    Add method to convert `OrganizationModel` to `Organization` domain object

--- app/Domain/Organizations/Models/OrganizationModel.php
+++ app/Domain/Organizations/Models/OrganizationModel.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Organizations\Models;
 
+use App\Domain\Organizations\Address;
+use App\Domain\Organizations\Events\OrganizationCreated;
+use App\Domain\Organizations\Organization;
 use App\Models\UuidModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 final class OrganizationModel extends UuidModel
 {
@@ -22,4 +26,21 @@ final class OrganizationModel extends UuidModel
         'city',
         'country',
     ];
+
+    public function toDomain(): Organization
+    {
+        $address = new Address(
+            $this->street ?: '',
+            $this->zip ?: '',
+            $this->city ?: '',
+            $this->country ?: '',
+        );
+
+        return new Organization(
+            Uuid::fromString($this->id),
+            $this->name,
+            $this->vat,
+            $address->isEmpty() ? null : $address,
+        );
+    }
 }
