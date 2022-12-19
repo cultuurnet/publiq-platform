107bf64b5f6fe731ac9427c7db7c2b14f810fd49
Luc Wollants <lucwollants@gmail.com>
2022-12-19 14:16:43 +0100

    Create custom field serializer `VatSerializer`

---
+++ app/Insightly/Serializers/VatSerializer.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Insightly\Serializers;
+
+final class VatSerializer
+{
+    private const CUSTOM_FIELD_VAT = 'BTW_nummer__c';
+
+    public function toInsightlyArray(string $vat): array
+    {
+        return [
+            'FIELD_NAME' => self::CUSTOM_FIELD_VAT,
+            'CUSTOM_FIELD_ID' => self::CUSTOM_FIELD_VAT,
+            'FIELD_VALUE' => $vat,
+        ];
+    }
+}
