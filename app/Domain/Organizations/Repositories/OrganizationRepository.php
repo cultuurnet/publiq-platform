b976a424a9d63572c552b7d5e429315e0bd59322
Luc Wollants <lucwollants@gmail.com>
2022-12-19 13:38:25 +0100

    Add interface `OrganizationRepository`

--- app/Domain/Organizations/Repositories/OrganizationRepository.php
+++ app/Domain/Organizations/Repositories/OrganizationRepository.php
@@ -4,21 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Organizations\Repositories;
 
-use App\Domain\Organizations\Models\OrganizationModel;
 use App\Domain\Organizations\Organization;
+use Ramsey\Uuid\UuidInterface;
 
-final class OrganizationRepository
+interface OrganizationRepository
 {
-    public function save(Organization $organization): void
-    {
-        OrganizationModel::query()->create([
-            'id' => $organization->id->toString(),
-            'name' => $organization->name,
-            'vat' => $organization->vat,
-            'street' => $organization->address?->street,
-            'zip' => $organization->address?->zip,
-            'city' => $organization->address?->city,
-            'country' => $organization->address?->country,
-        ]);
-    }
+    public function save(Organization $organization): void;
+
+    public function getById(UuidInterface $id): Organization;
 }
