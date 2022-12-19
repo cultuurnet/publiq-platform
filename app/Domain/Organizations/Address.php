5ad37b43a4331d47a389aa6c39d8129d28585c68
Luc Wollants <lucwollants@gmail.com>
2022-12-19 13:38:49 +0100

    Remove uuids from `Address` domain object

--- app/Domain/Organizations/Address.php
+++ app/Domain/Organizations/Address.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace App\Domain\Organizations;
 
-use Ramsey\Uuid\UuidInterface;
-
 final class Address
 {
     public function __construct(
-        public readonly UuidInterface $id,
-        public readonly UuidInterface $organizationId,
         public readonly string $street,
         public readonly string $zip,
         public readonly string $city,
