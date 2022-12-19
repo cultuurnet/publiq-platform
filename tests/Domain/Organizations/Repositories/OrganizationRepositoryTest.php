4615ef2ae333dbdb0be7672333e08f53ddd6ed5f
Luc Wollants <lucwollants@gmail.com>
2022-12-19 13:40:46 +0100

    Rework unit tests

--- tests/Domain/Organizations/Repositories/OrganizationRepositoryTest.php
+++ tests/Domain/Organizations/Repositories/OrganizationRepositoryTest.php
@@ -6,7 +6,7 @@ namespace Tests\Domain\Organizations\Repositories;
 
 use App\Domain\Organizations\Address;
 use App\Domain\Organizations\Organization;
-use App\Domain\Organizations\Repositories\OrganizationRepository;
+use App\Domain\Organizations\Repositories\EloquentOrganizationRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
@@ -15,13 +15,13 @@ final class OrganizationRepositoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    private OrganizationRepository $organizationRepository;
+    private EloquentOrganizationRepository $organizationRepository;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->organizationRepository = new OrganizationRepository();
+        $this->organizationRepository = new EloquentOrganizationRepository();
     }
 
     public function test_it_can_save_an_organization(): void
@@ -45,21 +45,16 @@ final class OrganizationRepositoryTest extends TestCase
     public function test_it_can_save_an_organization_with_address(): void
     {
         $organizationId = Uuid::uuid4();
-
-        $address = new Address(
-            Uuid::uuid4(),
-            $organizationId,
-            'Henegouwenkaai 41-43',
-            '1080',
-            'Brussel',
-            'België'
-        );
-
         $organization = new Organization(
             $organizationId,
             'Test Organization',
             null,
-            $address
+            new Address(
+                'Henegouwenkaai 41-43',
+                '1080',
+                'Brussel',
+                'België'
+            )
         );
 
         $this->organizationRepository->save($organization);
@@ -68,10 +63,10 @@ final class OrganizationRepositoryTest extends TestCase
             'id' => $organization->id->toString(),
             'name' => $organization->name,
             'vat' => $organization->vat,
-            'street' => $address->street,
-            'zip' => $address->zip,
-            'city' => $address->city,
-            'country' => $address->country,
+            'street' => $organization->address?->street,
+            'zip' => $organization->address?->zip,
+            'city' => $organization->address?->city,
+            'country' => $organization->address?->country,
         ]);
     }
 
@@ -91,5 +86,52 @@ final class OrganizationRepositoryTest extends TestCase
             'name' => $organization->name,
             'vat' => $organization->vat,
         ]);
+    }
+
+    public function test_it_can_get_an_organization(): void
+    {
+        $organization = new Organization(
+            Uuid::uuid4(),
+            'Test Organization',
+            null,
+            null
+        );
+
+        $this->organizationRepository->save($organization);
+
+        $this->assertEquals($organization, $this->organizationRepository->getById($organization->id));
+    }
+
+    public function test_it_can_get_an_organization_with_vat(): void
+    {
+        $organization = new Organization(
+            Uuid::uuid4(),
+            'Test Organization',
+            'BE 0475 250 609',
+            null
+        );
+
+        $this->organizationRepository->save($organization);
+
+        $this->assertEquals($organization, $this->organizationRepository->getById($organization->id));
+    }
+
+    public function test_it_can_get_an_organization_with_address(): void
+    {
+        $organization = new Organization(
+            Uuid::uuid4(),
+            'Test Organization',
+            null,
+            new Address(
+                'Henegouwenkaai 41-43',
+                '1080',
+                'Brussel',
+                'België'
+            )
+        );
+
+        $this->organizationRepository->save($organization);
+
+        $this->assertEquals($organization, $this->organizationRepository->getById($organization->id));
     }
 }
