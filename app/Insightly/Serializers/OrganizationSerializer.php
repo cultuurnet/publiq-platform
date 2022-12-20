26e23d41531aa53e8e0c9bd3c0b2aaf72abba6bb
Luc Wollants <lucwollants@gmail.com>
2022-12-20 11:45:23 +0100

    Improve type hinting

--- app/Insightly/Serializers/OrganizationSerializer.php
+++ app/Insightly/Serializers/OrganizationSerializer.php
@@ -8,14 +8,14 @@ use App\Domain\Organizations\Organization;
 
 final class OrganizationSerializer
 {
-    /** @return array<string, string> */
+    /** @return array<string, array|string|null> */
     public function toInsightlyArray(Organization $organization): array
     {
         $organizationAsArray = [
             'ORGANISATION_NAME' => $organization->name,
-            'ADDRESS_BILLING_STREET' => $organization->address->street,
-            'ADDRESS_BILLING_POSTCODE' => $organization->address->zip,
-            'ADDRESS_BILLING_CITY' => $organization->address->street,
+            'ADDRESS_BILLING_STREET' => $organization->address?->street,
+            'ADDRESS_BILLING_POSTCODE' => $organization->address?->zip,
+            'ADDRESS_BILLING_CITY' => $organization->address?->street,
         ];
 
         if ($organization->vat) {
