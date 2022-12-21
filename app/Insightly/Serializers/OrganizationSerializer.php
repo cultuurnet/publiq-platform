3288f20a76cac2ca0a962126aa9a8d4f5e46e3d5
Luc Wollants <lucwollants@gmail.com>
2022-12-21 11:25:04 +0100

    Correctly serialize an Organization

--- app/Insightly/Serializers/OrganizationSerializer.php
+++ app/Insightly/Serializers/OrganizationSerializer.php
@@ -8,14 +8,14 @@ use App\Domain\Organizations\Organization;
 
 final class OrganizationSerializer
 {
-    /** @return array<string, array|string|null> */
+    /** @return array<string, array|string> */
     public function toInsightlyArray(Organization $organization): array
     {
         $organizationAsArray = [
             'ORGANISATION_NAME' => $organization->name,
             'ADDRESS_BILLING_STREET' => $organization->address->street,
             'ADDRESS_BILLING_POSTCODE' => $organization->address->zip,
-            'ADDRESS_BILLING_CITY' => $organization->address->street,
+            'ADDRESS_BILLING_CITY' => $organization->address->city,
         ];
 
         if ($organization->vat) {
