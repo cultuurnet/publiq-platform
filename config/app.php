9d7f2012621a379ae73b83116348ddc90f8e310d
Bert Ramakers <bertramakers@me.com>
2023-01-03 15:06:06 +0100

    Glue it all together

--- config/app.php
+++ config/app.php
@@ -199,6 +199,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\UiTiDv1\UiTiDv1ServiceProvider::class,
 
     ],
 
