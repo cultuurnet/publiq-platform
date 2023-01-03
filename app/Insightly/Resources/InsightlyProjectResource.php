8fadc25f2d29266d72902ee2509a0e90d214f674
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-03 14:53:19 +0100

    make crud for ProjectResource

--- app/Insightly/Resources/InsightlyProjectResource.php
+++ app/Insightly/Resources/InsightlyProjectResource.php
@@ -7,6 +7,10 @@ namespace App\Insightly\Resources;
 use App\Domain\Integrations\Integration;
 use App\Insightly\InsightlyClient;
 use App\Insightly\Objects\ProjectStage;
+use App\Insightly\Serializers\ProjectSerializer;
+use App\Insightly\Serializers\ProjectStageSerializer;
+use App\Json;
+use GuzzleHttp\Psr7\Request;
 
 final class InsightlyProjectResource implements ProjectResource
 {
@@ -19,14 +23,48 @@ final class InsightlyProjectResource implements ProjectResource
 
     public function create(Integration $integration): int
     {
-        return 0;
+        $request = new Request(
+            'POST',
+            $this->path,
+            [],
+            JSON::encode(
+                (new ProjectSerializer($this->insightlyClient->getPipelines()))
+                    ->toInsightlyArray($integration)
+            )
+        );
+        $response = $this->insightlyClient->sendRequest($request);
+
+        $projectAsArray = JSON::decodeAssociatively($response->getBody()->getContents());
+
+        $id = (int) $projectAsArray['PROJECT_ID'];
+
+        $this->updateStage($id, ProjectStage::TEST);
+
+        return $id;
     }
 
     public function delete(int $id): void
     {
+        $request = new Request(
+            'DELETE',
+            $this->path . $id
+        );
+
+        $this->insightlyClient->sendRequest($request);
     }
 
     public function updateStage(int $id, ProjectStage $stage): void
     {
+        $stageRequest = new Request(
+            'PUT',
+            $this->path . $id . '/Pipeline',
+            [],
+            Json::encode(
+                (new ProjectStageSerializer($this->insightlyClient->getPipelines()))
+                    ->toInsightlyArray($stage)
+            )
+        );
+
+        $this->insightlyClient->sendRequest($stageRequest);
     }
 }
