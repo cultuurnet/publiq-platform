aab132037e56a9520287130330459952693b88af
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-03 14:50:07 +0100

    Add Enum Values ProjectState

--- app/Insightly/Objects/ProjectState.php
+++ app/Insightly/Objects/ProjectState.php
@@ -6,4 +6,10 @@ namespace App\Insightly\Objects;
 
 enum ProjectState: string
 {
+    case NOT_STARTED = 'Not Started';
+    case IN_PROGRESS = 'In Progress';
+    case DEFERRED = 'Deferred';
+    case CANCELLED = 'Cancelled';
+    case ABANDONED = 'Abandoned';
+    case COMPLETED = 'Completed';
 }
