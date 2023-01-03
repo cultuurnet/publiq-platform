5c37ac0de7c500281f64f3ca245fbf398f95ab2d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-03 14:52:35 +0100

    Add ProjectSerializer

--- app/Insightly/Serializers/ProjectSerializer.php
+++ app/Insightly/Serializers/ProjectSerializer.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace App\Insightly\Serializers;
 
 use App\Domain\Integrations\Integration;
+use App\Insightly\Objects\ProjectStage;
+use App\Insightly\Objects\ProjectState;
+use App\Insightly\Pipelines;
+use App\Insightly\Serializers\CustomFields\IntegrationTypeSerializer;
 
 final class ProjectSerializer
 {
@@ -14,6 +18,15 @@ final class ProjectSerializer
 
     public function toInsightlyArray(Integration $integration): array
     {
-        return [];
+        return [
+            'PROJECT_NAME' => $integration->name,
+            'STATUS' => ProjectState::NOT_STARTED->value,
+            'PROJECT_DETAILS' => $integration->description,
+            'PIPELINE_ID' => $this->pipelines->getProjectsPipelineId(),
+            'STAGE_ID' => $this->pipelines->getProjectStageId(ProjectStage::TEST),
+            'CUSTOMFIELDS' => [
+                (new IntegrationTypeSerializer())->toInsightlyArray($integration->type),
+            ],
+        ];
     }
 }
