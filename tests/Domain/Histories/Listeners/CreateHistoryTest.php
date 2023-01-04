c5911196096faf4dd9ced5790fcd9e8fbb56146d
Luc Wollants <lucwollants@gmail.com>
2023-01-04 15:11:09 +0100

    Create skeleton for tests

---
+++ tests/Domain/Histories/Listeners/CreateHistoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Histories\Listeners;
+
+use PHPUnit\Framework\TestCase;
+
+final class CreateHistoryTest extends TestCase
+{
+}
---
+++ tests/Domain/Histories/Repositories/EloquentHistoryRepositoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Histories\Repositories;
+
+use Tests\TestCase;
+
+final class EloquentHistoryRepositoryTest extends TestCase
+{
+}
