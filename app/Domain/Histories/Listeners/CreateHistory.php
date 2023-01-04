07498ec47d6865fcaa8ab501a22e91a8a4014c18
Luc Wollants <lucwollants@gmail.com>
2023-01-04 15:08:59 +0100

    Add a listener to create a history item

---
+++ app/Domain/Histories/Listeners/CreateHistory.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Histories\Listeners;
+
+use App\Domain\Auth\CurrentUser;
+use App\Domain\Histories\History;
+use App\Domain\Histories\Repositories\HistoryRepository;
+use Illuminate\Support\Carbon;
+use Ramsey\Uuid\Uuid;
+
+final class CreateHistory
+{
+    public function __construct(
+        private readonly HistoryRepository $historyRepository,
+        private readonly CurrentUser $currentUser
+    ) {
+    }
+
+    public function handle(string $eventName, array $data): void
+    {
+        $this->historyRepository->create(
+            new History(
+                Uuid::uuid4(),
+                $data['id'],
+                $eventName,
+                $this->currentUser->id(),
+                Carbon::now(),
+                $eventName
+            )
+        );
+    }
+}
