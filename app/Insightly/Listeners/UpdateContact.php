88b2df682c1f4d94b2b17436c88c33dceaabfa6e
steftrenson <steftrenson@gmail.com>
2023-01-05 09:14:23 +0100

    Test and implement UpdateContact

--- app/Insightly/Listeners/UpdateContact.php
+++ app/Insightly/Listeners/UpdateContact.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Insightly\Listeners;
 
 use App\Domain\Contacts\Events\ContactUpdated;
+use App\Domain\Contacts\Repositories\ContactRepository;
+use App\Insightly\InsightlyClient;
+use App\Insightly\Repositories\InsightlyMappingRepository;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Psr\Log\LoggerInterface;
@@ -15,12 +18,20 @@ final class UpdateContact implements ShouldQueue
     use Queueable;
 
     public function __construct(
+        private readonly InsightlyClient $insightlyClient,
+        private readonly ContactRepository $contactRepository,
+        private readonly InsightlyMappingRepository $insightlyMappingRepository,
         private readonly LoggerInterface $logger,
     ) {
     }
 
     public function handle(ContactUpdated $contactUpdated): void
     {
+        $contact = $this->contactRepository->getById($contactUpdated->id);
+        $insightlyMapping = $this->insightlyMappingRepository->getById($contact->id);
+
+        $this->insightlyClient->contacts()->update($contact, $insightlyMapping->insightlyId);
+
         $this->logger->info(
             'Contact updated',
             [
