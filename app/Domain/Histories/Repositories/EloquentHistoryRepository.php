11afcbe7d65a4decfb012ecd585a485dc84b7bee
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-05 15:40:32 +0100

    fill up HistoryModel

--- app/Domain/Histories/Repositories/EloquentHistoryRepository.php
+++ app/Domain/Histories/Repositories/EloquentHistoryRepository.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace App\Domain\Histories\Repositories;
 
 use App\Domain\Histories\History;
+use App\Domain\Histories\Models\HistoryModel;
 
 final class EloquentHistoryRepository implements HistoryRepository
 {
-    public function create(History $history): void
+    function create(History $history): void
     {
+        HistoryModel::query()->create([
+            'id' => $history->id,
+            'item_id' => $history->itemId,
+            'user_id' => $history->userId,
+            'type' => $history->type,
+            'action' => $history->action,
+            'timestamp' => $history->timestamp
+        ]);
     }
 }
