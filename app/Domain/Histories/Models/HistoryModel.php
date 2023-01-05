e55bb4cf14f3b398b6ab4bfc4ce03a7b7ba43c0d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-05 15:52:37 +0100

    create toDomain for HistoryModel

--- app/Domain/Histories/Models/HistoryModel.php
+++ app/Domain/Histories/Models/HistoryModel.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Domain\Histories\Models;
 
+use App\Domain\Histories\History;
 use App\Models\UuidModel;
+use Illuminate\Support\Carbon;
+use Ramsey\Uuid\Uuid;
 
 final class HistoryModel extends UuidModel
 {
@@ -20,4 +23,17 @@ final class HistoryModel extends UuidModel
         'action',
         'timestamp',
     ];
+
+    public function toDomain(): History
+    {
+        return new History(
+            Uuid::fromString($this->id),
+            Uuid::fromString($this->itemId),
+            $this->userId,
+            $this->type,
+            $this->action,
+            (new Carbon())->rawParse($this->timestamp)
+        );
+
+    }
 }
