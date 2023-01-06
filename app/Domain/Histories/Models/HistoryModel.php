b184182e269fd8f9dd2915cb48c86170c331efbd
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-06 11:27:51 +0100

    remove unused methods

--- app/Domain/Histories/Models/HistoryModel.php
+++ app/Domain/Histories/Models/HistoryModel.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Histories\Models;
 
-use App\Domain\Histories\History;
 use App\Models\UuidModel;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Carbon;
-use Ramsey\Uuid\Uuid;
 
 final class HistoryModel extends UuidModel
 {
@@ -24,24 +20,4 @@ final class HistoryModel extends UuidModel
         'action',
         'timestamp',
     ];
-
-    /**
-     * @return BelongsTo<UuidModel, HistoryModel>
-     */
-    public function integration(): BelongsTo
-    {
-        return $this->belongsTo(UuidModel::class, 'domain_id');
-    }
-
-    public function toDomain(): History
-    {
-        return new History(
-            Uuid::fromString($this->id),
-            Uuid::fromString($this->itemId),
-            $this->userId,
-            $this->type,
-            $this->action,
-            (new Carbon())->rawParse($this->timestamp)
-        );
-    }
 }
