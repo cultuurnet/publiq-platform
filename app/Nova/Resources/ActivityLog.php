c4e7a8b497e7e4240c5462146de66fc9135aad25
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-10 12:43:03 +0100

    Add Activity Resource

---
+++ app/Nova/Resources/ActivityLog.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Resources;
+
+use Laravel\Nova\Fields\Code;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Resource;
+use Spatie\Activitylog\Models\Activity;
+
+/**
+ * @mixin Activity
+ */
+final class ActivityLog extends Resource
+{
+    public static string $model = Activity::class;
+
+    public static $title = 'description';
+
+    /**
+     * @var array<string>
+     */
+    public static $search = [
+                'description',
+                'subject_id',
+            ];
+
+    /**
+     * @return array<Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+                ID::make()
+                    ->readonly()
+                    ->onlyOnDetail(),
+
+            DateTime::make('Timestamp', 'created_at')
+                ->readonly(),
+
+            Text::make('Action', 'description')
+                ->readonly(),
+
+            Text::make('Item ID', 'subject_id')
+                ->onlyOnDetail()
+                ->readonly(),
+
+            Text::make('User ID', 'causer_id')
+            ->readonly(),
+
+            Code::make('Metadata', 'properties')->json(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+            ->readonly(),
+        ];
+    }
+}
