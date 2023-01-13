f102c9d0a8f4ca7bdc5bc33e0064b410680759b9
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-13 16:14:48 +0100

    type db-migration

--- database/migrations/2023_01_13_142805_update_coupons_table.php
+++ database/migrations/2023_01_13_142805_update_coupons_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,14 +10,14 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('coupons', static function (Blueprint $table) {
-            $table->boolean('distributed')->after('id')->default(false);
+            $table->boolean('is_distributed')->after('id')->default(false);
         });
     }
 
     public function down(): void
     {
         Schema::table('coupons', static function (Blueprint $table) {
-            $table->dropColumn('distributed');
+            $table->dropColumn('is_distributed');
         });
     }
 };
