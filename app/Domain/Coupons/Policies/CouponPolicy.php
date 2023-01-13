6cf4183e78389ada04c632c09fc42123a05cbeaf
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-13 08:47:15 +0100

    create CouponPolicy.php

---
+++ app/Domain/Coupons/Policies/CouponPolicy.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Coupons\Policies;
+
+use App\Domain\Auth\Models\UserModel;
+use App\Domain\Coupons\Models\CouponModel;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+final class CouponPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(UserModel $userModel): bool
+    {
+        return true;
+    }
+
+    public function view(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return true;
+    }
+
+    public function create(UserModel $userModel): bool
+    {
+        return false;
+    }
+
+    public function update(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return false;
+    }
+
+    public function delete(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return false;
+    }
+
+    public function restore(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return false;
+    }
+
+    public function replicate(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(UserModel $userModel, CouponModel $couponModel): bool
+    {
+        return false;
+    }
+}
