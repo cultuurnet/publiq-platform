a391f2bfe3df2df6c5e9ac4ec21557b8e3a4acb3
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-18 13:06:00 +0100

    add validation for couponcode

--- app/Nova/Actions/ActivateIntegration.php
+++ app/Nova/Actions/ActivateIntegration.php
@@ -6,6 +6,7 @@ namespace App\Nova\Actions;
 
 use App\Domain\Coupons\Models\CouponModel;
 use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Actions\Action;
@@ -18,14 +19,22 @@ final class ActivateIntegration extends Action
     use InteractsWithQueue;
     use Queueable;
 
-    public function handle(ActionFields $fields, Collection $integrations): void
+    public function handle(ActionFields $fields, Collection $integrations): array
     {
-        $coupon = CouponModel::query()->where('code', '=', $fields->get('coupon'));
-        $integration = $integrations->first();
+        try {
+            $integration = $integrations->first();
+            $coupon = CouponModel::query()
+                ->where('code', '=', $fields->get('coupon'))
+                ->whereNull('integration_id')
+                ->firstOrFail();
 
-        $coupon->update([
-            'integration_id' => $integration->id,
-        ]);
+            $coupon->update([
+                'integration_id' => $integration->id,
+                ]);
+            return Action::message('Integration ' . $integration->name . ' activated with coupon ' . $fields->get('coupon'));
+        } catch (ModelNotFoundException $exception) {
+            return Action::danger($fields->get('coupon') . ' is not an valid coupon.');
+        }
     }
 
     public function fields(NovaRequest $request): array
