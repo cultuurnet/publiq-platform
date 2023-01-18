61df03913de3477b21e3c99b945a8fe4d2624824
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-18 09:16:04 +0100

    make action DistributeCoupon

---
+++ app/Nova/Actions/DistributeCoupon.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Actions;
+
+use App\Domain\Coupons\Models\CouponModel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+final class DistributeCoupon extends Action
+{
+    use InteractsWithQueue;
+    use Queueable;
+
+    public function handle(ActionFields $fields, Collection $coupons)
+    {
+        /** @var CouponModel $coupon */
+        foreach ($coupons as $coupon) {
+            $coupon->update(['is_distributed' => true]);
+        }
+    }
+
+    public function fields(NovaRequest $request): array
+    {
+        return [];
+    }
+}
