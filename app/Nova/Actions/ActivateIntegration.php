a965efa2126c2c68f043dfce32fb1cd31517b075
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-18 15:30:42 +0100

    Move activateWithCoupon to repo

--- app/Nova/Actions/ActivateIntegration.php
+++ app/Nova/Actions/ActivateIntegration.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Nova\Actions;
 
 use App\Domain\Integrations\Models\IntegrationModel;
+use App\Domain\Integrations\Repositories\IntegrationRepository;
 use Illuminate\Bus\Queueable;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Ramsey\Uuid\Uuid;
 
 final class ActivateIntegration extends Action
 {
@@ -21,10 +24,12 @@ final class ActivateIntegration extends Action
 
     public function handle(ActionFields $fields, Collection $integrations): array
     {
+        /** @var IntegrationRepository $repository */
+        $repository = App::make(IntegrationRepository::class);
         try {
             /** @var IntegrationModel $integration */
             $integration = $integrations->first();
-            $integration->activateWithCoupon($fields->get('coupon'));
+            $repository->activateWithCoupon(Uuid::fromString($integration->getKey('id')), $fields->get('coupon'));
             return Action::message('Integration ' . $integration->name . ' activated with coupon ' . $fields->get('coupon'));
         } catch (ModelNotFoundException $exception) {
             return Action::danger($fields->get('coupon') . ' is not an valid coupon.');
