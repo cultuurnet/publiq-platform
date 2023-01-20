d0578c1a0951621b0c2e5f73e465f53ae9def614
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-20 11:37:21 +0100

    Don't use uuidModel for InsightlyMappingModel

--- app/Insightly/Models/InsightlyMappingModel.php
+++ app/Insightly/Models/InsightlyMappingModel.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Insightly\Models;
 
-use App\Models\UuidModel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-final class InsightlyMappingModel extends UuidModel
+final class InsightlyMappingModel extends Model
 {
     use SoftDeletes;
 
