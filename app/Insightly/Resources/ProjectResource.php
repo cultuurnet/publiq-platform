96af3855934914162869520cb1506e95b475a30e
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-01-26 13:42:32 +0100

    Add updateState() on ProjectResource and use consistent naming for $insightlyId

--- app/Insightly/Resources/ProjectResource.php
+++ app/Insightly/Resources/ProjectResource.php
@@ -7,16 +7,19 @@ namespace App\Insightly\Resources;
 use App\Domain\Contacts\ContactType;
 use App\Domain\Integrations\Integration;
 use App\Insightly\Objects\ProjectStage;
+use App\Insightly\Objects\ProjectState;
 
 interface ProjectResource
 {
     public function create(Integration $integration): int;
 
-    public function updateWithCoupon(int $insightlyId, string $couponCode): void;
+    public function updateWithCoupon(int $projectId, string $couponCode): void;
 
-    public function delete(int $id): void;
+    public function delete(int $projectId): void;
 
-    public function updateStage(int $id, ProjectStage $stage): void;
+    public function updateStage(int $projectId, ProjectStage $stage): void;
+
+    public function updateState(int $projectId, ProjectState $state): void;
 
     public function linkOpportunity(int $projectId, int $opportunityId): void;
 
