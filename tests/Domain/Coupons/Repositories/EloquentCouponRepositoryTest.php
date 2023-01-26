a13f45d0c225c952a47766e313ff6f92dda83c9c
Luc Wollants <lucwollants@gmail.com>
2023-01-26 16:10:48 +0100

    Remove the redundant `RefreshDatabase` from tests

--- tests/Domain/Coupons/Repositories/EloquentCouponRepositoryTest.php
+++ tests/Domain/Coupons/Repositories/EloquentCouponRepositoryTest.php
@@ -7,14 +7,11 @@ namespace Tests\Domain\Coupons\Repositories;
 use App\Domain\Coupons\Coupon;
 use App\Domain\Coupons\Repositories\EloquentCouponRepository;
 use Illuminate\Database\QueryException;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 
 final class EloquentCouponRepositoryTest extends TestCase
 {
-    use RefreshDatabase;
-
     private EloquentCouponRepository $couponRepository;
 
     protected function setUp(): void
