89338354ded238e0ba82f6d2c586a6dec649ee58
Luc Wollants <lucwollants@gmail.com>
2023-02-08 10:56:34 +0100

    Use `ResourceType` for link name

--- app/Insightly/Serializers/LinkSerializer.php
+++ app/Insightly/Serializers/LinkSerializer.php
@@ -6,17 +6,15 @@ namespace App\Insightly\Serializers;
 
 use App\Domain\Contacts\ContactType;
 use App\Insightly\Objects\Role;
+use App\Insightly\Resources\ResourceType;
 
 final class LinkSerializer
 {
-    private const CONTACT_LINK_OBJECT_NAME = 'Contact';
-    private const OPPORTUNITY_LINK_OBJECT_NAME = 'Opportunity';
-
     public function contactToLink(int $contactId, ContactType $contactType): array
     {
         return [
             'LINK_OBJECT_ID' => $contactId,
-            'LINK_OBJECT_NAME' => self::CONTACT_LINK_OBJECT_NAME,
+            'LINK_OBJECT_NAME' => ResourceType::Contact->name,
             'ROLE' => $this->contactTypeToRole($contactType),
         ];
     }
@@ -30,7 +28,7 @@ final class LinkSerializer
     {
         return [
             'LINK_OBJECT_ID' => $opportunityId,
-            'LINK_OBJECT_NAME' => self::OPPORTUNITY_LINK_OBJECT_NAME,
+            'LINK_OBJECT_NAME' => ResourceType::Opportunity->name,
         ];
     }
 }
