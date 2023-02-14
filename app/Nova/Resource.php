820a6d30e3f9ed9822910ff43e068b2a16d37cc0
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-14 14:14:12 +0100

    Add new Abstract Resource

---
+++ app/Nova/Resource.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova;
+
+use Illuminate\Database\Eloquent\Builder;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Resource as NovaResource;
+
+abstract class Resource extends NovaResource
+{
+    public static ?array $defaultSort = null;
+
+    public static function indexQuery(NovaRequest $request, $query): Builder
+    {
+        if (static::$defaultSort && empty($request->get('orderBy'))) {
+            $query->getQuery()->orders = [];
+            foreach (static::$defaultSort as $field => $order) {
+                $query->orderBy($field, $order);
+            }
+        }
+
+        return $query;
+    }
+}
