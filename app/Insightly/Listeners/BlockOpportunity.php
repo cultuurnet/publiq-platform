0a8d9f01c5cba0b58471bfb0fbe23b19025c5c48
Luc Wollants <lucwollants@gmail.com>
2023-03-02 13:38:54 +0100

    Add `BlockOpportunity` Insightly listener

---
+++ app/Insightly/Listeners/BlockOpportunity.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Insightly\Listeners;
+
+use App\Domain\Integrations\Events\IntegrationBlocked;
+use App\Insightly\InsightlyClient;
+use App\Insightly\Objects\OpportunityState;
+use App\Insightly\Repositories\InsightlyMappingRepository;
+use App\Insightly\Resources\ResourceType;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+final class BlockOpportunity
+{
+    public function __construct(
+        private readonly InsightlyClient $insightlyClient,
+        private readonly InsightlyMappingRepository $insightlyMappingRepository,
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
+    public function handle(IntegrationBlocked $integrationBlocked): void
+    {
+        $integrationId = $integrationBlocked->id;
+
+        $insightlyMapping = $this->insightlyMappingRepository->getByIdAndType(
+            $integrationId,
+            ResourceType::Opportunity
+        );
+
+        $this->insightlyClient->opportunities()->updateState(
+            $insightlyMapping->insightlyId,
+            OpportunityState::SUSPENDED
+        );
+
+        $this->logger->info(
+            'Opportunity blocked',
+            [
+                'domain' => 'insightly',
+                'integration_id' => $integrationId,
+            ]
+        );
+    }
+
+    public function failed(
+        IntegrationBlocked $integrationBlocked,
+        Throwable $exception
+    ): void {
+        $this->logger->error(
+            'Failed to block opportunity',
+            [
+                'domain' => 'insightly',
+                'contact_id' => $integrationBlocked->id->toString(),
+                'exception' => $exception,
+            ]
+        );
+    }
+}
