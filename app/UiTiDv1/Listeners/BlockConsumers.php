1d8519fbf79e3e9c17a8aa98d9fbd815a7187f21
Luc Wollants <lucwollants@gmail.com>
2023-03-03 09:33:30 +0100

    Add UiTiD listener to block consumers

---
+++ app/UiTiDv1/Listeners/BlockConsumers.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UiTiDv1\Listeners;
+
+use App\Domain\Integrations\Events\IntegrationBlocked;
+use App\UiTiDv1\Repositories\UiTiDv1ConsumerRepository;
+use App\UiTiDv1\UiTiDv1ClusterSDK;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+final class BlockConsumers implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+        private readonly UiTiDv1ClusterSDK $clusterSDK,
+        private readonly UiTiDv1ConsumerRepository $consumerRepository,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    public function handle(IntegrationBlocked $integrationBlocked): void
+    {
+        $uiTiDv1Consumers = $this->consumerRepository->getByIntegrationId($integrationBlocked->id);
+
+        $this->clusterSDK->blockConsumers(...$uiTiDv1Consumers);
+
+        $this->logger->info(
+            'UiTiD v1 consumer(s) blocked',
+            [
+                'domain' => 'insightly',
+                'integration_id' => $integrationBlocked->id->toString(),
+            ]
+        );
+    }
+
+    public function failed(IntegrationBlocked $integrationBlocked, Throwable $throwable): void
+    {
+        $this->logger->error('Failed to block UiTiD v1 consumer(s)', [
+            'integration_id' => $integrationBlocked->id->toString(),
+            'exception' => $throwable,
+        ]);
+    }
+}
