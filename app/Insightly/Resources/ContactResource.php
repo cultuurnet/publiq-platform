87c0dbd7867077a74fbb886ed065696b1ba7c5a1
Luc Wollants <lucwollants@gmail.com>
2023-03-13 09:57:58 +0100

    Remove unused import

--- app/Insightly/Resources/ContactResource.php
+++ app/Insightly/Resources/ContactResource.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Insightly\Resources;
 
 use App\Domain\Contacts\Contact;
-use App\Domain\Contacts\ContactType;
 use App\Insightly\Objects\InsightlyContacts;
 
 interface ContactResource
