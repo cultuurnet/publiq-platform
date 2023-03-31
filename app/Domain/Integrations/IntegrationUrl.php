e087a6046c2674f309b626eb470f11c84860091f
Luc Wollants <lucwollants@gmail.com>
2023-03-31 14:33:06 +0200

    Create domain object for `IntegrationUrl`

---
+++ app/Domain/Integrations/IntegrationUrl.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Integrations;
+
+use Ramsey\Uuid\UuidInterface;
+
+final class IntegrationUrl
+{
+    public function __construct(
+        public readonly UuidInterface $integrationId,
+        public readonly Environment $environment,
+        public readonly IntegrationUrlType $type,
+        public readonly string $url
+    ) {
+    }
+}
