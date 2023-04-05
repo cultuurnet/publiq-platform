010e5b6a434330ef78c9354c17ef6293008e1ce5
Luc Wollants <lucwollants@gmail.com>
2023-04-05 14:50:49 +0200

    Create new events for when an `IntegrationUrl` gets created, updated or deleted

---
+++ app/Domain/Integrations/Events/IntegrationUrlUpdated.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Integrations\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Ramsey\Uuid\UuidInterface;
+
+final class IntegrationUrlUpdated
+{
+    use Dispatchable;
+
+    public function __construct(
+        public readonly UuidInterface $id,
+        public readonly UuidInterface $integrationId,
+    ) {
+    }
+}
