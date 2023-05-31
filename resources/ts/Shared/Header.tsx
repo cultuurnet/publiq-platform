86bdef0d3eb5dc0224d91a193cf66d1aeb82efe0
simon-debruijn <simon.debruijn@hotmail.com>
2023-05-31 17:30:16 +0200

    Show active state nav items based on url

--- resources/ts/Shared/Header.tsx
+++ resources/ts/Shared/Header.tsx
@@ -1,32 +1,42 @@
 import React from "react";
 import { Heading } from "./Heading";
 import { useTranslation } from "react-i18next";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import { classNames } from "../utils/classNames";
 
 export default function Header() {
   const { t, i18n } = useTranslation();
+  const { url } = usePage();
+
+  const path = new URL(url, "http://localhost").pathname;
 
   return (
     <header className="flex items-center justify-around w-full mb-4 py-4 bg-white">
-      <a>
-        <Heading className="text-base" level={1}>
+      <Link href="/">
+        <Heading
+          className={classNames(
+            "text-base",
+            "py-1",
+            path === "/" && "border-b-2 border-b-blue"
+          )}
+          level={1}
+        >
           {t("title")}
         </Heading>
-      </a>
+      </Link>
       <div className="flex gap-8 min-w-[50%]">
-        <Link
-          href={"/integrations"}
-          className={classNames(
-            "py-1",
-            "/integrations".startsWith("/integrations") &&
-              "border-b-2 border-b-blue"
-          )}
-        >
-          {t("nav.integrations")}
-        </Link>
-        <Link href="#">{t("nav.opportunities")}</Link>
-        <Link href="#">{t("nav.support")}</Link>
+        {["integrations", "opportunities", "support"].map((pageTitle) => (
+          <Link
+            key={pageTitle}
+            href={`/${pageTitle}`}
+            className={classNames(
+              "py-1",
+              path.startsWith(`/${pageTitle}`) && "border-b-2 border-b-blue"
+            )}
+          >
+            {t(`nav.${pageTitle}`)}
+          </Link>
+        ))}
       </div>
       <div className="flex items-center gap-3">
         <div className="flex gap-2">
