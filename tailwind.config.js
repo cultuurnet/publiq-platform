7548878322a1013be9d76b79230ce29ca91227a9
vhande <karasu.hnd@gmail.com>
2023-05-31 10:50:59 +0200

    Add color to tailwingconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./resources/ts/**/*.{jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      blue: "#009fdf",
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
