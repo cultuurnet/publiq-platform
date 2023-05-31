11ae788c6a3238381b6f5416db5a2eab8434addf
vhande <karasu.hnd@gmail.com>
2023-05-31 10:51:21 +0200

    Add translation en

--- resources/ts/Shared/Header.tsx
+++ resources/ts/Shared/Header.tsx
@@ -3,10 +3,14 @@ import { Heading } from "./Heading";
 import { useTranslation } from "react-i18next";
 
 export default function Header() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
   return (
-    <header className="mt-2 mb-4">
+    <header className="mt-2 mb-4 bg-[theme(colors.blue)]">
+      <p>HELLO</p>
       <Heading level={1}>{t("title")}</Heading>
+      <button onClick={()=> i18n.changeLanguage('nl')}>NL</button>
+      <button onClick={()=> i18n.changeLanguage('en')}>EN</button>
     </header>
   );
 }
