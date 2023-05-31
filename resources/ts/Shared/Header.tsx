37d60449304a0a7b8fdaa49975a494fe540c6ae5
vhande <karasu.hnd@gmail.com>
2023-05-31 11:22:17 +0200

    Add nav menu

--- resources/ts/Shared/Header.tsx
+++ resources/ts/Shared/Header.tsx
@@ -6,11 +6,24 @@ export default function Header() {
   const { t, i18n } = useTranslation();
 
   return (
-    <header className="mt-2 mb-4 bg-[theme(colors.blue)]">
-      <p>HELLO</p>
+    <header className="flex items-center justify-around w-full mb-4 bg-[theme(colors.blue)]">
       <Heading level={1}>{t("title")}</Heading>
-      <button onClick={()=> i18n.changeLanguage('nl')}>NL</button>
-      <button onClick={()=> i18n.changeLanguage('en')}>EN</button>
+      <div className="flex gap-8 min-w-[50%]">
+        <a>{t("nav.integrations")}</a>
+        <a>{t("nav.opportunities")}</a>
+        <a>{t("nav.support")}</a>
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="flex gap-2">
+          <button onClick={() => i18n.changeLanguage("nl")}>NL</button>
+          <button onClick={() => i18n.changeLanguage("en")}>EN</button>
+        </div>
+
+        <div className="border-l pl-3">
+          <span>🧙‍♂️</span>
+          <span>Hallo, Corneel</span>
+        </div>
+      </div>
     </header>
   );
 }
