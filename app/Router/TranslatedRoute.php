9891ae8dc6344b359d5b554e0f8f9c6469effb2a
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-05 10:05:13 +0200

    Use a language to prefix the route name

--- app/Router/TranslatedRoute.php
+++ app/Router/TranslatedRoute.php
@@ -8,13 +8,18 @@ use Illuminate\Support\Facades\Route;
 
 final class TranslatedRoute
 {
-    public static function get(array $uris, \Closure|array|string|null $action = null, ?string $routeName = null): void
+    public static function getTranslatedRouteName(string $routeName, string $language): string
     {
-        foreach ($uris as $uri) {
+        return implode('.', [$language, $routeName]);
+    }
+
+    public static function get(array $languageToUri, \Closure|array|string|null $action = null, ?string $routeName = null): void
+    {
+        foreach ($languageToUri as $language => $uri) {
             $route = Route::get($uri, $action);
 
             if ($routeName !== null) {
-                $route->name($routeName);
+                $route->name(self::getTranslatedRouteName($routeName, $language));
             }
         }
     }
