63199b2015901c47f0d7f40bb371340c26dc91b3
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-06 20:41:54 +0200

    Pass className to SecondaryButton

--- resources/ts/Shared/SecondaryButton.tsx
+++ resources/ts/Shared/SecondaryButton.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link, InertiaLinkProps } from "@inertiajs/react";
+import { classNames } from "../utils/classNames";
 
 type Props = InertiaLinkProps;
 
-export const SecondaryButton = ({ children, ...props }: Props) => {
+export const SecondaryButton = ({ children, className, ...props }: Props) => {
   return (
     <Link
-      className="relative inline-flex items-center justify-center px-10 py-3 font-medium outline outline-1 outline-publiq-blue text-publiq-blue group
-      hover:bg-publiq-blue-dark hover:bg-opacity-10
-    "
+      className={classNames(
+        "relative inline-flex items-center justify-center px-10 py-3 font-medium outline outline-1 outline-publiq-blue text-publiq-blue group hover:bg-publiq-blue-dark hover:bg-opacity-10",
+        className
+      )}
       {...props}
     >
       <div className="absolute w-[50%] h-[50%] opacity-0 group-focus:animate-pulse bg-publiq-blue"></div>
