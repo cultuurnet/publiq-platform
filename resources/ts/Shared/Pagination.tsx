7d2195e1d92c52ecb1a3dee082ac1ff5f1d910ce
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-06 20:41:01 +0200

    Add Pagination component

---
+++ resources/ts/Shared/Pagination.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SecondaryButton } from "./SecondaryButton";
+import { classNames } from "../utils/classNames";
+
+const getPageFromLink = (url: string) => new URL(url).searchParams.get("page");
+
+export const Pagination = ({ links }: { links: string[] }) => {
+  const page = new URL(document.location.href).searchParams.get("page");
+  const currentPage = page ? parseInt(page) : 1;
+
+  return (
+    <div className="inline-flex">
+      {links.map((link, index) => (
+        <SecondaryButton
+          key={link}
+          href={link}
+          className={classNames(
+            currentPage - 1 === index &&
+              "bg-publiq-blue-dark hover:bg-blue-800 text-white"
+          )}
+        >
+          {getPageFromLink(link)}
+        </SecondaryButton>
+      ))}
+    </div>
+  );
+};
