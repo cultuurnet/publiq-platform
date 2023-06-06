2b741f1287b11427387ee8386f588628634bce33
vhande <78013271+vhande@users.noreply.github.com>
2023-06-06 13:29:08 +0200

    Update resources/ts/Pages/Support/Index.tsx
    Co-authored-by: Simon de Bruijn <66943525+simon-debruijn@users.noreply.github.com>

--- resources/ts/Pages/Support/Index.tsx
+++ resources/ts/Pages/Support/Index.tsx
@@ -7,15 +7,13 @@ import { useTranslation } from "react-i18next";
 const Index = () => {
   const { t } = useTranslation();
   return (
-    <>
-      <div className="flex flex-col max-xl:px-14 xl:px-60">
+      <section className="flex flex-col max-xl:px-14 xl:px-60">
         <div className="flex flex-col gap-5 py-8">
           <Heading level={1}>{t("support.title")}</Heading>
           <p>{t("support.description")}</p>
         </div>
         <SupportTypes />
-      </div>
-    </>
+      </section>
   );
 };
 
