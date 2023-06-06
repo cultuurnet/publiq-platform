c1465431c1d5fce2231a629c781e90be8a3c5958
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-06 20:40:03 +0200

    Use dark gray color

--- resources/ts/Shared/Layout.tsx
+++ resources/ts/Shared/Layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col flex-1 items-center text-publiq-gray bg-publiq-gray-light">
+    <div className="flex flex-col flex-1 items-center text-publiq-gray-dark bg-publiq-gray-light">
       <Header />
       <main className="flex flex-col items-center pb-8 w-full">{children}</main>
       <Footer />
