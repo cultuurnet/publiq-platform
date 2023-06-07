2b5c636ff4a42678f6669f28e7709979769b425f
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-07 17:42:43 +0200

    Inline return variable

--- resources/ts/hooks/useTranslateRoute.ts
+++ resources/ts/hooks/useTranslateRoute.ts
@@ -4,7 +4,5 @@ import { useTranslation } from "react-i18next";
 export const useTranslateRoute = () => {
   const { t } = useTranslation();
 
-  const translate = useCallback((path: string) => t(`pages.${path}`), [t]);
-
-  return translate;
+  return useCallback((path: string) => t(`pages.${path}`), [t]);
 };
