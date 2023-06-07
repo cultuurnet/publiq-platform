6b2d777bbcead3188a798edf4671f5a5a90cb68f
vhande <karasu.hnd@gmail.com>
2023-06-07 15:44:13 +0200

    Add HeaderMobile

---
+++ resources/ts/Shared/HeaderMobile.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import NavigationElements from "./NavigationElements";
+import { Heading } from "./Heading";
+import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+export default function HeaderMobile() {
+  const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <header className="w-full flex items-center justify-around bg-white shadow-lg z-40 md:hidden">
+      <Heading className="py-3 border-transparent border-b-4" level={3}>
+        {t("title")}
+      </Heading>
+      <div>
+        <NavigationElements
+          visible={isVisible}
+          orientation="vertical"
+          className="fixed top-[1rem] right-[1rem] left-[1rem] bottom-[1rem] bg-publiq-gray-light shadow-lg"
+          isVisible
+          setIsVisible={setIsVisible}
+        >
+          <FontAwesomeIcon
+            icon={faXmark}
+            size="lg"
+            onClick={() => setIsVisible((prev) => !prev)}
+            className="text-publiq-blue-dark"
+          />
+        </NavigationElements>
+      </div>
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={() => setIsVisible((prev) => !prev)}
+      />
+    </header>
+  );
+}
--- resources/ts/Shared/Layout.tsx
+++ resources/ts/Shared/Layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
+import HeaderMobile from "./HeaderMobile";
 import Footer from "./Footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col items-center text-publiq-gray bg-publiq-gray-light">
       <Header />
+      <HeaderMobile />
       <section className="pb-8 w-full">{children}</section>
       <Footer />
     </div>
