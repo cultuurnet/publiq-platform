41b6df360909f73f8dad2d9a2a7bde6adb383d4f
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-09 08:39:32 +0200

    Make documentation link title dynamic

--- resources/ts/Shared/IntegrationCard.tsx
+++ resources/ts/Shared/IntegrationCard.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import type { Integration } from "../Pages/Integrations/Index";
 import { IconButton } from "./IconButton";
-import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Heading } from "./Heading";
 import { Card } from "./Card";
 import { useTranslation } from "react-i18next";
 import { Link } from "./Link";
 import { router } from "@inertiajs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = Integration;
 
-export const IntegrationCard = ({ id, name, type, description }: Props) => {
+export const IntegrationCard = ({
+  id,
+  name,
+  type,
+  description,
+  status,
+}: Props) => {
   const { t } = useTranslation();
 
   const handleDeleteIntegration = () => router.delete(`/integrations/${id}`);
+
+  const handleCopyToClipboard = () => {
+    console.log(`copy ${id} to clipboard`);
+  };
 
   return (
     <Card
@@ -39,17 +50,25 @@ export const IntegrationCard = ({ id, name, type, description }: Props) => {
       <div className="flex flex-col gap-2">
         <section className="inline-flex gap-3 items-center">
           <Heading level={3}>{t("integrations.test")}</Heading>
-          <span className="bg-gray-200 p-1">{id}</span>
+          <div className="inline-flex gap-2 items-center bg-gray-200 py-2 px-3">
+            <span>{id}</span>
+            <button onClick={handleCopyToClipboard}>
+              <FontAwesomeIcon
+                icon={faCopy}
+                className="text-gray-600 h-[1.2rem]"
+              />
+            </button>
+          </div>
         </section>
         <section className="inline-flex gap-3 items-center">
           <Heading level={3}>{t("integrations.live")}</Heading>
-          <span>{t("integrations.status.not_active")}</span>
+          <span>{t(`integrations.status.${status}`)}</span>
         </section>
         <section className="inline-flex gap-3 items-center">
           <Heading level={3}>{t("integrations.documentation.title")}</Heading>
           <Link href="#">
             {t("integrations.documentation.action_title", {
-              product: "Entry API",
+              product: t(`integrations.products.${type}`),
             })}
           </Link>
         </section>
