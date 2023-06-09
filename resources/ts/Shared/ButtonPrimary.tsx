8bddf43a9da2292f48da3ae298e3e4dc0469612f
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-09 15:56:22 +0200

    Add ButtonPrimary

---
+++ resources/ts/Shared/ButtonPrimary.tsx
@@ -0,0 +1,19 @@
+import React, { ComponentProps } from "react";
+import { classNames } from "../utils/classNames";
+
+type Props = ComponentProps<"button">;
+
+export const ButtonPrimary = ({ children, className, ...props }: Props) => {
+  return (
+    <button
+      className={classNames(
+        "relative inline-flex items-center justify-center bg-publiq-blue font-medium px-10 py-3 text-white group hover:bg-publiq-blue-light",
+        className
+      )}
+      {...props}
+    >
+      <div className="absolute w-[50%] h-[50%] opacity-0 group-focus:animate-pulse bg-publiq-blue text-white"></div>
+      <div className="relative z-10">{children}</div>
+    </button>
+  );
+};
