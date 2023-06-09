4c3b1d3d24e4388978d716813dd4bf4b36939be8
simon-debruijn <simon.debruijn@hotmail.com>
2023-06-09 16:06:18 +0200

    Make confirm and cancel label configurable

--- resources/ts/Shared/QuestionDialog.tsx
+++ resources/ts/Shared/QuestionDialog.tsx
@@ -3,27 +3,37 @@ import { Dialog } from "./Dialog";
 import type { DialogProps } from "./Dialog";
 import { ButtonSecondary } from "./ButtonSecondary";
 import { ButtonPrimary } from "./ButtonPrimary";
+import { useTranslation } from "react-i18next";
 
 type Props = DialogProps & {
   question: string;
+  confirmLabel?: string;
+  cancelLabel?: string;
   onConfirm: () => void;
   onCancel: () => void;
 };
 
 export const QuestionDialog = ({
   question,
+  confirmLabel,
+  cancelLabel,
   onConfirm,
   onCancel,
   children,
   ...props
 }: Props) => {
+  const { t } = useTranslation();
+
+  const confirm = confirmLabel ?? t("dialog.confirm");
+  const cancel = cancelLabel ?? t("dialog.cancel");
+
   return (
     <Dialog {...props}>
       <p className="flex flex-1">{question}</p>
       <div className="flex flex-1">{children}</div>
       <div className="self-end w-full inline-flex gap-3 justify-end border-t-publiq-gray-medium">
-        <ButtonSecondary onClick={onCancel}>No</ButtonSecondary>
-        <ButtonPrimary onClick={onConfirm}>Yes</ButtonPrimary>
+        <ButtonSecondary onClick={onCancel}>{cancel}</ButtonSecondary>
+        <ButtonPrimary onClick={onConfirm}>{confirm}</ButtonPrimary>
       </div>
     </Dialog>
   );
