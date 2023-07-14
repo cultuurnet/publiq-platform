1567e713cb12f92cffcb714f4f0c4b26100be36c
vhande <karasu.hnd@gmail.com>
2023-07-14 16:11:30 +0200

    Add show organisation

--- resources/ts/Components/Integrations/Detail/BillingInfo.tsx
+++ resources/ts/Components/Integrations/Detail/BillingInfo.tsx
@@ -4,9 +4,12 @@ import { FormElement } from "../../FormElement";
 import { Input } from "../../Input";
 import { Button } from "../../Button";
 import { FormDropdown } from "../../FormDropdown";
+import { Integration } from "../../../Pages/Integrations/Index";
 import { useTranslation } from "react-i18next";
 
-export const BillingInfo = () => {
+type Props = Integration;
+
+export const BillingInfo = ({ organisation }: Props) => {
   const { t } = useTranslation();
   const [isDisabled, setIsDisabled] = useState(true);
 
@@ -25,73 +28,78 @@ export const BillingInfo = () => {
           </Heading>
           <p>Plus $250</p>
         </div>
-        <div className="grid md:w-[50%] ">
-          <FormElement
-            label={`${t("details.billing_info.name")}`}
-            component={
-              <Input
-                type="text"
-                name="name"
-                defaultValue={`${t("details.billing_info.name")}`}
-                disabled={isDisabled}
-              />
-            }
-          />
-        </div>
-        <div className="grid grid-cols-3 gap-3 max-md:grid-cols-1">
-          <FormElement
-            label={`${t("details.billing_info.address.street")}`}
-            component={
-              <Input
-                type="text"
-                name="street"
-                defaultValue={`${t("details.billing_info.address.street")}`}
-                disabled={isDisabled}
-              />
-            }
-          />
-          <FormElement
-            label={`${t("details.billing_info.address.postcode")}`}
-            component={
-              <Input
-                type="text"
-                name="postcode"
-                defaultValue={`${t("details.billing_info.address.postcode")}`}
-                disabled={isDisabled}
-              />
-            }
-          />
 
-          <FormElement
-            label={`${t("details.billing_info.address.city")}`}
-            component={
-              <Input
-                type="text"
-                name="firstNameTechnicalContact"
-                defaultValue={`${t("details.billing_info.address.city")}`}
-                disabled={isDisabled}
+        {organisation && (
+          <>
+            <div className="grid md:w-[50%] ">
+              <FormElement
+                label={`${t("details.billing_info.name")}`}
+                component={
+                  <Input
+                    type="text"
+                    name="name"
+                    defaultValue={organisation.name}
+                    disabled={isDisabled}
+                  />
+                }
               />
-            }
-          />
-        </div>
-        <div className="grid md:w-[50%]">
-          <FormElement
-            label={`${t("details.billing_info.vat")}`}
-            component={
-              <Input
-                type="text"
-                name="firstNameTechnicalContact"
-                defaultValue={`${t("details.billing_info.vat")}`}
-                disabled={isDisabled}
+            </div>
+            <div className="grid grid-cols-3 gap-3 max-md:grid-cols-1">
+              <FormElement
+                label={`${t("details.billing_info.address.street")}`}
+                component={
+                  <Input
+                    type="text"
+                    name="street"
+                    defaultValue={organisation.address.street}
+                    disabled={isDisabled}
+                  />
+                }
               />
-            }
-          />
-        </div>
-        <div className="flex flex-col gap-2 items-center">
-          <Button onClick={() => setIsDisabled(true)}>
-            {t("details.save")}
-          </Button>
-        </div>
+              <FormElement
+                label={`${t("details.billing_info.address.postcode")}`}
+                component={
+                  <Input
+                    type="text"
+                    name="postcode"
+                    defaultValue={organisation.address.zip}
+                    disabled={isDisabled}
+                  />
+                }
+              />
+
+              <FormElement
+                label={`${t("details.billing_info.address.city")}`}
+                component={
+                  <Input
+                    type="text"
+                    name="firstNameTechnicalContact"
+                    defaultValue={organisation.address.city}
+                    disabled={isDisabled}
+                  />
+                }
+              />
+            </div>
+            <div className="grid md:w-[50%]">
+              <FormElement
+                label={`${t("details.billing_info.vat")}`}
+                component={
+                  <Input
+                    type="text"
+                    name="firstNameTechnicalContact"
+                    defaultValue={organisation.vat}
+                    disabled={isDisabled}
+                  />
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-2 items-center">
+              <Button onClick={() => setIsDisabled(true)}>
+                {t("details.save")}
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </FormDropdown>
   );
