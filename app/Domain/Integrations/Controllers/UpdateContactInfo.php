3018e2b3ec8896611bf6ab20b47231ad50c1e4c5
simon-debruijn <simon.debruijn@hotmail.com>
2023-07-17 17:34:21 +0200

    Fix validation errors

--- app/Domain/Integrations/Controllers/UpdateContactInfo.php
+++ app/Domain/Integrations/Controllers/UpdateContactInfo.php
@@ -15,38 +15,33 @@ final class UpdateContactInfo extends FormRequest
     public function rules(): array
     {
         // TODO: Check how to fix empty string values failing
-
         return [
-            'functional' => [
-                'id' => ['required', 'string'],
-                'integrationId' => ['required', 'string'],
-                'email' => ['required', 'string', 'email', 'min:2', 'max:255'],
-                'type' => ['required', 'string', 'min:2', 'max:255'],
-                'firstName' => ['required', 'string', 'min:2', 'max:255'],
-                'lastName' => ['required', 'string', 'min:2', 'max:255'],
-                'changed' => ['required', 'boolean'],
-            ],
-            'technical' => [
-                'id' => ['required', 'string'],
-                'integrationId' => ['required', 'string'],
-                'email' => ['required', 'string', 'email', 'min:2', 'max:255'],
-                'type' => ['required', 'string', 'min:2', 'max:255'],
-                'firstName' => ['required', 'string', 'min:2', 'max:255'],
-                'lastName' => ['required', 'string', 'min:2', 'max:255'],
-                'changed' => ['required', 'boolean'],
-            ],
-            'contributors' => ['required', 'array'],
-            'contributors.*' => Rule::forEach(function () {
-                return [
-                    'id' => ['required', 'string'],
-                    'integrationId' => ['required', 'string'],
-                    'email' => ['required', 'string', 'email', 'min:2', 'max:255'],
-                    'type' => ['required', 'string', 'min:2', 'max:255'],
-                    'firstName' => ['required', 'string', 'min:2', 'max:255'],
-                    'lastName' => ['required', 'string', 'min:2', 'max:255'],
-                    'changed' => ['required', 'boolean'],
-                ];
-            }),
+                'functional.id' => ['required', 'string'],
+                'functional.integrationId' => ['required', 'string'],
+                'functional.email' => ['required', 'string', 'email', 'min:2', 'max:255'],
+                'functional.type' => ['required', 'string', 'min:2', 'max:255'],
+                'functional.firstName' => ['required', 'string', 'min:2', 'max:255'],
+                'functional.lastName' => ['required', 'string', 'min:2', 'max:255'],
+                'functional.changed' => ['required', 'boolean'],
+                'technical.id' => ['required', 'string'],
+                'technical.integrationId' => ['required', 'string'],
+                'technical.email' => ['required', 'string', 'email', 'min:2', 'max:255'],
+                'technical.type' => ['required', 'string', 'min:2', 'max:255'],
+                'technical.firstName' => ['required', 'string', 'min:2', 'max:255'],
+                'technical.lastName' => ['required', 'string', 'min:2', 'max:255'],
+                'technical.changed' => ['required', 'boolean'],
+                'contributors' => ['required', 'array'],
+                'contributors.*' => Rule::forEach(function () {
+                    return [
+                        'id' => ['required', 'string'],
+                        'integrationId' => ['required', 'string'],
+                        'email' => ['required', 'string', 'email', 'min:2', 'max:255'],
+                        'type' => ['required', 'string', 'min:2', 'max:255'],
+                        'firstName' => ['required', 'string', 'min:2', 'max:255'],
+                        'lastName' => ['required', 'string', 'min:2', 'max:255'],
+                        'changed' => ['required', 'boolean'],
+                    ];
+                }),
     ];
     }
 }
