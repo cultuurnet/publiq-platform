f9b873f2d9992ca5683707eebdd598353b826e3c
simon-debruijn <simon.debruijn@hotmail.com>
2023-07-18 16:07:09 +0200

    Use billing info in frontend

--- resources/ts/Components/Integrations/Detail/BillingInfo.tsx
+++ resources/ts/Components/Integrations/Detail/BillingInfo.tsx
@@ -6,12 +6,21 @@ import { Button } from "../../Button";
 import { FormDropdown } from "../../FormDropdown";
 import { Integration } from "../../../Pages/Integrations/Index";
 import { useTranslation } from "react-i18next";
+import { useForm } from "@inertiajs/react";
 
 type Props = Integration;
 
-export const BillingInfo = ({ organisation }: Props) => {
+export const BillingInfo = ({ id, organisation, subscription }: Props) => {
   const { t } = useTranslation();
   const [isDisabled, setIsDisabled] = useState(true);
+
+  const initialFormValues = {
+    organisation,
+  };
+
+  const { data, setData, patch, errors: err } = useForm(initialFormValues);
+
+  const errors = err as Record<string, string | undefined>;
 
   return (
     <FormDropdown
@@ -26,7 +35,11 @@ export const BillingInfo = ({ organisation }: Props) => {
           <Heading level={5} className="font-semibold w50">
             {t("details.billing_info.subscription")}
           </Heading>
-          <p>Plus $250</p>
+          <p>
+            {subscription.category}{" "}
+            {subscription.currency === "EUR" ? "€" : subscription.currency}{" "}
+            {subscription.fee / 100}
+          </p>
         </div>
 
         {organisation && (
@@ -34,11 +47,20 @@ export const BillingInfo = ({ organisation }: Props) => {
             <div className="grid md:w-[50%] ">
               <FormElement
                 label={`${t("details.billing_info.name")}`}
+                error={errors["organisation.name"]}
                 component={
                   <Input
                     type="text"
-                    name="name"
-                    defaultValue={organisation.name}
+                    name="organisation.name"
+                    value={data.organisation?.name}
+                    onChange={(e) =>
+                      setData("organisation", {
+                        // We know organisation exists
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        ...data.organisation!,
+                        name: e.target.value,
+                      })
+                    }
                     disabled={isDisabled}
                   />
                 }
@@ -47,22 +69,48 @@ export const BillingInfo = ({ organisation }: Props) => {
             <div className="grid grid-cols-3 gap-3 max-md:grid-cols-1">
               <FormElement
                 label={`${t("details.billing_info.address.street")}`}
+                error={errors["organisation.address.street"]}
                 component={
                   <Input
                     type="text"
-                    name="street"
-                    defaultValue={organisation.address.street}
+                    name="organisation.address.street"
+                    value={data.organisation?.address.street}
+                    onChange={(e) =>
+                      setData("organisation", {
+                        // We know organisation exists
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        ...data.organisation!,
+                        address: {
+                          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          ...data.organisation!.address,
+                          street: e.target.value,
+                        },
+                      })
+                    }
                     disabled={isDisabled}
                   />
                 }
               />
               <FormElement
                 label={`${t("details.billing_info.address.postcode")}`}
+                error={errors["organisation.address.zip"]}
                 component={
                   <Input
                     type="text"
-                    name="postcode"
-                    defaultValue={organisation.address.zip}
+                    name="organisation.address.zip"
+                    value={data.organisation?.address.zip}
+                    onChange={(e) =>
+                      setData("organisation", {
+                        // We know organisation exists
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        ...data.organisation!,
+                        address: {
+                          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          ...data.organisation!.address,
+                          zip: e.target.value,
+                        },
+                      })
+                    }
                     disabled={isDisabled}
                   />
                 }
@@ -70,11 +118,24 @@ export const BillingInfo = ({ organisation }: Props) => {
 
               <FormElement
                 label={`${t("details.billing_info.address.city")}`}
+                error={errors["organisation.address.city"]}
                 component={
                   <Input
                     type="text"
-                    name="firstNameTechnicalContact"
-                    defaultValue={organisation.address.city}
+                    name="organisation.address.city"
+                    value={data.organisation?.address.city}
+                    onChange={(e) =>
+                      setData("organisation", {
+                        // We know organisation exists
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        ...data.organisation!,
+                        address: {
+                          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                          ...data.organisation!.address,
+                          city: e.target.value,
+                        },
+                      })
+                    }
                     disabled={isDisabled}
                   />
                 }
@@ -83,18 +144,36 @@ export const BillingInfo = ({ organisation }: Props) => {
             <div className="grid md:w-[50%]">
               <FormElement
                 label={`${t("details.billing_info.vat")}`}
+                error={errors["organisation.vat"]}
                 component={
                   <Input
                     type="text"
-                    name="firstNameTechnicalContact"
-                    defaultValue={organisation.vat}
+                    name="organisation.vat"
+                    value={data.organisation?.vat}
+                    onChange={(e) =>
+                      setData("organisation", {
+                        // We know organisation exists
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        ...data.organisation!,
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        vat: e.target.value,
+                      })
+                    }
                     disabled={isDisabled}
                   />
                 }
               />
             </div>
             <div className="flex flex-col gap-2 items-center">
-              <Button onClick={() => setIsDisabled(true)}>
+              <Button
+                onClick={() => {
+                  setIsDisabled(true);
+
+                  patch(`/integrations/${id}/billing`, {
+                    preserveScroll: true,
+                  });
+                }}
+              >
                 {t("details.save")}
               </Button>
             </div>
