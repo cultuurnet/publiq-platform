06015783ae4e6004a0e51377173f5bc07e61ced8
Koen Eelen <koen.eelenpubliq.be>
2023-09-13 17:11:54 +0200

    forgot CachedUitidv1status class & test

---
+++ app/UiTiDv1/CachedUiTiDv1Status.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UiTiDv1;
+
+final class CachedUiTiDv1Status
+{
+    private array $statuses = [];
+
+    public function __construct(private readonly UiTiDv1ClusterSDK $sdk)
+    {
+    }
+
+    public function findStatusOnConsumer(UiTiDv1Consumer $consumer): UiTiDv1ConsumerStatus
+    {
+        if(! isset($this->statuses[$consumer->consumerKey])) {
+            $this->statuses[$consumer->consumerKey] = $this->sdk->fetchStatusOfConsumer($consumer);
+        }
+
+        return $this->statuses[$consumer->consumerKey];
+    }
+}
