69a9ea9d25fc0a3fea96d9757af1a753818c5057
Luc Wollants <lucwollants@gmail.com>
2023-09-27 12:58:11 +0200

    Use Facade mocking

--- tests/Domain/Integrations/Mappers/StoreIntegrationMapperTest.php
+++ tests/Domain/Integrations/Mappers/StoreIntegrationMapperTest.php
@@ -5,25 +5,24 @@ declare(strict_types=1);
 namespace Tests\Domain\Integrations\Mappers;
 
 use App\Domain\Auth\CurrentUser;
+use App\Domain\Auth\Models\UserModel;
 use App\Domain\Integrations\FormRequests\StoreIntegrationRequest;
 use App\Domain\Integrations\Mappers\StoreIntegrationMapper;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class StoreIntegrationMapperTest extends TestCase
 {
-
-    public function test_it_creates_an_integration_from_request() {
+    public function test_it_creates_an_integration_from_request(): void
+    {
         $request = new StoreIntegrationRequest();
 
-        // This causes problem
-        $auth = $this->createMock(Auth::class);
+        Auth::shouldReceive('user')
+            ->once()
+            ->andReturn(UserModel::createSystemUser());
 
-        // mocking only currentUser isn't possible since it is a final class
-
-        // I need to mock currentUser
-        $currentUser = new CurrentUser($auth);
+        $currentUser = new CurrentUser(App::get(Auth::class));
 
         $integration = StoreIntegrationMapper::map($request, $currentUser);
     }
