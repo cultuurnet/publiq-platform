719fd07f1de656a7624feb42c7e9945a1b7da4a5
vhande <hande.vanhove@publiq.be>
2023-10-06 11:51:41 +0200

    Delete prop actions

--- resources/ts/Components/FormDropdown.tsx
+++ resources/ts/Components/FormDropdown.tsx
@@ -1,22 +1,20 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { Heading } from "./Heading";
 
 type Props = {
   children: React.ReactNode;
   title: string;
-  actions: ReactNode;
 };
 
-export const FormDropdown = ({ title, children, actions }: Props) => {
+export const FormDropdown = ({ title, children }: Props) => {
   return (
-    <div className="flex flex-col gap-4 max-md:px-5 px-10 py-5">
+    <div className="w-full flex max-lg:flex-col items-start max-lg:gap-4 lg:gap-48 max-md:px-5 px-10 py-5">
       <div className="flex gap-2 items-center">
-        <Heading className="font-semibold" level={2}>
+        <Heading className="font-semibold" level={3}>
           {title}
         </Heading>
-        {actions}
       </div>
-      <div className="flex flex-col gap-6 py-4">{children}</div>
+      <div className="w-full flex flex-col gap-6">{children}</div>
     </div>
   );
 };
