39d54f3ace20011aea5f48339a02124d2bc0e1d3
vhande <hande.vanhove@publiq.be>
2023-10-10 14:56:58 +0200

    Change overflow-x

--- resources/ts/Components/ContactsTableDesktop.tsx
+++ resources/ts/Components/ContactsTableDesktop.tsx
@@ -5,7 +5,7 @@ type Props = ComponentProps<"div">;
 
 export const ContactsTableDesktop = ({ className, children }: Props) => {
   return (
-    <div className={classNames("relative overflow-x-scroll", className)}>
+    <div className={classNames("relative overflow-x-auto", className)}>
       <table className="relative w-full text-left border border-publiq-gray-light text-gray-500">
         {children}
       </table>
