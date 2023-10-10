750d6d31922d0a3ab06e91824e08e52c866e3670
vhande <hande.vanhove@publiq.be>
2023-10-10 11:28:30 +0200

    Update Detail.tsx

--- resources/ts/Pages/Integrations/Detail.tsx
+++ resources/ts/Pages/Integrations/Detail.tsx
@@ -1,17 +1,16 @@
 import React, { ReactNode, useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import { Page } from "../../Components/Page";
-import { Heading } from "../../Components/Heading";
 import { Integration } from "./Index";
-import { BasicInfo } from "../../Components/Integrations/Detail/BasicInfo";
 import { ContactInfo } from "../../Components/Integrations/Detail/ContactInfo";
 import { BillingInfo } from "../../Components/Integrations/Detail/BillingInfo";
-import { IntegrationInfo } from "../../Components/Integrations/Detail/IntegrationInfo";
+import { Credentials } from "../../Components/Integrations/Detail/Credentials";
 import { IntegrationSettings } from "../../Components/Integrations/Detail/IntegrationSettings";
 import { useTranslation } from "react-i18next";
 import { router } from "@inertiajs/react";
 import { Tabs } from "../../Components/Tabs";
-import { MenageIntegration } from "../../Components/Integrations/Detail/MenageIntegration";
+import { DeleteIntegration } from "../../Components/Integrations/Detail/DeleteIntegration";
+import { Card } from "../../Components/Card";
 
 type Props = { integration: Integration };
 
@@ -21,7 +20,7 @@ const Detail = ({ integration }: Props) => {
   const [isMobile, setIsMobile] = useState(false);
 
   const url = new URL(document.location.href);
-  const activeTab = url.searchParams.get("tab") ?? "basic_info";
+  const activeTab = url.searchParams.get("tab") ?? "settings";
 
   const handleResize = () => {
     setIsMobile(window.innerWidth < 768);
@@ -39,54 +38,40 @@ const Detail = ({ integration }: Props) => {
 
   return (
     <Page>
-      <div className="w-full flex flex-col gap-5">
-        <div className="flex justify-between gap-5">
-          <div className="flex gap-3 items-center align-middle">
-            <Heading className="font-semibold" level={2}>
-              {integration.name}
-            </Heading>
-            <span className="bg-publiq-blue-dark text-white text-xs font-medium  mr-2 px-2.5 py-0.5 rounded">
-              {integration.type}
-            </span>
-          </div>
+      <Card title={integration.name} badge={integration.type} border>
+        <div className="w-full flex flex-col gap-5">
+          <Tabs active={activeTab} onChange={changeTabInUrl}>
+            <Tabs.Item
+              type="settings"
+              label={t("details.integration_settings.title")}
+            >
+              <IntegrationSettings
+                integration={integration}
+                {...integration}
+                isMobile={isMobile}
+              />
+            </Tabs.Item>
+            <Tabs.Item
+              type="credentials"
+              label={t("details.credentials.title")}
+            >
+              <Credentials {...integration} />
+            </Tabs.Item>
+            <Tabs.Item type="contacts" label={t("details.contact_info.title")}>
+              <ContactInfo {...integration} isMobile={isMobile} />
+            </Tabs.Item>
+            <Tabs.Item type="billing" label={t("details.billing_info.title")}>
+              <BillingInfo {...integration} />
+            </Tabs.Item>
+            <Tabs.Item
+              type="delete"
+              label={t("details.delete_integration.title")}
+            >
+              <DeleteIntegration {...integration} />
+            </Tabs.Item>
+          </Tabs>
         </div>
-
-        <Tabs active={activeTab} onChange={changeTabInUrl}>
-          <Tabs.Item type="basic_info" label={t("details.basic_info.title")}>
-            <BasicInfo integration={integration} isMobile={isMobile} />
-          </Tabs.Item>
-          <Tabs.Item
-            type="integration_info"
-            label={t("details.integration_info.title")}
-          >
-            <IntegrationInfo {...integration} />
-          </Tabs.Item>
-          <Tabs.Item
-            type="integration_settings"
-            label={t("details.integration_settings.title")}
-          >
-            <IntegrationSettings {...integration} isMobile={isMobile} />
-          </Tabs.Item>
-          <Tabs.Item
-            type="contact_info"
-            label={t("details.contact_info.title")}
-          >
-            <ContactInfo {...integration} isMobile={isMobile} />
-          </Tabs.Item>
-          <Tabs.Item
-            type="billing_info"
-            label={t("details.billing_info.title")}
-          >
-            <BillingInfo {...integration} />
-          </Tabs.Item>
-          <Tabs.Item
-            type="menage_account"
-            label={t("details.menage_account.title")}
-          >
-            <MenageIntegration {...integration} />
-          </Tabs.Item>
-        </Tabs>
-      </div>
+      </Card>
     </Page>
   );
 };
