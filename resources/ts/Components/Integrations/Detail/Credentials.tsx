85e5c3b0fd048a0b1d367b60cecd13515ac63e6d
vhande <hande.vanhove@publiq.be>
2023-10-11 15:54:21 +0200

    Update Credentials

--- resources/ts/Components/Integrations/Detail/Credentials.tsx
+++ resources/ts/Components/Integrations/Detail/Credentials.tsx
@@ -5,6 +5,7 @@ import type { Integration } from "../../../Pages/Integrations/Index";
 import { StatusLight } from "../../StatusLight";
 import { ButtonPrimary } from "../../ButtonPrimary";
 import { Link } from "../../Link";
+import { TabLayout } from "../../TabLayout";
 
 type Props = Integration;
 
@@ -15,28 +16,27 @@ export const Credentials = ({ id, status }: Props) => {
   const v2 = true;
 
   return (
-    <div className="flex flex-col gap-4 max-md:px-5 px-10 py-5 ">
-      <Heading className="font-semibold" level={2}>
-        {t("details.credentials.title")}
-      </Heading>
-      <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4 py-4">
+    <TabLayout>
+      <div>
         {v2 && (
-          <div className="flex flex-col gap-6 pt-4">
+          <div className="w-full max-lg:flex max-lg:flex-col lg:grid lg:grid-cols-3 gap-6 border-b pb-10 border-gray-300">
             <Heading className="font-semibold" level={3}>
               {t("details.credentials.uitid_v1")}
             </Heading>
-            <div className="flex">
-              <Heading className="font-semibold flex min-w-[5rem]" level={4}>
-                {t("details.credentials.test")}
-              </Heading>
-              <p>{id}</p>
-            </div>
-            <div className="flex">
-              <Heading className="font-semibold min-w-[5rem]" level={4}>
-                {t("details.credentials.live")}
-              </Heading>
-              <div className="flex gap-1 align-center">
-                <StatusLight status={status} />
+            <div className="flex flex-col gap-6 min-w-[40rem]">
+              <div>
+                <Heading className="font-semibold flex" level={4}>
+                  {t("details.credentials.test")}
+                </Heading>
+                <p>{id}</p>
+              </div>
+              <div>
+                <Heading className="font-semibold" level={4}>
+                  {t("details.credentials.live")}
+                </Heading>
+                <div className="flex gap-1 align-center">
+                  <StatusLight status={status} />
+                </div>
               </div>
             </div>
             {status === "pending_approval_payment" && (
@@ -59,50 +59,50 @@ export const Credentials = ({ id, status }: Props) => {
             )}
           </div>
         )}
-        <div className="flex flex-col gap-6 pt-4">
-          <Heading className="font-semibold" level={3}>
-            {t("details.credentials.uitid_v2")}
-          </Heading>
-          {v1 ? (
-            <div className="flex flex-col gap-4">
-              <p>{t("details.credentials.uitid_alert")}</p>
-              <ButtonPrimary className="self-start">
-                {t("details.credentials.action_uitid")}
-              </ButtonPrimary>
-            </div>
-          ) : (
-            <div>
-              <div className="flex">
-                <Heading className="font-semibold flex min-w-[5rem]" level={4}>
-                  {t("details.credentials.test")}
-                </Heading>
-                <p>{id}</p>
-              </div>
-              <div className="flex">
-                <Heading className="font-semibold min-w-[5rem]" level={4}>
-                  {t("details.credentials.live")}
-                </Heading>
-                <div className="flex gap-1 align-center">
-                  <StatusLight status={status} />
-                  <Heading level={5}>
-                    {t(`integrations.status.${status}`)}
-                  </Heading>
-                </div>
-              </div>
-              {status === "draft" && (
-                <div className="flex flex-col gap-4">
-                  <p className="ml-[5rem] max-w-[25rem]">
-                    {t("details.credentials.status_alert")}
-                  </p>
-                  <ButtonPrimary className="ml-[5rem] self-start">
-                    {t("details.credentials.action_status")}
-                  </ButtonPrimary>
-                </div>
-              )}
-            </div>
-          )}
-        </div>
       </div>
-    </div>
+      <div className="w-full max-lg:flex max-lg:flex-col lg:grid lg:grid-cols-3 gap-6">
+        <Heading className="font-semibold" level={3}>
+          {t("details.credentials.uitid_v2")}
+        </Heading>
+        {v1 ? (
+          <div className="flex flex-col gap-4">
+            <p>{t("details.credentials.uitid_alert")}</p>
+            <ButtonPrimary className="self-start">
+              {t("details.credentials.action_uitid")}
+            </ButtonPrimary>
+          </div>
+        ) : (
+          <div>
+            <div className="flex">
+              <Heading className="font-semibold flex min-w-[5rem]" level={4}>
+                {t("details.credentials.test")}
+              </Heading>
+              <p>{id}</p>
+            </div>
+            <div className="flex">
+              <Heading className="font-semibold min-w-[5rem]" level={4}>
+                {t("details.credentials.live")}
+              </Heading>
+              <div className="flex gap-1 align-center">
+                <StatusLight status={status} />
+                <Heading level={5}>
+                  {t(`integrations.status.${status}`)}
+                </Heading>
+              </div>
+            </div>
+            {status === "draft" && (
+              <div className="flex flex-col gap-4">
+                <p className="ml-[5rem] max-w-[25rem]">
+                  {t("details.credentials.status_alert")}
+                </p>
+                <ButtonPrimary className="ml-[5rem] self-start">
+                  {t("details.credentials.action_status")}
+                </ButtonPrimary>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </TabLayout>
   );
 };
