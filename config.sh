813a140523433463b1732ed6ad899b2a4757d133
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-10-16 17:15:14 +0200

    take care of hidden env in shellscript

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ DIR="../appconfig/files/platform/docker/"
 if [ -d "$DIR" ]; then
   cp -R "$DIR"/* .
   # needed because it is hidden
-  cp "$DIR"/.env .
+  mv ./env ./.env
 else
   echo "Error: missing appconfig see docker.md prerequisites to fix this."
   exit 1
