0cef8783e82b151e81b02f8fd65fb8d7cd355350
vhande <hande.vanhove@publiq.be>
2023-10-17 14:06:55 +0200

    Fix stan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Router\TranslatedRoute;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
+use Inertia\Response;
 
 final class HomeController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): Response|RedirectResponse
     {
         if (!Auth::check()) {
             return Inertia::render('Index');
