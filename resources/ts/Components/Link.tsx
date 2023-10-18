5d00424fe1b3862a145e8f5d6706049c7ac677e7
vhande <hande.vanhove@publiq.be>
2023-10-18 09:50:31 +0200

    Use <a> tag for external links

--- resources/ts/Components/Link.tsx
+++ resources/ts/Components/Link.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 import { Link as InertiaLink, InertiaLinkProps } from "@inertiajs/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { classNames } from "../utils/classNames";
-type Props = InertiaLinkProps;
+type Props = InertiaLinkProps & ComponentProps<"a">;
 
 export const Link = ({ children, href, className, ...props }: Props) => {
   const isExternal = !href.startsWith("/") && !href.startsWith("#");
 
   return (
-    <InertiaLink
-      className={classNames(
-        "text-publiq-blue inline-flex gap-2 items-center",
-        className
+    <>
+      {isExternal ? (
+        <a
+          className={classNames(
+            "text-publiq-blue inline-flex gap-2 items-center",
+            className
+          )}
+          href={href}
+          target="_blank"
+          rel="noreferrer"
+          {...props}
+        >
+          <span className="hover:underline">{children}</span>
+          <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+        </a>
+      ) : (
+        <InertiaLink
+          className={classNames(
+            "text-publiq-blue inline-flex gap-2 items-center",
+            className
+          )}
+          href={href}
+          {...props}
+        >
+          <span className="hover:underline">{children}</span>
+        </InertiaLink>
       )}
-      href={href}
-      {...props}
-    >
-      <span className="hover:underline">{children}</span>
-      {isExternal && <FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
-    </InertiaLink>
+    </>
   );
 };
