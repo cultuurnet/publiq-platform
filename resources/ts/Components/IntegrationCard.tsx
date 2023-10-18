c3156054485685984c656641655b0a0f02d5753d
vhande <hande.vanhove@publiq.be>
2023-10-18 10:05:24 +0200

    Revert commit
    This reverts commit b480efc0b536087e37595d31dc515e03d159c082.

--- resources/ts/Components/IntegrationCard.tsx
+++ resources/ts/Components/IntegrationCard.tsx
@@ -29,18 +29,6 @@ export const IntegrationCard = ({ id, name, type, status, onEdit }: Props) => {
     }, 1000);
   }
 
-  const getProductUrl = () => {
-    if (type === "entry-api") {
-      return "/uitdatabank/entry-api%2Fintroduction";
-    }
-    if (type === "search-api") {
-      return "/uitdatabank/search-api%2Fintroduction";
-    }
-    if (type === "widgets") {
-      return "/widgets/aan-de-slag";
-    }
-  };
-
   return (
     <Card
       title={name}
@@ -91,15 +79,6 @@ export const IntegrationCard = ({ id, name, type, status, onEdit }: Props) => {
               product: t(`integrations.products.${type}`),
             })}
           </Link>
-            <Link
-              href={t("integrations.documentation.action_url", {
-                product: getProductUrl(),
-              })}
-            >
-              {t("integrations.documentation.action_title", {
-                product: t(`integrations.products.${type}`),
-              })}
-            </Link>
         </section>
       </div>
     </Card>
