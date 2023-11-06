e5e360358f74819b601b64c243c9d4b242980cfb
Luc Wollants <lucwollants@gmail.com>
2023-11-06 15:10:05 +0100

    Create client to consume Project Aanvraag API

---
+++ app/ProjectAanvraag/ProjectAanvraagClient.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ProjectAanvraag;
+
+use App\Json;
+use App\ProjectAanvraag\Requests\CreateWidgetRequest;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Client\ClientInterface;
+
+final readonly class ProjectAanvraagClient
+{
+    public function __construct(private ClientInterface $httpClient)
+    {
+    }
+
+    public function createWidget(CreateWidgetRequest $createWidgetRequest): void
+    {
+        $request = new Request(
+            'POST',
+            'projects',
+            [],
+            Json::encode([
+                'userId' => $createWidgetRequest->userId,
+                'name' => $createWidgetRequest->name,
+                'summary' => $createWidgetRequest->summary,
+                'groupId' => $createWidgetRequest->groupId,
+                'testApiKeySapi3' => $createWidgetRequest->testApiKeySapi3,
+                'liveApiKeySapi3' => $createWidgetRequest->liveApiKeySapi3,
+            ])
+        );
+
+        $this->httpClient->sendRequest($request);
+    }
+}
