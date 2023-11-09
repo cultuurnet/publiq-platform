0c65c86c8da4cc3ca8b2ec75373d9edd821b1471
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-09 15:38:48 +0100

    Integrate OpenWidgetBeheer Action in IntegrationUI

--- app/Nova/Resources/Integration.php
+++ app/Nova/Resources/Integration.php
@@ -13,6 +13,7 @@ use App\Domain\Organizations\Repositories\OrganizationRepository;
 use App\Nova\Actions\ActivateIntegrationWithCoupon;
 use App\Nova\Actions\ActivateIntegrationWithOrganization;
 use App\Nova\Actions\BlockIntegration;
+use App\Nova\Actions\OpenWidgetBeheer;
 use App\Nova\Resource;
 use Illuminate\Support\Facades\App;
 use Laravel\Nova\Fields\BelongsTo;
@@ -176,6 +177,18 @@ final class Integration extends Resource
 
                     return $model->status === IntegrationStatus::Draft->value;
                 }),
+
+            (new OpenWidgetBeheer())
+                ->showOnDetail()
+                ->showInline()
+                ->withoutConfirmation()
+                ->canRun(function ($request, $model) {
+                    if ($request instanceof ActionRequest) {
+                        return true;
+                    }
+                    return $model->type === IntegrationType::Widgets->value;
+                })
+                ->standalone(),
 
             (new BlockIntegration())
                 ->showOnDetail()
