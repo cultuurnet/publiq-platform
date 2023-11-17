99bd16e0be90e096316a348d385f74c6570d3e77
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-17 10:21:24 +0100

    return jsonresponse instead of string

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,18 +4,27 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 final class ApiController extends Controller
 {
-    public function validateUser(Request $request): string
+    public function validateUser(Request $request): JsonResponse
     {
         $idToken = $request->get('idToken');
         if ($idToken ===  $request->session()->get('id_token')) {
-            return $idToken;
+            return new JsonResponse([
+                'user' => $idToken,
+                'id' => 'id',
+                'token' => 'token',
+                'secret' => 'secret',
+            ]);
+
         }
 
-        return 'false';
+        return new JsonResponse([
+            'user' => 'unknown'
+        ]);
     }
 
 }
