6fa951473b418a56e68213b2a71fc02d6265d9d5
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-24 10:55:06 +0100

    use platform id & let projectaanvraag handle it

--- app/Nova/Actions/OpenWidgetBeheer.php
+++ app/Nova/Actions/OpenWidgetBeheer.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace App\Nova\Actions;
 
+use App\Domain\Integrations\Models\IntegrationModel;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Session;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Actions\ActionResponse;
+use Laravel\Nova\Fields\ActionFields;
 
 final class OpenWidgetBeheer extends Action
 {
-    public function handle(): ActionResponse|Action
+    public function handle(ActionFields $fields, Collection $integrations): ActionResponse|Action
     {
+        /** @var IntegrationModel $integration */
+        $integration = $integrations->first();
         $idToken = Session::get('id_token');
-        return Action::redirect(config('project_aanvraag.base_uri') . 'project/1/widget/?idToken=' . $idToken);
+        return Action::redirect(config('project_aanvraag.base_uri') . 'project/' . $integration->id . '/widget/?idToken=' . $idToken);
     }
 }
