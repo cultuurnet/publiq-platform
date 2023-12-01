0657b5bf92341fc3fd87a9a6afd09e5ce6a03ab3
Luc Wollants <lucwollants@gmail.com>
2023-12-01 09:37:34 +0100

    Fix dependency injection

--- app/Domain/Auth/AuthServiceProvider.php
+++ app/Domain/Auth/AuthServiceProvider.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Domain\Auth;
 
 use App\Domain\Auth\Controllers\AccessController;
+use Auth0\SDK\Auth0;
+use Auth0\SDK\Contract\Auth0Interface;
 use Illuminate\Support\ServiceProvider;
 
 final class AuthServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(Auth0Interface::class, Auth0::class);
+
         $this->app->when(AccessController::class)
             ->needs('$adminEmails')
             ->give(config('nova.users'));
