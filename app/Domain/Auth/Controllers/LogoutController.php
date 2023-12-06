16de3ce0a1b7c4d16388ddb865e4a016835e58d0
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-12-06 12:34:48 +0100

    split out getLogoutLink()

--- app/Domain/Auth/Controllers/LogoutController.php
+++ app/Domain/Auth/Controllers/LogoutController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Auth\Controllers;
 
-use Auth0\SDK\Auth0;
+use Auth0\SDK\Contract\Auth0Interface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -12,21 +12,27 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class LogoutController
 {
-    private function getLogoutLink(): string
-    {
-        /** @var Auth0 $auth0 */
-        $auth0 = app(Auth0::class);
+    public function __construct(
+        private readonly Auth0Interface $auth0
+    ) {
+    }
 
+    private function logout(): void
+    {
         if (Auth::check()) {
-            $auth0->logout();
+            $this->auth0->logout();
             Auth::guard(config('nova.guard'))->logout();
         }
+    }
 
-        return $auth0->authentication()->getLogoutLink(config('app.url'));
+    private function getLogoutLink(): string
+    {
+        return $this->auth0->authentication()->getLogoutLink(config('app.url'));
     }
 
     public function adminLogout(): JsonResponse
     {
+        $this->logout();
         $logoutLink = $this->getLogoutLink();
 
         return new JsonResponse([
@@ -36,6 +42,7 @@ final class LogoutController
 
     public function inertiaLogout(): Response
     {
+        $this->logout();
         $logoutLink = $this->getLogoutLink();
 
         return Inertia::location($logoutLink);
