d2c268d4447251e5d78a6003a0b6450d8afee4ea
simon-debruijn <simon.debruijn@hotmail.com>
2023-12-14 16:09:54 +0100

    Add access-integration gate

--- app/Domain/Auth/AuthServiceProvider.php
+++ app/Domain/Auth/AuthServiceProvider.php
@@ -5,12 +5,25 @@ declare(strict_types=1);
 namespace App\Domain\Auth;
 
 use App\Domain\Auth\Controllers\AccessController;
+use App\Domain\Auth\Models\UserModel;
+use App\Domain\Contacts\Repositories\ContactRepository;
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Contract\Auth0Interface;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Ramsey\Uuid\Uuid;
 
 final class AuthServiceProvider extends ServiceProvider
 {
+    private ContactRepository $contactRepository;
+
+    public function __construct($app)
+    {
+        parent::__construct($app);
+
+        $this->contactRepository = app(ContactRepository::class);
+    }
+
     public function register(): void
     {
         $this->app->bind(Auth0Interface::class, Auth0::class);
@@ -18,5 +31,11 @@ final class AuthServiceProvider extends ServiceProvider
         $this->app->when(AccessController::class)
             ->needs('$adminEmails')
             ->give(config('nova.users'));
+
+        Gate::define('access-integration', function (UserModel $user, string $integrationId): bool {
+            $contacts = $this->contactRepository->getByIntegrationIdAndEmail(Uuid::fromString($integrationId), $user->email);
+
+            return $contacts->count() > 0;
+        });
     }
 }
