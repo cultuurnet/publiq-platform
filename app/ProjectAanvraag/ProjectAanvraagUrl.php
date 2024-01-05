d5d71ddb221104b08a9ba9507e01c4818cd75016
Emma Fabre <ehtnam6@gmail.com>
2024-01-05 14:23:28 +0100

    Use live platform if integration is active

--- app/ProjectAanvraag/ProjectAanvraagUrl.php
+++ app/ProjectAanvraag/ProjectAanvraagUrl.php
@@ -3,6 +3,7 @@
 namespace App\ProjectAanvraag;
 
 use App\Domain\Integrations\Integration;
+use App\Domain\Integrations\IntegrationStatus;
 use Illuminate\Support\Facades\Session;
 
 class ProjectAanvraagUrl
@@ -10,7 +11,8 @@ class ProjectAanvraagUrl
     public static function getForIntegration(Integration $integration): string
     {
         $idToken = Session::get('id_token');
-        $base = config('project_aanvraag.base_uri');
+        $stage = $integration->status === IntegrationStatus::Active ? 'live' : 'test';
+        $base = config('project_aanvraag.base_uri_' . $stage);
 
         return $base . 'project/' . $integration->id . '/widget/?idToken=' . $idToken;
     }
