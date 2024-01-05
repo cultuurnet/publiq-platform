614f52d70b4bc3ebdc42861ccd4c1c3679f345bc
Emma Fabre <ehtnam6@gmail.com>
2024-01-05 16:28:53 +0100

    Make Sentry also listen for front-end errors

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -4,6 +4,12 @@ import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { initializeI18n } from "./i18n/initializeI18n";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.VITE_APP_ENV,
+});
 
 createInertiaApp({
   resolve: (name) =>
