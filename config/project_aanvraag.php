d2fa2480360ad6e7e503728392e60806ca88fb6e
Emma Fabre <ehtnam6@gmail.com>
2024-01-05 14:03:10 +0100

    Split URLs in configuration

--- config/project_aanvraag.php
+++ config/project_aanvraag.php
@@ -4,5 +4,6 @@ declare(strict_types=1);
 
 return [
     'create_widget' => env('PROJECT_AANVRAAG_CREATE_WIDGET', false),
-    'base_uri' => env('PROJECT_AANVRAAG_BASE_URI', 'http://localhost'),
+    'base_uri_test' => env('PROJECT_AANVRAAG_BASE_URI_TEST', 'http://localhost'),
+    'base_uri_live' => env('PROJECT_AANVRAAG_BASE_URI_LIVE', 'http://localhost'),
 ];
