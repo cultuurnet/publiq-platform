db9e3245f214c9aba102826aa2b13d3d78ee51d7
Emma Fabre <ehtnam6@gmail.com>
2024-01-12 14:03:36 +0100

    Add DistributeKeys event listener

--- app/Domain/Integrations/IntegrationServiceProvider.php
+++ app/Domain/Integrations/IntegrationServiceProvider.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Domain\Integrations;
 
+use App\Domain\Integrations\Events\IntegrationActivatedWithCoupon;
+use App\Domain\Integrations\Events\IntegrationActivatedWithOrganization;
+use App\Domain\Integrations\Listeners\DistributeKeys;
 use App\Domain\Integrations\Repositories\EloquentIntegrationRepository;
 use App\Domain\Integrations\Repositories\EloquentIntegrationUrlRepository;
 use App\Domain\Integrations\Repositories\IntegrationRepository;
 use App\Domain\Integrations\Repositories\IntegrationUrlRepository;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 final class IntegrationServiceProvider extends ServiceProvider
@@ -16,5 +20,8 @@ final class IntegrationServiceProvider extends ServiceProvider
     {
         $this->app->bind(IntegrationRepository::class, EloquentIntegrationRepository::class);
         $this->app->bind(IntegrationUrlRepository::class, EloquentIntegrationUrlRepository::class);
+
+        Event::listen(IntegrationActivatedWithCoupon::class, [DistributeKeys::class, 'handle']);
+        Event::listen(IntegrationActivatedWithOrganization::class, [DistributeKeys::class, 'handle']);
     }
 }
