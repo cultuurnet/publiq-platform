d1785a8662b2873c2126b880503051a671f6d51d
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-05 15:23:34 +0100

    Use new flat config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import javascriptPlugin from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+
+export default [
+  javascriptPlugin.configs.recommended,
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "@typescript-eslint": typescriptPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      "react/prop-types": "off",
+
+      ...reactHooksPlugin.configs.recommended.rules,
+      "react-hooks/exhaustive-deps": "error",
+
+      ...typescriptPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        es2021: true,
+      },
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
