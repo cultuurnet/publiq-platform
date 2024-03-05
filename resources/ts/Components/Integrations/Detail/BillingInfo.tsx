ec01e65b6c27a695f6dddafa019eac1037950f7a
Simon de Bruijn <66943525+simon-debruijn@users.noreply.github.com>
2024-03-05 17:04:19 +0100

    Update resources/ts/Components/Integrations/Detail/BillingInfo.tsx

--- resources/ts/Components/Integrations/Detail/BillingInfo.tsx
+++ resources/ts/Components/Integrations/Detail/BillingInfo.tsx
@@ -82,7 +82,6 @@ export const BillingInfo = ({ id, organization, subscription }: Props) => {
                       onChange={(e) =>
                         setData("organization", {
                           // We know organization exists
-
                           ...data.organization!,
                           address: {
                             ...data.organization!.address,
