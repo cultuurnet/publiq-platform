ce926a537b1e2bbfbdcc525e9e4f8c52e725024c
Emma Fabre <ehtnam6@gmail.com>
2024-03-06 13:47:17 +0100

    Remove width constraint on delete integration page

--- resources/ts/Components/Integrations/Detail/DeleteIntegration.tsx
+++ resources/ts/Components/Integrations/Detail/DeleteIntegration.tsx
@@ -17,7 +17,7 @@ export const DeleteIntegration = ({ id }: Props) => {
   };
   return (
     <>
-      <div className="max-w-[30rem] flex flex-col gap-5">
+      <div className="flex flex-col gap-5">
         <Heading level={2} className="font-semibold">
           {t("details.delete_integration.title")}
         </Heading>
