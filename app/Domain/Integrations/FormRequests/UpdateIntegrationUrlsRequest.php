2e13ddef9bb15b8e430ef7f93b9472f2c049df73
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-06 10:42:27 +0100

    Add comments to after method validation

--- app/Domain/Integrations/FormRequests/UpdateIntegrationUrlsRequest.php
+++ app/Domain/Integrations/FormRequests/UpdateIntegrationUrlsRequest.php
@@ -28,23 +28,45 @@ final class UpdateIntegrationUrlsRequest extends FormRequest
         ];
     }
 
+    /**
+     * By default, the validation will return error keys containing the index position of the urls that do not conform to the validation rules.
+     * e.g. 'urls.0.url'
+     * In the frontend the urls order is changed because they are grouped by type and environment, thus we cannot map the index position to the
+     * corresponding input field.
+     * That is why we replace the default error keys by custom error keys containing the type, environment and url.
+     */
     public function after(): array
     {
+
         return [
             function (Validator $validator): void {
+                // get the errors array containing key/value pairs from error key to error message
                 $errors = $validator->errors()->get('urls.*');
+                // get the form data for the urls
                 $data = $validator->getData()['urls'];
 
                 foreach ($errors as $originalKey => $originalMessage) {
+                    // get the index out of the original key
+                    // e.g. 'urls.0.url' -> '0'
                     $index = explode('.', $originalKey)[1];
+
+                    // get the url form value for the current index
                     $url = $data[$index];
-                    $errorKey = sprintf('%s.%s.%s', $url['type'], $url['environment'], $url['url']);
+
+                    // create a custom error key, with the following structure 'type.environment.url'
+                    $errorKey = "{$url['type']}.{$url['environment']}.{$url['url']}";
+
+                    // create new error message where the prefix is replaced by 'Url'
                     $errorMessage = str_replace(
                         ucfirst($originalKey),
                         'Url',
                         $originalMessage
                     );
+
+                    // add custom error
                     $validator->errors()->add($errorKey, $errorMessage);
+
+                    // delete original error
                     $validator->errors()->forget($originalKey);
                 }
             },
