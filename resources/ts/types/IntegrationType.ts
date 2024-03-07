bfca8ad91d4906c0259478a59bb8e0f61e50e5e0
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-07 17:52:10 +0100

    Add IntegrationType

--- resources/ts/types/IntegrationType.ts
+++ resources/ts/types/IntegrationType.ts
@@ -1,5 +1,13 @@
+import { Values } from "./Values";
+
 export const IntegrationType = {
   EntryApi: "entry-api",
   SearchApi: "search-api",
   Widgets: "widgets",
 } as const;
+
+export type IntegrationType = Values<typeof IntegrationType>;
+
+export const isIntegrationType = (val: unknown): val is IntegrationType => {
+  return Object.values(IntegrationType).includes(val as IntegrationType);
+};
