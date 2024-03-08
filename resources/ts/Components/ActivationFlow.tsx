3f6e63d05ea6f2869213fe295c2b6dde2f01bdf6
vhande <hande.vanhove@publiq.be>
2024-03-08 16:36:27 +0100

    Create ActivationFlow.tsx

---
+++ resources/ts/Components/ActivationFlow.tsx
@@ -0,0 +1,44 @@
+import React, { ComponentProps, useState } from "react";
+import { IntegrationStatus } from "../types/IntegrationStatus";
+import { router } from "@inertiajs/react";
+import { ActivationDialog } from "./ActivationDialog";
+import { Subscription } from "../Pages/Integrations/Index";
+import { Values } from "../types/Values";
+import { IntegrationType } from "../types/IntegrationType";
+import { ActivationRequest } from "./ActivationRequest";
+
+type Props = ComponentProps<"div"> & {
+  status: IntegrationStatus;
+  id: string;
+  subscription?: Subscription;
+  type: Values<typeof IntegrationType>;
+  email?: string;
+};
+
+export const ActivationFlow = ({ id, subscription, type, email }: Props) => {
+  const url = new URL(document.location.href);
+  const isDialogVisible = url.searchParams.get("isDialogVisible");
+
+  const [isActivationDialogVisible, setIsActivationDialogVisible] = useState(
+    !!isDialogVisible ?? false
+  );
+
+  return (
+    <>
+      <ActivationRequest id={id} />
+      <ActivationDialog
+        isVisible={isActivationDialogVisible}
+        onClose={() => {
+          router.get(url.toString(), {
+            isDialogVisible: undefined,
+          });
+          setIsActivationDialogVisible(false);
+        }}
+        id={id}
+        subscription={subscription}
+        type={type}
+        email={email!}
+      />
+    </>
+  );
+};
