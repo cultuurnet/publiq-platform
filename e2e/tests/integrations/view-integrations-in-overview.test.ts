bb4d3b19f05a81db5871224112428080eb14d46e
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-15 15:19:51 +0100

    Make selector more specific

--- e2e/tests/integrations/view-integrations-in-overview.test.ts
+++ e2e/tests/integrations/view-integrations-in-overview.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from "@playwright/test";
 
 test("view my integrations in overview", async ({ page }) => {
   await page.goto("/nl/integraties");
-  await expect(page.getByText("Integraties")).toBeVisible();
+  await expect(page.locator("h2").getByText("Mijn integraties")).toBeVisible();
   await expect(page.getByText("Geen integraties gevonden")).toBeVisible();
 });
