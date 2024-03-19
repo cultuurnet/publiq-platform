4457d930e2f969926861e61b0d5f331b2b484a97
vhande <hande.vanhove@publiq.be>
2024-03-19 16:00:11 +0100

    Improve style

--- resources/ts/Components/Integrations/Detail/CredentialsWidgets.tsx
+++ resources/ts/Components/Integrations/Detail/CredentialsWidgets.tsx
@@ -26,35 +26,41 @@ export const CredentialsWidgets = ({
   const { t } = useTranslation();
 
   return (
-    <div className="flex w-full max-lg:flex-col gap-6">
+    <div className="flex w-full max-lg:flex-col">
       <Heading className="font-semibold lg:min-w-60" level={4}>
         {t("integrations.products.widgets")}
       </Heading>
-      <div className="flex flex-col gap-4 min-w-[40rem]">
+      <div className="flex flex-col gap-6 min-w-[40rem]">
         {status !== IntegrationStatus.Active && (
-          <>
+          <div>
             <Heading className="font-semibold flex min-w-[5rem]" level={4}>
               {t("details.credentials.test")}
             </Heading>
             <OpenWidgetBuilderButton type={type} id={id} />
-          </>
+          </div>
         )}
-        <Heading className="font-semibold flex min-w-[5rem]" level={4}>
-          {t("details.credentials.live")}
-        </Heading>
-        <StatusLight status={status} />
-        {status === IntegrationStatus.Active && (
-          <OpenWidgetBuilderButton type={type} id={id} />
-        )}
-        {status === IntegrationStatus.Draft && (
-          <ActivationFlow
-            status={status}
-            id={id}
-            subscription={subscription}
-            type={type}
-            email={email}
-          />
-        )}
+        <div className="flex flex-col gap-3">
+          <div>
+            <Heading className="font-semibold flex min-w-[5rem]" level={4}>
+              {t("details.credentials.live")}
+            </Heading>
+            <StatusLight status={status} />
+          </div>
+          <div>
+            {status === IntegrationStatus.Active && (
+              <OpenWidgetBuilderButton type={type} id={id} />
+            )}
+            {status === IntegrationStatus.Draft && (
+              <ActivationFlow
+                status={status}
+                id={id}
+                subscription={subscription}
+                type={type}
+                email={email}
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
