462327d43fcb4cf752ebf39651028a7a750abdf0
vhande <hande.vanhove@publiq.be>
2024-03-21 10:15:18 +0100

    Create NewsletterDialog.tsx

---
+++ resources/ts/Components/NewsletterDialog.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Dialog } from "./Dialog";
+import { ButtonSecondary } from "./ButtonSecondary";
+import { ButtonPrimary } from "./ButtonPrimary";
+import { useIsMobile } from "../hooks/useIsMobile";
+import { FormElement } from "./FormElement";
+import { Input } from "./Input";
+import { Alert } from "./Alert";
+import { useForm } from "@inertiajs/react";
+
+type Props = {
+  isVisible?: boolean;
+  onClose: () => void;
+};
+
+export const NewsletterDialog = ({ isVisible, onClose }: Props) => {
+  const { t } = useTranslation();
+
+  const isMobile = useIsMobile();
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const initialFormValues = {
+    email: "",
+  };
+
+  const {
+    data,
+    setData,
+    put,
+    errors: err,
+    wasSuccessful,
+    hasErrors,
+  } = useForm(initialFormValues);
+
+  const errors = err as Record<string, string | undefined>;
+
+  return (
+    <Dialog
+      title={t("footer.newsletter_dialog.title")}
+      actions={
+        isFormVisible && (
+          <>
+            <ButtonSecondary>{t("dialog.cancel")}</ButtonSecondary>
+            <ButtonPrimary>{t("dialog.confirm")}</ButtonPrimary>
+          </>
+        )
+      }
+      isFullscreen={isMobile}
+      isVisible={isVisible}
+      onClose={onClose}
+    >
+      {isFormVisible ? (
+        <FormElement
+          label={`${t("footer.newsletter_dialog.email")}`}
+          className="col-span-2"
+          error={errors["email"]}
+          component={
+            <Input
+              type="text"
+              name="email"
+              placeholder={t("footer.newsletter_dialog.placeholder")}
+              value={data.email}
+              onChange={(e) => setData("email", e.target.value)}
+            />
+          }
+        />
+      ) : (
+        <Alert
+          variant="success"
+          title={t("footer.newsletter_dialog.success.title")}
+        >
+          <p>{t("footer.newsletter_dialog.success.description")}</p>
+        </Alert>
+      )}
+    </Dialog>
+  );
+};
