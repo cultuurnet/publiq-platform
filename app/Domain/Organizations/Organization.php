106be29af9e32dfa74230cbfe276a039ae0135d7
Luc Wollants <lucwollants@gmail.com>
2024-03-21 10:38:10 +0100

    Make `invoiceEmail` nullable

--- app/Domain/Organizations/Organization.php
+++ app/Domain/Organizations/Organization.php
@@ -11,7 +11,7 @@ final class Organization
     public function __construct(
         public readonly UuidInterface $id,
         public readonly string $name,
-        public readonly string $invoiceEmail,
+        public readonly ?string $invoiceEmail,
         public readonly ?string $vat,
         public readonly Address $address,
     ) {
