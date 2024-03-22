c0730b8b3f023830dbcefa9aa4ca12b2debf7dbf
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-03-22 09:08:55 +0100

    Add newletter config

---
+++ config/newsletter.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'apiKey' => env('NEWSLETTER_API_KEY'),
+    'apiSecret' => env('NEWSLETTER_API_SECRET'),
+    'id' => env('NEWSLETTER_API_ID'),
+];
