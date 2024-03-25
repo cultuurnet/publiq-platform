9caadb8ee190be85a3eb85723dccd942f920ad82
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-25 11:03:40 +0100

    Remove unused import

--- resources/ts/Components/ActivationDialog.tsx
+++ resources/ts/Components/ActivationDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Dialog } from "./Dialog";
 import { ButtonSecondary } from "./ButtonSecondary";
 import { ButtonPrimary } from "./ButtonPrimary";
