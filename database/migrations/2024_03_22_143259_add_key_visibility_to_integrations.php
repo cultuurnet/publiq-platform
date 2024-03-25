2c1c0f656b7710ea2ebf8560598bb21934a97af9
Luc Wollants <lucwollants@gmail.com>
2024-03-25 10:19:35 +0100

    Linting

--- database/migrations/2024_03_22_143259_add_key_visibility_to_integrations.php
+++ database/migrations/2024_03_22_143259_add_key_visibility_to_integrations.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Domain\Integrations\KeyVisibility;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::table('integrations', function (Blueprint $table) {
