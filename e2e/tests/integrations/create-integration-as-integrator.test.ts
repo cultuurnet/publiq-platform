813627bc8ff708169d056499885a829c80e5ff90
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-03-26 09:36:51 +0100

    PPF-343 - Remove redundant intermediate clicks

--- e2e/tests/integrations/create-integration-as-integrator.test.ts
+++ e2e/tests/integrations/create-integration-as-integrator.test.ts
@@ -5,18 +5,13 @@ test("As an integrator I can create a new integration", async ({ page }) => {
   await expect(page.locator("h2").getByText("Mijn integraties")).toBeVisible();
 
   await page.getByRole('main').getByRole('link', { name: 'Integratie toevoegen' }).click();
-  await page.getByLabel('Naam integratie').click();
   await page.getByLabel('Naam integratie').fill('Test E2E integration');
-  await page.getByLabel('Doel van de integratie').click();
   await page.getByLabel('Doel van de integratie').fill('Test E2e integration');
-  await page.locator('input[name="lastNameFunctionalContact"]').click();
   await page.locator('input[name="lastNameFunctionalContact"]').fill('E2E');
   await page.locator('input[name="lastNameFunctionalContact"]').press('Tab');
   await page.locator('input[name="firstNameFunctionalContact"]').fill('test');
   await page.locator('input[name="firstNameFunctionalContact"]').press('Tab');
-  await page.locator('input[name="emailFunctionalContact"]').click();
   await page.locator('input[name="emailFunctionalContact"]').fill('dev+e2etest@publiq.be');
-  await page.locator('input[name="emailPartner"]').click();
   await page.locator('input[name="emailPartner"]').fill('dev+e2etest@publiq.be');
   await page.getByLabel('Ik ga akkoord met de').check();
   await page.getByRole('button', { name: 'Integratie aanmaken' }).click();
