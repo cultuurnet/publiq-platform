85068409db0107741996ce8cad6cced84836451a
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-03-26 14:40:38 +0100

    PPF-350 - use faker js for dummy data in other tests as well

--- e2e/tests/integrations/create-integration-as-integrator.test.ts
+++ e2e/tests/integrations/create-integration-as-integrator.test.ts
@@ -1,23 +1,32 @@
 import { expect, test } from "@playwright/test";
+import { faker } from '@faker-js/faker';
+
+test.use({ storageState: 'playwright/.auth/user.json' });
 
 test("As an integrator I can create a new integration", async ({ page }) => {
   await page.goto("/nl/integraties");
   await expect(page.locator("h2").getByText("Mijn integraties")).toBeVisible();
 
   await page.getByRole('main').getByRole('link', { name: 'Integratie toevoegen' }).click();
-  await page.getByLabel('Naam integratie').fill('Test E2E integration');
-  await page.getByLabel('Doel van de integratie').fill('Test E2e integration');
-  await page.locator('input[name="lastNameFunctionalContact"]').fill('E2E');
+
+  const integrationName = faker.word.adjective();
+  await page.getByLabel('Naam integratie').fill(integrationName);
+  await page.getByLabel('Doel van de integratie').fill(faker.lorem.lines(2));
+  await page.locator('input[name="lastNameFunctionalContact"]').fill(faker.person.lastName());
   await page.locator('input[name="lastNameFunctionalContact"]').press('Tab');
-  await page.locator('input[name="firstNameFunctionalContact"]').fill('test');
+  await page.locator('input[name="firstNameFunctionalContact"]').fill(faker.person.firstName());
   await page.locator('input[name="firstNameFunctionalContact"]').press('Tab');
-  await page.locator('input[name="emailFunctionalContact"]').fill(process.env.E2E_TEST_EMAIL!);
-  await page.locator('input[name="emailPartner"]').fill(process.env.E2E_TEST_EMAIL!);
+  await page.locator('input[name="emailFunctionalContact"]').fill(faker.internet.email());
+  await page.locator('input[name="lastNameTechnicalContact"]').fill(faker.person.lastName());
+  await page.locator('input[name="lastNameTechnicalContact"]').press('Tab');
+  await page.locator('input[name="firstNameTechnicalContact"]').fill(faker.person.firstName());
+  await page.locator('input[name="firstNameTechnicalContact"]').press('Tab');
+  await page.locator('input[name="emailPartner"]').fill(faker.internet.email());
   await page.getByLabel('Ik ga akkoord met de').check();
   await page.getByRole('button', { name: 'Integratie aanmaken' }).click();
 
   await page.waitForURL(/https?:\/\/[^/]*\/nl\/integraties(\/.*)?/);
-  await expect(page.getByText('Test E2E integration')).toBeVisible();
+  await expect(page.getByText(integrationName)).toBeVisible();
 });
 
 
