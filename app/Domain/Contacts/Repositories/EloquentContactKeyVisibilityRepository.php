8ce7055dc79c990cfef59965452f3fc90a3aab4d
Luc Wollants <lucwollants@gmail.com>
2024-03-26 08:31:49 +0100

    Create repo for `ContactKeyVisibility`

---
+++ app/Domain/Contacts/Repositories/EloquentContactKeyVisibilityRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Contacts\Repositories;
+
+use App\Domain\Contacts\Models\ContactKeyVisibilityModel;
+use App\Domain\Integrations\KeyVisibility;
+
+final class EloquentContactKeyVisibilityRepository implements ContactKeyVisibilityRepository
+{
+    public function forEmail(string $email): KeyVisibility
+    {
+        try {
+            /** @var ContactKeyVisibilityModel $contactKeyVisibility */
+            $contactKeyVisibility = ContactKeyVisibilityModel::query()
+                ->where('email', $email)
+                ->firstOrFail();
+            return $contactKeyVisibility->key_visibility;
+        } catch (\Throwable) {
+            return KeyVisibility::v2;
+        }
+    }
+}
