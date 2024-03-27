c5451f61ddc878111bf362b7f246f619669e8de0
simon-debruijn <simon.debruijn@hotmail.com>
2024-03-27 11:31:52 +0100

    Remove unused subscription page and controller

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Domain\Auth\Controllers\LoginController;
 use App\Domain\Auth\Controllers\LogoutController;
 use App\Domain\Integrations\Controllers\IntegrationController;
 use App\Domain\Newsletter\Controllers\NewsletterController;
-use App\Domain\Subscriptions\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 use App\Router\TranslatedRoute;
 use App\Http\Controllers\HomeController;
@@ -50,14 +49,6 @@ Route::group(['middleware' => 'auth'], static function () {
 Route::group(['middleware' => 'auth'], static function () {
     Route::post('/support/slack', [SupportController::class, 'sendInvitation']);
 });
-
-TranslatedRoute::get(
-    [
-        '/en/subscriptions',
-        '/nl/abonnementen',
-    ],
-    [SubscriptionController::class, 'index']
-);
 
 Route::group(['middleware' => 'auth'], static function () {
     TranslatedRoute::get(
