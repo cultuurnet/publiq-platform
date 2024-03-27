0d2a5af59e9de302557c16360ce1189869c206a8
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-03-27 11:27:18 +0100

    PPF-359 - Add new test: activate an integration as an admin

---
+++ e2e/tests/integrations/admin/activate-integration.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "@playwright/test";
+import {
+  IntegrationTypes,
+  createIntegration,
+} from "./create-integration.test.js";
+import { createOrganization } from "./create-organization.test.js";
+
+test.use({ storageState: "playwright/.auth/admin.json" });
+
+test("As an admin I can activate an integration", async ({ page }) => {
+
+  // create organization
+  const organizationPage = await createOrganization(page);
+  const organizationUrl = organizationPage.url();
+  const organizationId = organizationUrl.split("/").pop();
+
+  // create integration
+  const integrationPage = await createIntegration(
+    IntegrationTypes.SEARCH_API,
+    page
+  );
+  const integrationUrl = integrationPage.url();
+  const integrationId = integrationUrl.split("/").pop();
+
+  // activate integration
+  await page.goto(`/admin/resources/integrations/${integrationId}`);
+  await page.getByRole("button", { name: "Actions" }).click();
+  await page.getByRole("button", { name: "Activate Integration" }).click();
+  await page.locator("#organization").selectOption(organizationId!);
+  await page.getByRole("button", { name: "Activate" }).click();
+
+  await expect(page.locator(`a[href="/admin/resources/organizations/${organizationId}"]`)).toBeVisible();
+  await expect(page.getByText("active", { exact: true })).toBeVisible();
+});
