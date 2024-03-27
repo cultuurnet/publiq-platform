9ba774f115836c0527b52b8a48f1ccaa3b735cba
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-03-27 10:05:10 +0100

    PPF-359 - Use faker js data for creation of organization

--- e2e/tests/integrations/create-organization-as-admin.test.ts
+++ e2e/tests/integrations/create-organization-as-admin.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from "@playwright/test";
+import { fakerNL_BE as faker } from "@faker-js/faker";
 
 test.use({ storageState: "playwright/.auth/admin.json" });
 
@@ -6,19 +7,21 @@ test("As an admin I can create a new organization", async ({ page }) => {
   await page.goto("/admin");
   await page.getByRole("link", { name: "Organizations" }).click();
   await page.getByRole("link", { name: "Create Organization" }).click();
-  await page.getByPlaceholder("Name").fill("E2E test organization");
-  await page.getByPlaceholder("Street").fill("test street 21");
-  await page.getByPlaceholder("City").fill("Brussels");
-  await page.getByPlaceholder("Zip").fill("1080");
-  await page.getByPlaceholder("Country").fill("Belgium");
+  const organizationName = faker.company.name()
+  await page.getByPlaceholder("Name").fill(organizationName);
+  await page.getByPlaceholder("Street").fill(faker.location.streetAddress());
+  await page.getByPlaceholder("City").fill(faker.location.city());
+  await page.getByPlaceholder("Zip").fill(faker.location.zipCode('####'));
+  await page.getByPlaceholder("Country").fill(faker.location.country());
   await page
     .getByPlaceholder("Invoice Email")
-    .fill(process.env.E2E_TEST_ADMIN_EMAIL!);
+    .fill(faker.internet.email());
   await page.getByPlaceholder("Vat").fill("BE 0475 250 609");
   await page.getByRole("button", { name: "Create Organization" }).click();
+  await page.screenshot({ path: `screenshots/create-organization-as-admin.png` });
   await expect(
     page
       .locator("h1")
-      .getByText("Organization Details: E2E test organization")
+      .getByText(`Organization Details: ${organizationName}`)
   ).toBeVisible();
 });
