2a694c31def6eac65335e1348584981bd989766b
Luc Wollants <lucwollants@gmail.com>
2024-03-27 11:33:49 +0100

    Add `save` method on `ContactKeyVisibilityRepository`

---
+++ tests/Domain/Contacts/Repositories/EloquentContactKeyVisibilityRepositoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Contacts\Repositories;
+
+use App\Domain\Contacts\Models\ContactKeyVisibilityModel;
+use App\Domain\Contacts\Repositories\EloquentContactKeyVisibilityRepository;
+use App\Domain\Integrations\KeyVisibility;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
+use Tests\TestCase;
+use Tests\UuidTestFactory;
+
+final class EloquentContactKeyVisibilityRepositoryTest extends TestCase
+{
+    private EloquentContactKeyVisibilityRepository $repository;
+
+    protected function setUp(): void
+    {
+        Uuid::setFactory(new UuidTestFactory(['uuid4' => ['a40a0c9d-988e-48da-8543-fc74510ea495']]));
+
+        $this->repository = new EloquentContactKeyVisibilityRepository();
+
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        Uuid::setFactory(new UuidFactory());
+
+        parent::tearDown();
+    }
+
+    public function test_it_can_save_a_key_visibility(): void
+    {
+        $this->repository->save('info@publiq.be', KeyVisibility::v1);
+
+        $this->assertDatabaseHas('contacts_key_visibility', [
+            'id' => 'a40a0c9d-988e-48da-8543-fc74510ea495',
+            'email' => 'info@publiq.be',
+            'key_visibility' => KeyVisibility::v1,
+        ]);
+    }
+
+    public function test_it_can_find_a_key_visibility_by_email(): void
+    {
+        ContactKeyVisibilityModel::query()->create([
+            'id' => 'a40a0c9d-988e-48da-8543-fc74510ea495',
+            'email' => 'info@publiq.be',
+            'key_visibility' => KeyVisibility::all,
+        ]);
+
+        $keyVisibility = $this->repository->findByEmail('info@publiq.be');
+
+        $this->assertEquals(KeyVisibility::all, $keyVisibility);
+    }
+}
