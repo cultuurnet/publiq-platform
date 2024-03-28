29a0b010165db9c3282ec826fe9150c7c7a64f36
vhande <hande.vanhove@publiq.be>
2024-03-28 14:00:09 +0100

    Hide urls when an integration only has v1 credentials

--- resources/ts/Pages/Integrations/Index.tsx
+++ resources/ts/Pages/Integrations/Index.tsx
@@ -22,6 +22,7 @@ import { IntegrationType } from "../../types/IntegrationType";
 import { Values } from "../../types/Values";
 import { Auth0Tenant } from "../../types/Auth0Tenant";
 import { UiTiDv1Environment } from "../../types/UiTiDv1Environment";
+import { KeyVisibility } from "../../types/KeyVisibility";
 
 type Organization = {
   id: string;
@@ -99,6 +100,7 @@ export type Integration = {
   urls: IntegrationUrl[];
   authClients: AuthClient[];
   legacyAuthConsumers: LegacyAuthConsumer[];
+  keyVisibility: KeyVisibility;
 };
 
 type Props = {
