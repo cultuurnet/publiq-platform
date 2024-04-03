a4620b765fcd49e4faa65f29b06992edcf83bcd1
Emma Fabre <ehtnam6@gmail.com>
2024-04-03 13:16:33 +0200

    Fix Coupon properties alignment

--- app/Domain/Coupons/Coupon.php
+++ app/Domain/Coupons/Coupon.php
@@ -11,11 +11,11 @@ final class Coupon
     public const REDUCTION_AMOUNT = 125.0;
 
     public function __construct(
-        public readonly UuidInterface  $id,
-        public readonly bool           $isDistributed,
+        public readonly UuidInterface $id,
+        public readonly bool $isDistributed,
         public readonly ?UuidInterface $integrationId,
-        public readonly string         $code,
-        public readonly float          $reduction = self::REDUCTION_AMOUNT
+        public readonly string $code,
+        public readonly float $reduction = self::REDUCTION_AMOUNT
     ) {
     }
 }
