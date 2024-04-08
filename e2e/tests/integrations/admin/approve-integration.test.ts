602c28c16468c0d4c67e7691ca511f24ebdb272d
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-04-08 13:12:20 +0200

    PPF-366 - Finalise approve integration test

--- e2e/tests/integrations/admin/approve-integration.test.ts
+++ e2e/tests/integrations/admin/approve-integration.test.ts
@@ -1,26 +1,35 @@
 import { expect, test } from "@playwright/test";
 import { createIntegrationAsIntegrator } from "../integrator/create-integration.js";
 import { requestActivationAsIntegrator } from "../integrator/request-activation.js";
+import { IntegrationTypes } from "../../types.js";
 
 test.use({ storageState: "playwright/.auth/admin.json" });
 
-test("As an admin I can activate an integration", async ({ page }) => {
+test("As an admin I can approve an integration", async ({ page }) => {
+  // create integration as integrator
+  const {page: integrationPage, integrationName} = await createIntegrationAsIntegrator(
+    page,
+    IntegrationTypes.ENTRY_API
+  );
 
-  // create integration
-  const integrationPage = await createIntegrationAsIntegrator(page);
+  await page.waitForURL(/https?:\/\/[^/]*\/nl\/integraties(\/.*)?/);
+  await expect(page.getByText(integrationName)).toBeVisible();
+
   const integrationUrl = integrationPage.url();
   const integrationId = integrationUrl.split("/").pop();
 
   // request activation
-  await requestActivationAsIntegrator(page, integrationId!);
+  await requestActivationAsIntegrator(
+    page,
+    integrationId!,
+    IntegrationTypes.ENTRY_API
+  );
 
-  // activate integration
+  // approve integration
   await page.goto(`/admin/resources/integrations/${integrationId}`);
   await page.getByRole("button", { name: "Actions" }).click();
-  await page.getByRole("button", { name: "Activate Integration" }).click();
-  await page.locator("#organization").selectOption(organizationId!);
-  await page.getByRole("button", { name: "Activate" }).click();
+  await page.getByRole("button", { name: "Approve Integration" }).click();
+  await page.getByRole("button", { name: "Approve" }).click();
 
-  await expect(page.locator(`a[href="/admin/resources/organizations/${organizationId}"]`)).toBeVisible();
   await expect(page.getByText("active", { exact: true })).toBeVisible();
 });
