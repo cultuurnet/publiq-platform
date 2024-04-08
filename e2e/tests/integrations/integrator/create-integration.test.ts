1f84e5e9a97f1d2d3027d484bde46738254f5595
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-04-08 13:11:36 +0200

    PPF-366 - Do assertion in test file instead of helper

--- e2e/tests/integrations/integrator/create-integration.test.ts
+++ e2e/tests/integrations/integrator/create-integration.test.ts
@@ -1,10 +1,14 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { createIntegrationAsIntegrator } from "./create-integration.js";
+import { IntegrationTypes } from "../../types.js";
 
 test.use({ storageState: 'playwright/.auth/user.json' });
 
 test("As an integrator I can create a new integration", async ({ page }) => {
-  await createIntegrationAsIntegrator(page);
+  const { integrationName} = await createIntegrationAsIntegrator(page, IntegrationTypes.SEARCH_API);
+
+  await page.waitForURL(/https?:\/\/[^/]*\/nl\/integraties(\/.*)?/);
+  await expect(page.getByText(integrationName)).toBeVisible();
 });
 
 
