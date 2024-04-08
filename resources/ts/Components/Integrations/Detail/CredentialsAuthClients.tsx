3b0715abf73e59448d2c8f1307e1b46edfe3ef80
vhande <hande.vanhove@publiq.be>
2024-04-08 15:43:11 +0200

    Implement key visibility

--- resources/ts/Components/Integrations/Detail/CredentialsAuthClients.tsx
+++ resources/ts/Components/Integrations/Detail/CredentialsAuthClients.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Heading } from "../../Heading";
 import { useTranslation } from "react-i18next";
 import { StatusLight } from "../../StatusLight";
-//import { ButtonPrimary } from "../../ButtonPrimary";
+import { ButtonPrimary } from "../../ButtonPrimary";
 import { CopyText } from "../../CopyText";
 import { ActivationFlow } from "../../ActivationFlow";
 import { IntegrationStatus } from "../../../types/IntegrationStatus";
 import type { Credentials } from "./Credentials";
 import type { Integration } from "../../../types/Integration";
+import { KeyVisibility } from "../../../types/KeyVisibility";
 
-type Props = Pick<Integration, "id" | "status" | "subscription" | "type"> &
+type Props = Pick<
+  Integration,
+  "id" | "status" | "subscription" | "type" | "keyVisibility"
+> &
   Credentials & { email: string };
 
 export const CredentialsAuthClients = ({
@@ -20,6 +24,7 @@ export const CredentialsAuthClients = ({
   email,
   subscription,
   type,
+  keyVisibility,
 }: Props) => {
   const { t } = useTranslation();
 
@@ -50,67 +55,65 @@ export const CredentialsAuthClients = ({
       <Heading className="font-semibold lg:min-w-60" level={4}>
         {t("details.credentials.uitid_v2")}
       </Heading>
-      {/*{!auth0ProdClient ? (
+      {keyVisibility.includes(KeyVisibility.v1) ? (
         <div className="flex flex-col flex-1 gap-4">
           <p>{t("details.credentials.uitid_alert")}</p>
-          <ButtonPrimary
-            className="self-start"
-          >
+          <ButtonPrimary className="self-start">
             {t("details.credentials.action_uitid")}
           </ButtonPrimary>
         </div>
-      ) : ( */}
-      <div className="flex flex-col flex-1 gap-4">
-        <div className="flex flex-col gap-3">
-          <Heading className="font-semibold flex min-w-[5rem]" level={4}>
-            {t("details.credentials.test")}
-          </Heading>
-          {auth0TestClientWithLabels.map((client) => (
-            <div
-              key={`${client.label}-${client.value}`}
-              className="flex gap-1 max-md:flex-col max-md:items-start"
-            >
-              <span className="flex items-center whitespace-nowrap">
-                {t(client.label)}
-              </span>
-              <CopyText>{client.value}</CopyText>
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col gap-2">
-          <Heading className="font-semibold min-w-[5rem]" level={4}>
-            {t("details.credentials.live")}
-          </Heading>
-          <StatusLight status={status} />
-          {status === IntegrationStatus.Active && (
-            <div className="flex flex-col gap-3">
-              {auth0ProdClientWithLabels.map((client) => (
-                <div
-                  key={`${client.label}-${client.value}`}
-                  className="flex gap-1 max-md:flex-col max-md:items-start"
-                >
-                  <span className="flex items-center whitespace-nowrap">
-                    {t(client.label)}
-                  </span>
-                  <CopyText>{client.value}</CopyText>
-                </div>
-              ))}
-            </div>
-          )}
-          <div className="flex flex-col gap-3 align-center">
-            {status === IntegrationStatus.Draft && (
-              <ActivationFlow
-                status={status}
-                id={id}
-                subscription={subscription}
-                type={type}
-                email={email}
-              />
+      ) : (
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="flex flex-col gap-3">
+            <Heading className="font-semibold flex min-w-[5rem]" level={4}>
+              {t("details.credentials.test")}
+            </Heading>
+            {auth0TestClientWithLabels.map((client) => (
+              <div
+                key={`${client.label}-${client.value}`}
+                className="flex gap-1 max-md:flex-col max-md:items-start"
+              >
+                <span className="flex items-center whitespace-nowrap">
+                  {t(client.label)}
+                </span>
+                <CopyText>{client.value}</CopyText>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Heading className="font-semibold min-w-[5rem]" level={4}>
+              {t("details.credentials.live")}
+            </Heading>
+            <StatusLight status={status} />
+            {status === IntegrationStatus.Active && (
+              <div className="flex flex-col gap-3">
+                {auth0ProdClientWithLabels.map((client) => (
+                  <div
+                    key={`${client.label}-${client.value}`}
+                    className="flex gap-1 max-md:flex-col max-md:items-start"
+                  >
+                    <span className="flex items-center whitespace-nowrap">
+                      {t(client.label)}
+                    </span>
+                    <CopyText>{client.value}</CopyText>
+                  </div>
+                ))}
+              </div>
             )}
+            <div className="flex flex-col gap-3 align-center">
+              {status === IntegrationStatus.Draft && (
+                <ActivationFlow
+                  status={status}
+                  id={id}
+                  subscription={subscription}
+                  type={type}
+                  email={email}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      {/* )} */}
+      )}
     </div>
   );
 };
