7b57759ef9bdb860534af06d7a68648c123d3a01
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-04-08 13:08:10 +0200

    PPF-366 - Only fill in TVA when IntegrationType is not "entry-api"

--- e2e/tests/integrations/integrator/request-activation.ts
+++ e2e/tests/integrations/integrator/request-activation.ts
@@ -1,9 +1,11 @@
 import { type Page, expect } from "@playwright/test";
 import { fakerNL_BE as faker } from "@faker-js/faker";
+import { IntegrationTypes, type IntegrationType } from "../../types.js";
 
 export async function requestActivationAsIntegrator(
   page: Page,
-  integrationId: string
+  integrationId: string,
+  integrationType: IntegrationType
 ) {
   await page.goto(`/nl/integraties/${integrationId}`);
   await page
@@ -20,14 +22,18 @@ export async function requestActivationAsIntegrator(
     .fill(faker.location.streetAddress());
   await page.getByLabel("Postcode").nth(1).fill(faker.location.zipCode("####"));
   await page.getByLabel("Gemeente").nth(3).fill(faker.location.city());
-  await page
-    .getByLabel("BTW of ondernemingsnummer")
-    .nth(1)
-    .fill("BE 0475 250 609");
-  await page
-    .getByLabel("E-mail boekhouding")
-    .nth(1)
-    .fill(faker.internet.email());
+
+  if (integrationType !== IntegrationTypes.ENTRY_API) {
+    await page
+      .getByLabel("BTW of ondernemingsnummer")
+      .nth(1)
+      .fill("BE 0475 250 609");
+    await page
+      .getByLabel("E-mail boekhouding")
+      .nth(1)
+      .fill(faker.internet.email());
+  }
+
   await page.getByRole("button", { name: "Bevestigen" }).nth(1).click();
   await expect(page.getByText("Actief").nth(1)).toBeVisible();
 }
