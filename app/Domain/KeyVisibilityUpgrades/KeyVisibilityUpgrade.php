eb9fbc40e45329bb4be442e03799c23e50904e4d
Luc Wollants <lucwollants@gmail.com>
2024-04-11 12:10:57 +0200

    Extend `KeyVisibilityUpgrade` with created_at

--- app/Domain/KeyVisibilityUpgrades/KeyVisibilityUpgrade.php
+++ app/Domain/KeyVisibilityUpgrades/KeyVisibilityUpgrade.php
@@ -5,14 +5,29 @@ declare(strict_types=1);
 namespace App\Domain\KeyVisibilityUpgrades;
 
 use App\Domain\Integrations\KeyVisibility;
+use DateTimeImmutable;
 use Ramsey\Uuid\UuidInterface;
 
 final readonly class KeyVisibilityUpgrade
 {
+    private DateTimeImmutable $createdAt;
+
     public function __construct(
         public UuidInterface $id,
         public UuidInterface $integrationId,
         public KeyVisibility $keyVisibility
     ) {
+    }
+
+    public function withCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $clone = clone $this;
+        $clone->createdAt = $createdAt;
+        return $clone;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
     }
 }
