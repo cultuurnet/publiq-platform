3dce0464b48264e9b3ba785f7176aa6dadf0e02a
Koen Eelen <koen.eelenpubliq.be>
2024-05-07 10:17:43 +0200

    Updated test

--- tests/Keycloak/TokenStrategy/ClientCredentialsTest.php
+++ tests/Keycloak/TokenStrategy/ClientCredentialsTest.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 namespace Tests\Keycloak\TokenStrategy;
 
-use App\Keycloak\Dto\Config;
-use App\Keycloak\Dto\Realm;
+use App\Keycloak\Config;
 use App\Keycloak\Exception\KeyCloakApiFailed;
+use App\Keycloak\Realm;
+use App\Keycloak\RealmCollection;
 use App\Keycloak\TokenStrategy\ClientCredentials;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 final class ClientCredentialsTest extends TestCase
 {
+    public const ACCESS_TOKEN = 'pqeaefosdfhbsdq';
     private Config $config;
+
+    private LoggerInterface&MockObject $logger;
 
     protected function setUp(): void
     {
@@ -25,68 +31,52 @@ final class ClientCredentialsTest extends TestCase
             'https://keycloak.example.com/',
             'php_client',
             'a_true_secret',
-            new Realm('uitidpoc', 'Acceptance')
+            new RealmCollection([new Realm('uitidpoc', 'Acceptance')])
         );
+        $this->logger = $this->createMock(LoggerInterface::class);
     }
 
-    public function testHappyPath(): void
+    public function test_it_returns_a_valid_token(): void
     {
         $mock = new MockHandler([
-            new Response(200, [], json_encode(['access_token' => 'pqeaefosdfhbsdq'], JSON_THROW_ON_ERROR)),
+            new Response(200, [], json_encode(['access_token' => self::ACCESS_TOKEN], JSON_THROW_ON_ERROR)),
         ]);
 
         $handlerStack = HandlerStack::create($mock);
-        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config);
+        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config, $this->logger);
         $token = $clientCredentials->fetchToken(Realm::getMasterRealm());
 
-        $this->assertEquals('pqeaefosdfhbsdq', $token);
+        $this->assertEquals(self::ACCESS_TOKEN, $token);
     }
 
-    public function testFailedBecauseIntegrationIsDisabled(): void
-    {
-        $config = new Config(
-            false,
-            'https://keycloak.example.com/',
-            'php_client',
-            'a_true_secret',
-            new Realm('uitidpoc', 'Acceptance')
-        );
-
-        $client = new ClientCredentials(new Client(), $config);
-        $this->expectException(KeyCloakApiFailed::class);
-        $this->expectExceptionCode(KeyCloakApiFailed::IS_DISABLED);
-
-        $client->fetchToken(Realm::getMasterRealm());
-    }
-
-    public function testFailedBecauseResponseDidNotContainToken(): void
+    public function test_failed_because_response_did_not_contain_token(): void
     {
         $mock = new MockHandler([
             new Response(200, [], json_encode([], JSON_THROW_ON_ERROR)),
         ]);
 
         $handlerStack = HandlerStack::create($mock);
-        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config);
+        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config, $this->logger);
 
         $this->expectException(KeyCloakApiFailed::class);
         $this->expectExceptionCode(KeyCloakApiFailed::UNEXPECTED_TOKEN_RESPONSE);
 
         $token = $clientCredentials->fetchToken(Realm::getMasterRealm());
 
-        $this->assertEquals('pqeaefosdfhbsdq', $token);
+        $this->assertEquals(self::ACCESS_TOKEN, $token);
     }
 
-    public function testFailedBecauseUnauthorized(): void
+    public function test_failed_because_unauthorized(): void
     {
         $mock = new MockHandler([
             new Response(401, [], json_encode([], JSON_THROW_ON_ERROR)),
         ]);
 
         $handlerStack = HandlerStack::create($mock);
-        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config);
+        $clientCredentials = new ClientCredentials(new Client(['handler' => $handlerStack]), $this->config, $this->logger);
 
         $this->expectException(KeyCloakApiFailed::class);
         $this->expectExceptionCode(KeyCloakApiFailed::COULD_NOT_FETCH_ACCESS_TOKEN);
-        $token = $clientCredentials->fetchToken(Realm::getMasterRealm());
+        $clientCredentials->fetchToken(Realm::getMasterRealm());
     }
 }
