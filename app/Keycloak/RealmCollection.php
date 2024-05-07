c08214adfd53ca31e86d1dd3c3438a8976724199
Koen Eelen <koen.eelenpubliq.be>
2024-05-07 10:15:14 +0200

    move and change base class RealmCollection

---
+++ app/Keycloak/RealmCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Keycloak\Collection;
+
+use App\Keycloak\Realm;
+use Illuminate\Support\Collection;
+
+/**
+ * @extends Collection<int, Realm>
+ */
+final class RealmCollection extends Collection
+{
+}
