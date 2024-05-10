277242caf08494c9aa2edf16a68cfb61f7ca4d76
Koen Eelen <koen.eelenpubliq.be>
2024-05-10 16:12:51 +0200

    Add elequant models

--- app/Keycloak/RealmCollection.php
+++ app/Keycloak/RealmCollection.php
@@ -5,15 +5,27 @@ declare(strict_types=1);
 namespace App\Keycloak;
 
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 
 /**
  * @extends Collection<int, Realm>
  */
 final class RealmCollection extends Collection
 {
-    public static function getDefaultRealms(): RealmCollection
+    public static function getRealms(): RealmCollection
     {
         //@todo Change this once all Realms have been configured
         return new self([new Realm('uitidpoc', 'Acceptance')]);
+    }
+
+    public static function fromInternalName(string $internalName) : Realm
+    {
+        foreach(self::getRealms() as $realm) {
+            if($realm->internalName === $internalName) {
+                return $realm;
+            }
+        }
+
+        throw new InvalidArgumentException('Invalid realm: '. $internalName);
     }
 }
