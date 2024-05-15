52dcf15319c2e45eb1278f1ce3aa868787c85b4a
Koen Eelen <koen.eelenpubliq.be>
2024-05-15 17:09:40 +0200

    Add listener when integration is created

--- app/Keycloak/KeycloakServiceProvider.php
+++ app/Keycloak/KeycloakServiceProvider.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace App\Keycloak;
 
+use App\Domain\Integrations\Events\IntegrationCreated;
+use App\Domain\Integrations\Events\IntegrationUpdated;
+use App\Domain\Integrations\Repositories\IntegrationRepository;
+use App\Keycloak\Listeners\CreateClients;
 use App\Keycloak\Client\KeycloakHttpClient;
+use App\Keycloak\Repositories\EloquentKeycloakClientRepository;
+use App\Keycloak\Repositories\KeycloakClientRepository;
 use App\Keycloak\Service\ApiClient;
+use App\Keycloak\Service\ApiClientInterface;
+use App\Keycloak\Service\CreateClientHandler;
 use App\Keycloak\TokenStrategy\ClientCredentials;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
@@ -17,7 +26,7 @@ final class KeycloakServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(ApiClient::class, function () {
+        $this->app->singleton(ApiClientInterface::class, function () {
             return new ApiClient(
                 new KeycloakHttpClient(
                     new Client([RequestOptions::HTTP_ERRORS => false]),
@@ -48,5 +57,20 @@ final class KeycloakServiceProvider extends ServiceProvider
                 Uuid::fromString(config('keycloak.scope.widgets_id')),
             );
         });
+
+        $this->app->singleton(KeycloakClientRepository::class, function () {
+            return $this->app->get(EloquentKeycloakClientRepository::class);
+        });
+
+        $this->applyEvents();
+    }
+
+    private function applyEvents(): void
+    {
+        if (!$this->app->get(Config::class)->isEnabled) {
+            return;
+        }
+
+        Event::listen(IntegrationCreated::class, [CreateClients::class, 'handle']);
     }
 }
