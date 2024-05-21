a74995da7cc8d0bf15eaf5e6de56e5d5bbf87641
Koen Eelen <koen.eelenpubliq.be>
2024-05-21 10:14:52 +0200

    pr feedback

--- config/keycloak.php
+++ config/keycloak.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'enabled' => env('KEYCLOAK_ENABLE', false),
+    'enabled' => env('KEYCLOAK_ENABLED', false),
     'base_url' => env('KEYCLOAK_BASE_URL', ''),
     'client_id' => env('KEYCLOAK_CLIENT_ID', ''),
     'client_secret' => env('KEYCLOAK_CLIENT_SECRET', ''),
