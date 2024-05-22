9d3012ec8a83962c0e050c024ebf050dfda7d60e
Koen Eelen <koen.eelenpubliq.be>
2024-05-22 14:30:37 +0200

    Always send the groups when updating uitidv1

--- tests/UiTiDv1/Jobs/ActivateConsumerHandlerTest.php
+++ tests/UiTiDv1/Jobs/ActivateConsumerHandlerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\UiTiDv1\Jobs;
 
+use App\Domain\Integrations\Repositories\IntegrationRepository;
 use App\UiTiDv1\Events\ConsumerActivated;
 use App\UiTiDv1\Jobs\ActivateConsumer;
 use App\UiTiDv1\Jobs\ActivateConsumerHandler;
@@ -16,6 +17,7 @@ use LogicException;
 use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Log\NullLogger;
 use Ramsey\Uuid\Uuid;
+use Tests\IntegrationHelper;
 use Tests\TestCase;
 use Tests\UiTiDv1\CreatesMockUiTiDv1ClusterSDK;
 use Tests\UiTiDv1\CreatesMockUiTiDv1Consumer;
@@ -24,11 +26,11 @@ final class ActivateConsumerHandlerTest extends TestCase
 {
     use CreatesMockUiTiDv1ClusterSDK;
     use CreatesMockUiTiDv1Consumer;
+    use IntegrationHelper;
 
     private ClientInterface&MockObject $httpClient;
-
     private UiTiDv1ConsumerRepository&MockObject $clientRepository;
-
+    private IntegrationRepository&MockObject $integrationRepository;
     private ActivateConsumerHandler $activateClient;
 
     protected function setUp(): void
@@ -38,10 +40,12 @@ final class ActivateConsumerHandlerTest extends TestCase
         $this->httpClient = $this->createMock(ClientInterface::class);
 
         $this->clientRepository = $this->createMock(UiTiDv1ConsumerRepository::class);
+        $this->integrationRepository = $this->createMock(IntegrationRepository::class);
 
         $this->activateClient = new ActivateConsumerHandler(
             $this->createMockUiTiDv1ClusterSDK($this->httpClient),
             $this->clientRepository,
+            $this->integrationRepository,
             new NullLogger()
         );
     }
@@ -49,10 +53,16 @@ final class ActivateConsumerHandlerTest extends TestCase
     public function test_does_it_sent_an_activate_request(): void
     {
         $id = Uuid::uuid4();
+        $uiTiDv1Consumer = $this->createConsumer($id);
         $this->clientRepository->expects($this->once())
             ->method('getById')
             ->with($id)
-            ->willReturn($this->createConsumer($id));
+            ->willReturn($uiTiDv1Consumer);
+
+        $this->integrationRepository->expects($this->once())
+            ->method('getById')
+            ->with($uiTiDv1Consumer->integrationId)
+            ->willReturn($this->givenThereIsAnIntegration($id));
 
         $this->httpClient->expects($this->once())
             ->method('request')
@@ -62,7 +72,7 @@ final class ActivateConsumerHandlerTest extends TestCase
                         [
                             'POST',
                             'serviceconsumer/consumer-key-1',
-                            'status=ACTIVE',
+                            'status=ACTIVE&group=3',
                         ]
                         => new Response(200, [], ''),
                         default => throw new LogicException('Invalid arguments received'),
@@ -73,7 +83,6 @@ final class ActivateConsumerHandlerTest extends TestCase
         $this->activateClient->handle(new ActivateConsumer($id));
 
         Event::assertDispatched(ConsumerActivated::class);
-
     }
 
     public function test_it_does_not_try_to_block_an_invalid_client(): void
@@ -96,10 +105,16 @@ final class ActivateConsumerHandlerTest extends TestCase
     public function test_it_stops_on_invalid_request(): void
     {
         $id = Uuid::uuid4();
+        $uiTiDv1Consumer = $this->createConsumer($id);
         $this->clientRepository->expects($this->once())
             ->method('getById')
             ->with($id)
-            ->willReturn($this->createConsumer($id));
+            ->willReturn($uiTiDv1Consumer);
+
+        $this->integrationRepository->expects($this->once())
+            ->method('getById')
+            ->with($uiTiDv1Consumer->integrationId)
+            ->willReturn($this->givenThereIsAnIntegration($id));
 
         $this->httpClient->expects($this->once())
             ->method('request')
