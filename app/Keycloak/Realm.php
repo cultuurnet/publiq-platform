98d2d54e990db29d23c769a937045d15986f5cad
Koen Eelen <koen.eelenpubliq.be>
2024-05-22 10:24:53 +0200

    Add enviroment to Realm link

--- app/Keycloak/Realm.php
+++ app/Keycloak/Realm.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Keycloak;
 
+use App\Domain\Integrations\Environment;
+
 final readonly class Realm
 {
-    public function __construct(public string $internalName, public string $publicName)
+    public function __construct(public string $internalName, public string $publicName, public Environment $environment = Environment::Unknown)
     {
     }
 
