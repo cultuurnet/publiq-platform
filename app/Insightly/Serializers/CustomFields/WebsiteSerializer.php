bda00cf1e8ccc93d18a1ba19b5f4fc39079723a6
Luc Wollants <lucwollants@gmail.com>
2024-05-22 11:34:07 +0200

    Handle empty website as `-` to clear it once set

--- app/Insightly/Serializers/CustomFields/WebsiteSerializer.php
+++ app/Insightly/Serializers/CustomFields/WebsiteSerializer.php
@@ -10,12 +10,12 @@ final class WebsiteSerializer
 {
     public const CUSTOM_FIELD_WEBSITE = 'URL_agenda__c';
 
-    public function toInsightlyArray(Website $website): array
+    public function toInsightlyArray(?Website $website): array
     {
         return [
             'FIELD_NAME' => self::CUSTOM_FIELD_WEBSITE,
             'CUSTOM_FIELD_ID' => self::CUSTOM_FIELD_WEBSITE,
-            'FIELD_VALUE' => $website->value,
+            'FIELD_VALUE' => $website ? $website->value : '-',
         ];
     }
 }
