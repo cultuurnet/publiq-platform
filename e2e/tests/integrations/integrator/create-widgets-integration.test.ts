1ded99bdb982cb1720b8b0cb4b296f89edd60902
vhande <hande.vanhove@publiq.be>
2024-05-23 11:09:18 +0200

    Add E2E for widget integration

---
+++ e2e/tests/integrations/integrator/create-widgets-integration.test.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import { createIntegrationAsIntegrator } from "./create-integration.js";
+import { IntegrationType } from "@app-types/IntegrationType";
+
+test.use({ storageState: "playwright/.auth/user.json" });
+
+test("As an integrator I can create a new integration", async ({
+  page,
+  context,
+}) => {
+  const { integrationName } = await createIntegrationAsIntegrator(
+    page,
+    IntegrationType.Widgets
+  );
+  await expect(page.getByText(integrationName)).toBeVisible();
+
+  // nieuwe
+  const pagePromise = context.waitForEvent("page");
+  await page
+    .getByText(/^TestWidgets bouwen$/)
+    .locator("button")
+    .click();
+  const newPage = await pagePromise;
+  await newPage.getByRole("button", { name: "Nieuwe widget maken" }).click();
+});
