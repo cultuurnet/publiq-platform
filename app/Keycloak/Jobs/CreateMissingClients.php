6ca35517a3f793175e3e2f2fbae064b2bdec10a6
Koen Eelen <koen.eelenpubliq.be>
2024-05-23 15:05:45 +0200

    add nova hook for missing keycloak clients

---
+++ app/Keycloak/Jobs/CreateMissingClients.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Keycloak\Jobs;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Ramsey\Uuid\UuidInterface;
+
+final class CreateMissingClients
+{
+    use Dispatchable;
+
+    public function __construct(public readonly UuidInterface $id)
+    {
+    }
+}
---
+++ app/Nova/Actions/Keycloak/CreateMissingKeycloakClients.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Actions\Keycloak;
+
+use App\Domain\Integrations\Models\IntegrationModel;
+use App\Keycloak\Jobs\CreateMissingClients;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Event;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Actions\ActionModelCollection;
+use Laravel\Nova\Fields\ActionFields;
+use Ramsey\Uuid\Uuid;
+
+final class CreateMissingKeycloakClients extends Action
+{
+    use InteractsWithQueue;
+    use Queueable;
+
+    public function __construct()
+    {
+    }
+
+    public function handle(ActionFields $fields, ActionModelCollection $actionModelCollection): void
+    {
+        foreach ($actionModelCollection as $integrationModel) {
+            if (!$integrationModel instanceof IntegrationModel) {
+                continue;
+            }
+
+            Event::dispatch(new CreateMissingClients(Uuid::fromString($integrationModel->id)));
+        }
+    }
+}
