5b769901f997b63b322ced068b9ebe61bce0fa31
Koen Eelen <koen.eelenpubliq.be>
2024-05-24 09:30:53 +0200

    add method to convert string to env enum value

--- app/Domain/Integrations/Environment.php
+++ app/Domain/Integrations/Environment.php
@@ -9,4 +9,14 @@ enum Environment: string
     case Acceptance = 'acc';
     case Testing = 'test';
     case Production = 'prod';
+
+    public static function fromString(string $string): self
+    {
+        return match ($string) {
+            'acceptance' => self::Acceptance,
+            'testing' => self::Testing,
+            'production' => self::Production,
+            default => throw new \InvalidArgumentException(sprintf('Invalid environment: %s', $string)),
+        };
+    }
 }
