3f15e61769f95ce27de336298e4e9305344303a6
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-05-28 11:22:41 +0200

    add migration

---
+++ database/migrations/2024_05_28_105201_integration_status_before_block.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('integration_status_before_block', function (Blueprint $table) {
+            $table->uuid('integration_id')->primary();
+            $table->string('status');
+        });    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('integration_status_before_block');
+    }
+};
