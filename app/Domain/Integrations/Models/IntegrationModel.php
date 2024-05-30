1943c54fb921135ba9cf88ea886672ec62f65c8d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-05-30 12:49:05 +0200

    add canBeUnblocked() & unblock() to IntegrationModel

--- app/Domain/Integrations/Models/IntegrationModel.php
+++ app/Domain/Integrations/Models/IntegrationModel.php
@@ -81,6 +81,11 @@ final class IntegrationModel extends UuidModel
         return $this->status !== IntegrationStatus::Blocked->value;
     }
 
+    public function canBeUnblocked(): bool
+    {
+        return $this->status === IntegrationStatus::Blocked->value;
+    }
+
     public function isWidgets(): bool
     {
         return $this->type === IntegrationType::Widgets->value;
@@ -150,6 +155,11 @@ final class IntegrationModel extends UuidModel
             'status' => IntegrationStatus::Blocked,
         ]);
         IntegrationBlocked::dispatch(Uuid::fromString($this->id));
+    }
+
+    public function unblock(): void
+    {
+
     }
 
     /**
