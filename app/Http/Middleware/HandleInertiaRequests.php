6487b56ced7924612c5609de952b858d9dffc4b3
Emma Fabre <ehtnam6@gmail.com>
2024-06-06 13:56:48 +0200

    Pass runtime env variables through Inertia shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -25,6 +25,18 @@ final class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'config' => [
+                'VITE_PUSHER_APP_KEY' => env('VITE_PUSHER_APP_KEY'),
+                'VITE_PUSHER_HOST' => env('VITE_PUSHER_HOST'),
+                'VITE_PUSHER_PORT' => env('VITE_PUSHER_PORT'),
+                'VITE_PUSHER_SCHEME' => env('VITE_PUSHER_SCHEME'),
+                'VITE_PUSHER_APP_CLUSTER' => env('VITE_PUSHER_APP_CLUSTER'),
+                'VITE_APP_ENV' => env('VITE_APP_ENV'),
+                'VITE_SENTRY_DSN' => env('VITE_SENTRY_DSN'),
+                'VITE_SENTRY_ENABLED' => env('VITE_SENTRY_ENABLED'),
+                'VITE_UITID_WIDGET_URL' => env('VITE_UITID_WIDGET_URL'),
+                'VITE_UITPAS_INTEGRATION_TYPE_ENABLED' => env('VITE_UITPAS_INTEGRATION_TYPE_ENABLED'),
+            ],
             'widgetConfig' => [
                 'profileUrl' => config('uitidwidget.profileUrl'),
                 'registerUrl' => config('uitidwidget.registerUrl'),
