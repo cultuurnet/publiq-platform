5c533eb1128c2a1ef89d160248bd2c46445f444f
Emma Fabre <ehtnam6@gmail.com>
2024-06-11 15:16:52 +0200

    Add new Search Servie method to find organizers

--- app/Search/Sapi3/Sapi3SearchService.php
+++ app/Search/Sapi3/Sapi3SearchService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Search\Sapi3;
 
+use CultuurNet\SearchV3\Parameter\Id;
 use CultuurNet\SearchV3\Parameter\Label;
 use CultuurNet\SearchV3\SearchClientInterface;
 use CultuurNet\SearchV3\SearchQuery;
@@ -21,6 +22,17 @@ final readonly class Sapi3SearchService implements SearchService
         $searchQuery->addParameter(new Label('UiTPAS'));
         $searchQuery->addParameter(new Name($name));
         $searchQuery->setEmbed(true);
+
+        return $this->searchClient->searchOrganizers($searchQuery);
+    }
+
+    public function findUiTPASOrganizers(string ...$ids): PagedCollection
+    {
+        $searchQuery = new SearchQuery();
+        $searchQuery->setEmbed(true);
+        foreach ($ids as $id) {
+            $searchQuery->addParameter(new Id($id));
+        }
 
         return $this->searchClient->searchOrganizers($searchQuery);
     }
