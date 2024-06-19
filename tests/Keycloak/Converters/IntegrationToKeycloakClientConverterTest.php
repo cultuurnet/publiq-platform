5117b1941cf598fd8b2169c2c3f2d08394acc2e2
Koen Eelen <koen.eelenpubliq.be>
2024-06-19 11:35:34 +0200

    changed payload create client

--- tests/Keycloak/Converters/IntegrationToKeycloakClientConverterTest.php
+++ tests/Keycloak/Converters/IntegrationToKeycloakClientConverterTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Keycloak\Converters;
 
+use App\Domain\Integrations\Environment;
 use App\Domain\Integrations\IntegrationPartnerStatus;
+use App\Domain\Integrations\IntegrationUrl;
+use App\Domain\Integrations\IntegrationUrlType;
 use App\Keycloak\Converters\IntegrationToKeycloakClientConverter;
-use Tests\TestCase;
 use Ramsey\Uuid\Uuid;
 use Tests\CreatesIntegration;
+use Tests\TestCase;
 
 final class IntegrationToKeycloakClientConverterTest extends TestCase
 {
@@ -24,7 +27,7 @@ final class IntegrationToKeycloakClientConverterTest extends TestCase
 
         $integration = $this->givenThereIsAnIntegration($id, ['partnerStatus' => $partnerStatus]);
 
-        $convertedData = IntegrationToKeycloakClientConverter::convert($id, $integration, $clientId);
+        $convertedData = IntegrationToKeycloakClientConverter::convert($id, $integration, $clientId, Environment::Acceptance);
 
         $this->assertIsArray($convertedData);
         $this->assertEquals('openid-connect', $convertedData['protocol']);
@@ -40,6 +43,18 @@ final class IntegrationToKeycloakClientConverterTest extends TestCase
         $this->assertFalse($convertedData['directAccessGrantsEnabled']);
         $this->assertFalse($convertedData['alwaysDisplayInConsole']);
         $this->assertTrue($convertedData['frontchannelLogout']);
+        $this->assertEquals([
+            'origin' => 'publiq-platform',
+            'use.refresh.tokens' => true,
+            'post.logout.redirect.uris' => '',
+        ], $convertedData['attributes']);
+        $this->assertEquals('', $convertedData['baseUrl']);
+        $this->assertEquals([], $convertedData['redirectUris']);
+        $this->assertEquals([
+            '+',
+            'https://docs.publiq.be',
+            'https://publiq.stoplight.io',
+        ], $convertedData['webOrigins']);
     }
 
     public static function integrationDataProvider(): array
@@ -48,5 +63,29 @@ final class IntegrationToKeycloakClientConverterTest extends TestCase
             [IntegrationPartnerStatus::THIRD_PARTY, true, false],
             [IntegrationPartnerStatus::FIRST_PARTY, false, true],
         ];
+    }
+
+    public function test_combining_keycloak_convert_with_configured_uris(): void
+    {
+        $id = Uuid::uuid4();
+        $clientId = Uuid::uuid4()->toString();
+
+        $integration = $this->givenThereIsAnIntegration($id, ['partnerStatus' => IntegrationPartnerStatus::FIRST_PARTY]);
+        $integration = $integration->withUrls(
+            new IntegrationUrl(Uuid::uuid4(), $integration->id, Environment::Acceptance, IntegrationUrlType::Logout, 'https://example.com/logout1'),
+            new IntegrationUrl(Uuid::uuid4(), $integration->id, Environment::Acceptance, IntegrationUrlType::Logout, 'https://example.com/logout2'),
+            new IntegrationUrl(Uuid::uuid4(), $integration->id, Environment::Acceptance, IntegrationUrlType::Callback, 'https://example.com/callback1'),
+            new IntegrationUrl(Uuid::uuid4(), $integration->id, Environment::Acceptance, IntegrationUrlType::Callback, 'https://example.com/callback2'),
+            new IntegrationUrl(Uuid::uuid4(), $integration->id, Environment::Acceptance, IntegrationUrlType::Login, 'https://example.com/login1'),
+        );
+
+        $convertedData = IntegrationToKeycloakClientConverter::convert($id, $integration, $clientId, Environment::Acceptance);
+        $this->assertEquals([
+            'origin' => 'publiq-platform',
+            'use.refresh.tokens' => true,
+            'post.logout.redirect.uris' => 'https://example.com/logout1#https://example.com/logout2',
+        ], $convertedData['attributes']);
+        $this->assertEquals('https://example.com/login1', $convertedData['baseUrl']);
+        $this->assertEquals(['https://example.com/callback1', 'https://example.com/callback2'], $convertedData['redirectUris']);
     }
 }
