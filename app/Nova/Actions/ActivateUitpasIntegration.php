c1afca96425182b396731d2b10221f74c2a6004b
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-07-04 09:42:05 +0200

    create Action for UiTPAS integration Activation

---
+++ app/Nova/Actions/ActivateUitpasIntegration.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Actions;
+
+use App\Domain\Integrations\Models\IntegrationModel;
+use App\Domain\Integrations\Organizer;
+use App\Domain\Integrations\Repositories\IntegrationRepository;
+use App\Domain\Integrations\Repositories\OrganizerRepository;
+use App\Domain\Organizations\Models\OrganizationModel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Actions\ActionResponse;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Ramsey\Uuid\Uuid;
+
+final class ActivateUitpasIntegration extends Action
+{
+    use InteractsWithQueue;
+    use Queueable;
+
+    public $name = 'Activate Integration';
+
+    public function __construct(
+        private readonly IntegrationRepository $integrationRepository,
+        private readonly OrganizerRepository $organizerRepository
+    ) {
+    }
+
+    public function handle(ActionFields $fields, Collection $integrations): ActionResponse
+    {
+        /** @var IntegrationModel $integration */
+        $integration = $integrations->first();
+
+        /** @var string $organizationIdAsString */
+        $organizationIdAsString = $fields->get('organization');
+        $organizationId = Uuid::fromString($organizationIdAsString);
+
+        /** @var string $organizersAsString */
+        $organizersAsString = $fields->get('organizers');
+        $organizerId = Uuid::fromString($organizersAsString);
+
+        $this->integrationRepository->activateWithOrganization(
+            Uuid::fromString($integration->id),
+            $organizationId,
+            null
+        );
+
+        $this->organizerRepository->create(
+            new Organizer(
+                Uuid::uuid4(),
+                Uuid::fromString($integration->id),
+                $organizerId
+            )
+        );
+
+        return Action::message('Integration "' . $integration->name . '" activated.');
+    }
+
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            Select::make('Organization', 'organization')
+                ->options(
+                    OrganizationModel::query()->pluck('name', 'id')
+                )
+                ->rules(
+                    'required',
+                    'exists:organizations,id'
+                ),
+            Text::make('UiTdatabank Organizers', 'organizers')
+                ->rules(
+                    'nullable',
+                    'string',
+                ),
+        ];
+    }
+}
