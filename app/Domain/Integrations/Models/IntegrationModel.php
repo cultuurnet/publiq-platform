5eb938a0c7728c52a2c2d1355648481ae07df57f
Luc Wollants <lucwollants@gmail.com>
2024-07-16 14:32:41 +0200

    Revert `isUiTPAS` call to check type only

--- app/Domain/Integrations/Models/IntegrationModel.php
+++ app/Domain/Integrations/Models/IntegrationModel.php
@@ -97,7 +97,7 @@ final class IntegrationModel extends UuidModel
 
     public function isUiTPAS(): bool
     {
-        return $this->type === IntegrationType::UiTPAS->value || $this->type === null;
+        return $this->type === IntegrationType::UiTPAS->value;
     }
 
     protected static function booted(): void
