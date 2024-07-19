17d5ddf1e7ba8663a95d7c57a585563cbdb7b302
Emma Fabre <ehtnam6@gmail.com>
2024-07-19 14:42:56 +0200

    Only display URL settings for First party partners

--- resources/ts/Components/Integrations/Detail/IntegrationSettings.tsx
+++ resources/ts/Components/Integrations/Detail/IntegrationSettings.tsx
@@ -106,9 +106,10 @@ export const IntegrationSettings = ({
   isKeepChangesDialogVisible,
   onConfirmLeaveTab,
   onCancelLeaveTab,
+  partnerStatus,
+  ...rest
 }: Props) => {
   const { t } = useTranslation();
-
   const [status, setStatus] = useState<"idle" | "success" | "error">("idle");
 
   const basicInfoForm = useBasicInfoForm({
@@ -230,6 +231,11 @@ export const IntegrationSettings = ({
     }
   };
 
+  const hasUrlSettings =
+    type === IntegrationType.EntryApi &&
+    keyVisibility !== KeyVisibility.v1 &&
+    partnerStatus === "First party";
+
   return (
     <>
       {status !== "idle" && (
@@ -254,8 +260,7 @@ export const IntegrationSettings = ({
         errors={basicInfoForm.errors}
       />
 
-      {type === IntegrationType.EntryApi &&
-        keyVisibility !== KeyVisibility.v1 &&
+      {hasUrlSettings &&
         Object.values(IntegrationUrlType).map((type) => (
           <UrlList
             key={type}
