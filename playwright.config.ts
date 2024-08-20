b1995bd001efb3e1cede61d0c1bacf366ae91710
simon-debruijn <simon.debruijn@hotmail.com>
2024-08-20 16:20:19 +0200

    Decrease workers due too high load

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: process.env.CI ? 1 : 2, // acceptance doesn't play well with more than 2 workers
   expect: {
     timeout: 5000,
   },
