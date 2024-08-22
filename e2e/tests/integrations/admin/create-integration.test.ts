9fd4d6cb0dccc01069953893567c36a76ba8c696
simon-debruijn <simon.debruijn@hotmail.com>
2024-08-22 11:23:27 +0200

    Refactor and speedup create-integration

--- e2e/tests/integrations/admin/create-integration.test.ts
+++ e2e/tests/integrations/admin/create-integration.test.ts
@@ -9,53 +9,47 @@ async function addContactToIntegration(
   integrationId: string,
   page: Page
 ) {
-  const contributrorEmail = faker.internet.email();
-  await page.goto("/admin");
-  await page.getByRole("link", { name: "Contacts", exact: true }).click();
-  await page.getByRole("link", { name: "Create Contact" }).click();
-  await page.getByPlaceholder("Email").fill(contributrorEmail);
+  await page.goto("/admin/resources/contacts/new");
+  const email = faker.internet.email();
+  await page.getByPlaceholder("Email").fill(email);
   await page.locator("#type").selectOption(type);
   await page.getByPlaceholder("First Name").fill(faker.person.firstName());
   await page.getByPlaceholder("Last Name").fill(faker.person.lastName());
   await page
     .locator("[dusk='integrations-select']")
-    .selectOption(integrationId!);
+    .selectOption(integrationId);
   await page.getByRole("button", { name: "Create Contact" }).click();
+  await expect(page.getByText("Contact was created!")).toBeVisible({
+    timeout: 10_000,
+  });
 
-  await page.waitForURL(/\/admin\/resources\/contacts\/(?!new).+$/);
-  await expect(
-    page.getByText(`Contact Details: ${contributrorEmail}`)
-  ).toBeVisible({ timeout: 10_000 });
-  await expect(page.getByText(type)).toBeVisible();
-  return contributrorEmail;
+  return { email };
 }
 
 test.use({ storageState: "playwright/.auth/admin.json" });
 
 test("create a new integration as an admin (with functional, technical and contributor contact)", async ({
   page,
+  context,
 }) => {
   const { id } = await createIntegration(page, IntegrationType.SearchApi);
 
-  const contributorEmail = await addContactToIntegration(
-    ContactType.Contributor,
-    id,
-    page
-  );
-  const functionalEmail = await addContactToIntegration(
-    ContactType.Functional,
-    id,
-    page
-  );
-  const technicalEmail = await addContactToIntegration(
-    ContactType.Technical,
-    id,
-    page
+  const [contributor, functional, technical] = await Promise.all(
+    [
+      ContactType.Contributor,
+      ContactType.Functional,
+      ContactType.Technical,
+    ].map(async (contactType) => {
+      const newPage = await context.newPage();
+      const contact = await addContactToIntegration(contactType, id, newPage);
+      await newPage.close();
+      return contact;
+    })
   );
 
   // Go to overview page and see if the contacts are visible
   await page.goto(`/admin/resources/integrations/${id}`);
-  await expect(page.getByText(functionalEmail)).toBeVisible();
-  await expect(page.getByText(contributorEmail)).toBeVisible();
-  await expect(page.getByText(technicalEmail)).toBeVisible();
+  await expect(page.getByText(functional.email)).toBeVisible();
+  await expect(page.getByText(contributor.email)).toBeVisible();
+  await expect(page.getByText(technical.email)).toBeVisible();
 });
