ecfa0f0106771de55833cc652cc62912bfb458df
simon-debruijn <simon.debruijn@hotmail.com>
2024-09-05 13:55:15 +0200

    Fix import

--- resources/ts/Components/Navigation.tsx
+++ resources/ts/Components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react";
+import type { ComponentProps } from "react";
 import React from "react";
 import { Heading } from "./Heading";
 import { useTranslation } from "react-i18next";
