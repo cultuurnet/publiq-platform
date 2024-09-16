8789dc025090595c7a5a408b5a8c232b1331af34
Koen Eelen <koen.eelenpubliq.be>
2024-09-16 16:42:00 +0200

    add command to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,6 +11,7 @@ final class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('integration:send-activation-reminder-email --force')->dailyAt('00:00');
     }
 
     protected function commands(): void
