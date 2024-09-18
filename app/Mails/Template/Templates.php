1bc572f100f76c6c23780acc4b61892e4f2fe135
Koen Eelen <koen.eelenpubliq.be>
2024-09-18 15:00:07 +0200

    remove useless constants

--- app/Mails/Template/Templates.php
+++ app/Mails/Template/Templates.php
@@ -12,11 +12,6 @@ use InvalidArgumentException;
  */
 final class Templates extends Collection
 {
-    public const INTEGRATION_CREATED = 'integration_created';
-    public const INTEGRATION_ACTIVATION_REMINDER = 'integration_activation_reminder';
-    public const INTEGRATION_BLOCKED = 'integration_blocked';
-    public const INTEGRATION_ACTIVATED = 'integration_activated';
-
     public static function build(array $mails): self
     {
         $collection = new self();
