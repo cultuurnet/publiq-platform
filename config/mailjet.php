d1d711b7d2be2f9a2a653e99587a4b3653eafed4
Koen Eelen <koen.eelenpubliq.be>
2024-09-18 14:35:41 +0200

    Reformat test and add extra test case

--- config/mailjet.php
+++ config/mailjet.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Mails\MailJet\MailjetConfig;
 use App\Mails\Template\TemplateName;
 
 return [
--- tests/Mails/MailJet/SandboxModeTest.php
+++ tests/Mails/MailJet/SandboxModeTest.php
@@ -6,9 +6,9 @@ namespace Tests\Mails\MailJet;
 
 use App\Domain\Mail\Addresses;
 use App\Mails\MailJet\SandboxMode;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Mime\Address;
-use PHPUnit\Framework\Attributes\DataProvider;
 
 final class SandboxModeTest extends TestCase
 {
@@ -28,12 +28,48 @@ final class SandboxModeTest extends TestCase
         $allowedDomains = ['publiq.be', 'smurfen@gmail.com'];
 
         return [
-            'Sandbox mode is off, should return false' => [false, $allowedDomains, ['test@gmail.com'], false],
-            'Sandbox mode is on, but domain is allowed, should return false' => [true, $allowedDomains, ['test@publiq.be'], false],
-            'Sandbox mode is on, and domain is not allowed, should return true' => [true, $allowedDomains, ['test@verkeerd.com'], true],
-            'Sandbox mode is on, but one of the addresses is from the allowed domain' => [true, $allowedDomains, ['test@publiq.be', 'test@verkeerd.com'], false],
-            'Sandbox mode is on, all addresses are not from allowed domains' => [true, $allowedDomains, ['test@verkeerd.com', 'admin@verkeerd.com'], true],
-            'Sandbox mode is on, entire email matches, should return false' => [true, $allowedDomains, ['smurfen@gmail.com'], false],
+            'Sandbox mode is off and domain is not allowed, should return false' => [
+                false,
+                $allowedDomains,
+                ['test@gmail.com'],
+                false,
+            ],
+            'Sandbox mode is off and domain is allowed, should return false' => [
+                false,
+                $allowedDomains,
+                ['test@publiq.be'],
+                false,
+            ],
+            'Sandbox mode is on, but domain is allowed, should return false' => [
+                true,
+                $allowedDomains,
+                ['test@publiq.be'],
+                false,
+            ],
+            'Sandbox mode is on, and domain is not allowed, should return true' => [
+                true,
+                $allowedDomains,
+                ['test@verkeerd.com'],
+                true,
+            ],
+            'Sandbox mode is on, but one of the addresses is from the allowed domain' => [
+                true,
+                $allowedDomains,
+                ['test@publiq.be', 'test@verkeerd.com'],
+                false,
+            ],
+            'Sandbox mode is on, all addresses are not from allowed domains' => [
+                true,
+                $allowedDomains,
+                ['test@verkeerd.com', 'admin@verkeerd.com'],
+                true,
+            ],
+            'Sandbox mode is on, entire email matches, should return false' => [
+                true,
+                $allowedDomains,
+                ['smurfen@gmail.com'],
+                false,
+            ],
         ];
     }
 }
