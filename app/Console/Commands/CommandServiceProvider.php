ecb9a694942a8378f1d4a593fc2238869060f652
Koen Eelen <koen.eelenpubliq.be>
2024-09-19 11:01:59 +0200

    add service provider for cli command

---
+++ app/Console/Commands/CommandServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Domain\Integrations\Repositories\IntegrationRepository;
+use App\Mails\MailJet\MailjetConfig;
+use Illuminate\Support\ServiceProvider;
+use Psr\Log\LoggerInterface;
+
+final class CommandServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->singleton(SearchExpiredIntegrations::class, function () {
+            return new SearchExpiredIntegrations(
+                $this->app->get(IntegrationRepository::class),
+                $this->app->get(LoggerInterface::class),
+                config(MailjetConfig::MAILJET_EXPIRATION_TIMERS),
+            );
+        });
+    }
+}
