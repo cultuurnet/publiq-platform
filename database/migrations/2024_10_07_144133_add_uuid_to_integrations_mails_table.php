cf101e575bf7317d4170dd709abeaf1b7711a432
Koen Eelen <koen.eelenpubliq.be>
2024-10-07 15:07:15 +0200

    added migration

---
+++ database/migrations/2024_10_07_144133_add_uuid_to_integrations_mails_table.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('integrations_mails', function (Blueprint $table) {
+            // Add the UUID field (but don't set it as primary key yet)
+            $table->uuid('uuid')->nullable()->first();
+        });
+
+        // Autofill UUIDs for existing records to prevent null errors later
+        DB::table('integrations_mails')->update(['uuid' => Uuid::uuid4()->toString()]);
+
+        // Finish the migration, drop existing primary key and make the UUID field non-nullable and set it as the primary key
+        Schema::table('integrations_mails', function (Blueprint $table) {
+            $table->dropPrimary(['integration_id', 'template_name']);
+            $table->uuid('uuid')->primary()->change();
+
+            $table->index(['integration_id', 'template_name'], 'integration_id_template_name_index');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('integrations_mails', function (Blueprint $table) {
+            $table->dropPrimary(['uuid']);
+
+            $table->dropIndex('integration_id_template_name_index');
+
+            $table->primary(['integration_id', 'template_name']);
+
+            $table->dropColumn('uuid');
+        });
+    }
+};
