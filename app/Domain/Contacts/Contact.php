ecd8cbc0a69f393f936ab096bdf4ae6506e10455
Koen Eelen <koen.eelenpubliq.be>
2024-10-09 10:47:35 +0200

    Revert "Add new Email DTO for Contact Resource"
    This reverts commit 18a38aac9283ce9c95892c24d4be806a97674d6b.

--- app/Domain/Contacts/Contact.php
+++ app/Domain/Contacts/Contact.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Domain\Contacts;
 
-use App\Domain\Integrations\Email;
 use Ramsey\Uuid\UuidInterface;
 
 final class Contact
@@ -12,7 +11,7 @@ final class Contact
     public function __construct(
         public readonly UuidInterface $id,
         public readonly UuidInterface $integrationId,
-        public readonly Email $email,
+        public readonly string $email,
         public readonly ContactType $type,
         public readonly string $firstName,
         public readonly string $lastName
