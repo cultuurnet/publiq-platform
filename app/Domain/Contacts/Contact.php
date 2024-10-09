18a38aac9283ce9c95892c24d4be806a97674d6b
Koen Eelen <koen.eelenpubliq.be>
2024-10-09 09:43:29 +0200

    Add new Email DTO for Contact Resource

--- app/Domain/Contacts/Contact.php
+++ app/Domain/Contacts/Contact.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Contacts;
 
+use App\Domain\Integrations\Email;
 use Ramsey\Uuid\UuidInterface;
 
 final class Contact
@@ -11,7 +12,7 @@ final class Contact
     public function __construct(
         public readonly UuidInterface $id,
         public readonly UuidInterface $integrationId,
-        public readonly string $email,
+        public readonly Email $email,
         public readonly ContactType $type,
         public readonly string $firstName,
         public readonly string $lastName
