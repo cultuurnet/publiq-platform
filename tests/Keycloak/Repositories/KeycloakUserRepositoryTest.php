b693b49e4a62a833e68969bc3e13887b15f9dd03
Luc Wollants <lucwollants@gmail.com>
2024-10-11 14:52:36 +0200

    Implement a concrete `KeycloakUserRepository`

---
+++ tests/Keycloak/Repositories/KeycloakUserRepositoryTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Keycloak\Repositories;
+
+use App\Domain\Integrations\Environment;
+use App\Json;
+use App\Keycloak\Client\KeycloakHttpClient;
+use App\Keycloak\Realm;
+use App\Keycloak\Repositories\KeycloakUserRepository;
+use App\Keycloak\ScopeConfig;
+use App\Keycloak\TokenStrategy\TokenStrategy;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
+
+final class KeycloakUserRepositoryTest extends TestCase
+{
+    private KeycloakUserRepository $keycloakUserRepository;
+
+    private ClientInterface&MockObject $client;
+
+    private TokenStrategy&MockObject $tokenStrategy;
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createMock(ClientInterface::class);
+
+        $this->tokenStrategy = $this->createMock(TokenStrategy::class);
+
+        $keycloakHttpClient = new KeycloakHttpClient(
+            $this->client,
+            $this->tokenStrategy,
+            $this->createMock(LoggerInterface::class),
+        );
+
+        $realm = new Realm(
+            'myAcceptanceRealm',
+            'Acc',
+            'https://keycloak.com/api',
+            'php_client',
+            'dfgopopzjcvijogdrg',
+            Environment::Acceptance,
+            new ScopeConfig(Uuid::uuid4(), Uuid::uuid4(), Uuid::uuid4()),
+        );
+
+        $this->keycloakUserRepository = new KeycloakUserRepository($keycloakHttpClient, $realm);
+    }
+
+    public function test_it_searches_users_by_email(): void
+    {
+        $this->client->expects($this->once())
+            ->method('send')
+            ->with($this->callback(function (RequestInterface $request): bool {
+                $this->assertEquals('GET', $request->getMethod());
+                $this->assertEquals('/api/admin/realms/myAcceptanceRealm/users', $request->getUri()->getPath());
+                $this->assertEquals('email=john%40publiq.be&exact=true', $request->getUri()->getQuery());
+                $this->assertEquals('Bearer token', $request->getHeaderLine('Authorization'));
+                return true;
+            }))
+            ->willReturn(
+                new Response(
+                    200,
+                    [],
+                    Json::encode(
+                        [
+                            [
+                                'id' => 'google-oauth2|105581372645959335478',
+                                'username' => 'john@publiq.be',
+                                'firstName' => 'John',
+                                'email' => 'john@publiq.be',
+                            ],
+                        ]
+                    )
+                )
+            );
+
+        $this->tokenStrategy->expects($this->once())
+            ->method('fetchToken')
+            ->willReturn('token');
+
+        $userId = $this->keycloakUserRepository->findUserIdByEmail('john@publiq.be');
+
+        $this->assertEquals('google-oauth2|105581372645959335478', $userId);
+    }
+}
