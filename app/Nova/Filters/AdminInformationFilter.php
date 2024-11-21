ed6f8ebe93de6390b27a524c2c780df82dd7d2ad
Luc Wollants <lucwollants@gmail.com>
2024-11-21 19:05:37 +0100

    Use Select filter instead of Boolean filter

--- app/Nova/Filters/AdminInformationFilter.php
+++ app/Nova/Filters/AdminInformationFilter.php
@@ -5,28 +5,35 @@ declare(strict_types=1);
 namespace App\Nova\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
-use Laravel\Nova\Filters\BooleanFilter;
+use Laravel\Nova\Filters\Filter;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-final class AdminInformationFilter extends BooleanFilter
+final class AdminInformationFilter extends Filter
 {
     public $name = 'On hold';
 
     public function apply(NovaRequest $request, $query, $value): Builder
     {
-        if (empty($value['on_hold'])) {
-            return $query;
+        if ($value === 'on_hold') {
+            return $query->whereHas('adminInformation', function ($q) {
+                $q->where('on_hold', '=', 1);
+            });
         }
 
-        return $query->whereHas('adminInformation', function ($q) {
-            $q->where('on_hold', '=', 1);
-        });
+        if ($value === 'not_on_hold') {
+            return $query->whereDoesntHave('adminInformation', function ($q) {
+                $q->where('on_hold', '=', 1);
+            });
+        }
+
+        return $query;
     }
 
     public function options(NovaRequest $request): array
     {
         return [
             'On Hold' => 'on_hold',
+            'Not On Hold' => 'not_on_hold',
         ];
     }
 }
