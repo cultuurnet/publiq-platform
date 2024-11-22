ff7b3f888e40a6c22a6cec8f3b4d536dcb0f342e
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-11-22 14:29:12 +0100

    try fix frontend

--- resources/ts/Components/Integrations/Detail/Credentials.tsx
+++ resources/ts/Components/Integrations/Detail/Credentials.tsx
@@ -4,7 +4,6 @@ import { CredentialsAuthClients } from "./CredentialsAuthClients";
 import { IntegrationType } from "../../../types/IntegrationType";
 import { CredentialsWidgets } from "./CredentialsWidgets";
 import type {
-  AuthClient,
   KeycloakClient,
   LegacyAuthConsumer,
 } from "../../../types/Credentials";
@@ -14,7 +13,6 @@ import { useTranslation } from "react-i18next";
 import { usePolling } from "../../../hooks/usePolling";
 import { KeyVisibility } from "../../../types/KeyVisibility";
 import { UiTiDv1Environment } from "../../../types/UiTiDv1Environment";
-import { Auth0Tenant } from "../../../types/Auth0Tenant";
 import { KeycloakEnvironment } from "../../../types/KeycloakEnvironment";
 import { usePageProps } from "../../../hooks/usePageProps";
 
@@ -24,8 +22,6 @@ type Props = Integration & {
 };
 
 export type Credentials = {
-  testClient?: AuthClient;
-  prodClient?: AuthClient;
   legacyTestConsumer?: LegacyAuthConsumer;
   legacyProdConsumer?: LegacyAuthConsumer;
   keycloakTestClient?: KeycloakClient;
@@ -33,18 +29,17 @@ export type Credentials = {
 };
 
 export const Credentials = ({
-  id,
-  status,
-  email,
-  subscription,
-  type,
-  keyVisibility,
-  keyVisibilityUpgrade,
-  legacyAuthConsumers,
-  authClients,
-  keycloakClients,
-  oldCredentialsExpirationDate,
-}: Props) => {
+                              id,
+                              status,
+                              email,
+                              subscription,
+                              type,
+                              keyVisibility,
+                              keyVisibilityUpgrade,
+                              legacyAuthConsumers,
+                              keycloakClients,
+                              oldCredentialsExpirationDate,
+                            }: Props) => {
   const { t } = useTranslation();
   const { config } = usePageProps();
   const hasCredentials = useMemo(() => {
@@ -68,75 +63,69 @@ export const Credentials = ({
   ]);
 
   const isV1Upgraded =
-    keyVisibility === KeyVisibility.v1 && !!keyVisibilityUpgrade;
+      keyVisibility === KeyVisibility.v1 && !!keyVisibilityUpgrade;
   usePolling(!hasCredentials || isV1Upgraded, { only: ["integration"] });
   const credentials = useMemo(
-    () => ({
-      legacyTestConsumer: legacyAuthConsumers.find(
-        (consumer) => consumer.environment === UiTiDv1Environment.Testing
-      ),
-      legacyProdConsumer: legacyAuthConsumers.find(
-        (consumer) => consumer.environment === UiTiDv1Environment.Production
-      ),
-      testClient: authClients.find(
-        (client) => client.tenant === Auth0Tenant.Testing
-      ),
-      prodClient: authClients.find(
-        (client) => client.tenant === Auth0Tenant.Production
-      ),
-      keycloakTestClient: keycloakClients.find(
-        (client) => client.environment === KeycloakEnvironment.Testing
-      ),
-      keycloakProdClient: keycloakClients.find(
-        (client) => client.environment === KeycloakEnvironment.Production
-      ),
-    }),
-    [legacyAuthConsumers, authClients, keycloakClients]
+      () => ({
+        legacyTestConsumer: legacyAuthConsumers.find(
+            (consumer) => consumer.environment === UiTiDv1Environment.Testing
+        ),
+        legacyProdConsumer: legacyAuthConsumers.find(
+            (consumer) => consumer.environment === UiTiDv1Environment.Production
+        ),
+        keycloakTestClient: keycloakClients.find(
+            (client) => client.environment === KeycloakEnvironment.Testing
+        ),
+        keycloakProdClient: keycloakClients.find(
+            (client) => client.environment === KeycloakEnvironment.Production
+        ),
+      }),
+      [legacyAuthConsumers, keycloakClients]
   );
 
   if (!hasCredentials) {
     return (
-      <Alert variant={"info"}>{t("integrations.pending_credentials")}</Alert>
+        <Alert variant={"info"}>{t("integrations.pending_credentials")}</Alert>
     );
   }
 
   if (type === IntegrationType.Widgets) {
     return (
-      <CredentialsWidgets
-        {...credentials}
-        email={email}
-        status={status}
-        id={id}
-        type={type}
-        subscription={subscription}
-      />
+        <CredentialsWidgets
+            {...credentials}
+            email={email}
+            status={status}
+            id={id}
+            type={type}
+            subscription={subscription}
+        />
     );
   }
 
   return (
-    <>
-      {keyVisibility !== KeyVisibility.v2 && (
-        <CredentialsLegacyAuthConsumers
-          {...credentials}
-          email={email}
-          status={status}
-          id={id}
-          type={type}
-          subscription={subscription}
-          keyVisibility={keyVisibility}
-          oldCredentialsExpirationDate={oldCredentialsExpirationDate}
+      <>
+        {keyVisibility !== KeyVisibility.v2 && (
+            <CredentialsLegacyAuthConsumers
+                {...credentials}
+                email={email}
+                status={status}
+                id={id}
+                type={type}
+                subscription={subscription}
+                keyVisibility={keyVisibility}
+                oldCredentialsExpirationDate={oldCredentialsExpirationDate}
+            />
+        )}
+        <CredentialsAuthClients
+            {...credentials}
+            email={email}
+            status={status}
+            id={id}
+            type={type}
+            subscription={subscription}
+            keyVisibility={keyVisibility}
+            keyVisibilityUpgrade={keyVisibilityUpgrade}
         />
-      )}
-      <CredentialsAuthClients
-        {...credentials}
-        email={email}
-        status={status}
-        id={id}
-        type={type}
-        subscription={subscription}
-        keyVisibility={keyVisibility}
-        keyVisibilityUpgrade={keyVisibilityUpgrade}
-      />
-    </>
+      </>
   );
 };
