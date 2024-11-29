d08b8f349098694519630ec28506050dda8c9b2f
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-11-29 13:07:19 +0100

    use agnostic oAuthDomain instead auth0Domain in widgets

--- config/uitidwidget.php
+++ config/uitidwidget.php
@@ -6,5 +6,5 @@ return [
     'url' => env('UITID_WIDGET_URL'),
     'profileUrl' => env('UITID_WIDGET_PROFILE_URL'),
     'registerUrl' => env('UITID_WIDGET_REGISTER_URL'),
-    'auth0Domain' => env('UITID_WIDGET_AUTH0_DOMAIN'),
+    'oAuthDomain' => env('UITID_WIDGET_OAUTH_DOMAIN'),
 ];
