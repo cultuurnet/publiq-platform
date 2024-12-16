cb55ca46e1767b6260b6fc9939f147bd8f943cf8
Luc Wollants <lucwollants@gmail.com>
2024-12-16 12:12:53 +0100

    Use `Illuminate\Contracts\Database\Eloquent\Builder`

--- app/Nova/Resources/Integration.php
+++ app/Nova/Resources/Integration.php
@@ -23,7 +23,7 @@ use App\Nova\Actions\UiTiDv1\CreateMissingUiTiDv1Consumers;
 use App\Nova\Actions\UnblockIntegration;
 use App\Nova\Filters\AdminInformationFilter;
 use App\Nova\Resource;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Laravel\Nova\Fields\BelongsTo;
