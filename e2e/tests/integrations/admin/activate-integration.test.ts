96503b9fdb8a2c945e500ae65216128f64aa8962
Luc Wollants <lucwollants@gmail.com>
2024-12-19 14:58:34 +0100

    Fix Laravel Nova 5 random dropdown issue

--- e2e/tests/integrations/admin/activate-integration.test.ts
+++ e2e/tests/integrations/admin/activate-integration.test.ts
@@ -19,7 +19,7 @@ test("As an admin I can activate an integration", async ({ page }) => {
 
   // activate integration
   await page.goto(`/admin/resources/integrations/${integrationId}`);
-  await page.locator("#nova-ui-dropdown-button-5").click();
+  await page.locator(`[dusk="${integrationId}-control-selector"]`).click();
   await page.getByRole("button", { name: "Activate Integration" }).click();
   await page.locator("#organization").selectOption(organizationId);
   await page.locator("[dusk='confirm-action-button']").click();
