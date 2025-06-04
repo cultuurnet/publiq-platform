24a023df8dd0d9097c4dcdf79f8f7205743978b2
Koen Eelen <koen.eelenpubliq.be>
2025-06-04 14:32:21 +0200

    New flavor of http client, the uitpas client, because endpoint and token fetching are different

--- app/Keycloak/Client/UitpasHttpClient.php
+++ app/Keycloak/Client/UitpasHttpClient.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Keycloak\Client;
 
+use App\Domain\Integrations\Environment;
 use App\Keycloak\Realm;
 use App\Keycloak\TokenStrategy\TokenStrategy;
 use GuzzleHttp\ClientInterface;
@@ -11,15 +12,18 @@ use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Uri;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 
-final readonly class KeycloakHttpClient implements HttpClient
+/*
+ * Difference between this class and KeycloakHttpClient is that this fetches a token from the uitid realm, not the master realm
+ * It also calculates the base url differently
+ * */
+final readonly class UitpasHttpClient implements HttpClient
 {
     public function __construct(
         private ClientInterface $client,
         private TokenStrategy $tokenStrategy,
-        private LoggerInterface $logger = new NullLogger()
+        private string $testApiEndpoint,
+        private string $prodApiEndpoint,
     ) {
     }
 
@@ -34,18 +38,25 @@ final readonly class KeycloakHttpClient implements HttpClient
         return $this->client->send($request);
     }
 
-    /**
-     * @throws GuzzleException
-     */
+    /** @throws GuzzleException */
     public function sendWithBearer(RequestInterface $request, Realm $realm): ResponseInterface
     {
         $request = $request
-            ->withUri(new Uri($realm->baseUrl . $request->getUri()))
+            ->withUri(new Uri($this->getEndpoint($realm) . $request->getUri()))
             ->withAddedHeader(
                 'Authorization',
-                'Bearer ' . $this->tokenStrategy->fetchToken($this, $realm->getMasterRealm())
+                'Bearer ' . $this->tokenStrategy->fetchToken($this, $realm)
             );
 
         return $this->client->send($request);
+    }
+
+    private function getEndpoint(Realm $keycloakClient): string
+    {
+        if ($keycloakClient->environment === Environment::Testing) {
+            return $this->testApiEndpoint;
+        }
+
+        return $this->prodApiEndpoint;
     }
 }
