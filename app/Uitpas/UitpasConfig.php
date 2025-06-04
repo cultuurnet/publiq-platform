b9772fd3194f50a194d52611fbf58eb9fbac4003
Koen Eelen <koen.eelenpubliq.be>
2025-06-04 14:26:58 +0200

    add uitpas config

---
+++ app/Uitpas/UitpasConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Uitpas;
+
+enum UitpasConfig: string
+{
+    case AUTOMATIC_PERMISSIONS_ENABLED = 'uitpas.automatic_permissions_enabled';
+    case TEST_ORGANISATION = 'uitpas.test.organisation';
+    case TEST_API_ENDPOINT = 'uitpas.test.api_endpoint';
+    case PROD_API_ENDPOINT = 'uitpas.prod.api_endpoint';
+}
