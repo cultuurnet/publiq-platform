11a9c0ac4fd7831bde56c7260fcd1407d0a6c63d
Koen Eelen <koen.eelenpubliq.be>
2025-06-18 15:50:35 +0200

    added tests

---
+++ tests/Nova/Actions/RejectUdbOrganizerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Nova\Actions;
+
+use App\Domain\Integrations\Models\UdbOrganizerModel;
+use App\Domain\Integrations\Repositories\UdbOrganizerRepository;
+use App\Domain\Integrations\UdbOrganizer;
+use App\Domain\Integrations\UdbOrganizerStatus;
+use App\Nova\Actions\RejectUdbOrganizer;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Fields\ActionFields;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Tests\GivenUitpasOrganizers;
+use Tests\TestCase;
+
+final class RejectUdbOrganizerTest extends TestCase
+{
+    use GivenUitpasOrganizers;
+
+    public function test_it_handles_reject_udbOrganizer(): void
+    {
+        $udbOrganizerRepository = $this->createMock(UdbOrganizerRepository::class);
+
+        $uuid = Uuid::uuid4();
+        $integrationUuid = Uuid::uuid4();
+
+        $uuid2 = Uuid::uuid4();
+        $integrationUuid2 = Uuid::uuid4();
+
+        $expected = [
+            new UdbOrganizer($uuid, $integrationUuid, 'org-1'),
+            new UdbOrganizer($uuid2, $integrationUuid2, 'org-2'),
+        ];
+
+        $callIndex = 0;
+
+        $udbOrganizerRepository->expects($this->exactly(2))
+            ->method('delete')
+            ->with($this->callback(function ($actual) use (&$callIndex, $expected) {
+                TestCase::assertEquals($expected[$callIndex++], $actual);
+                return true;
+            }));
+
+        $a = new RejectUdbOrganizer($udbOrganizerRepository);
+        $udbOrganizers = new Collection();
+        $udbOrganizers->push($this->givenUdbOrganizerModel($uuid, $integrationUuid, 'org-1'));
+        $udbOrganizers->push($this->givenUdbOrganizerModel($uuid2, $integrationUuid2, 'org-2'));
+
+        $a->handle(
+            new ActionFields(collect(), collect()),
+            $udbOrganizers
+        );
+    }
+
+    public function givenUdbOrganizerModel(UuidInterface $uuid, UuidInterface $integrationUuid, string $orgId): UdbOrganizerModel
+    {
+        $model = new UdbOrganizerModel();
+        $model->id = $uuid->toString();
+        $model->integration_id = $integrationUuid->toString();
+        $model->organizer_id = $orgId;
+        $model->status = UdbOrganizerStatus::Pending->value;
+        return $model;
+    }
+}
---
+++ tests/UiTPAS/Jobs/ActivateUiTPASClientHandlerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\UiTPAS\Jobs;
+
+use App\Api\ClientCredentialsContext;
+use App\Domain\Integrations\Environment;
+use App\Domain\Integrations\Repositories\IntegrationRepository;
+use App\Domain\Integrations\Repositories\UdbOrganizerRepository;
+use App\Domain\Integrations\UdbOrganizer;
+use App\Domain\Integrations\UdbOrganizerStatus;
+use App\Keycloak\Client;
+use App\UiTPAS\Jobs\ActivateUiTPASClient;
+use App\UiTPAS\Jobs\ActivateUiTPASClientHandler;
+use App\UiTPAS\UiTPASApiInterface;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Tests\CreatesIntegration;
+
+final class ActivateUiTPASClientHandlerTest extends TestCase
+{
+    use CreatesIntegration;
+
+    public function test_it_handles_activate_uitpas_client(): void
+    {
+        $udbOrganizerRepository = $this->createMock(UdbOrganizerRepository::class);
+        $integrationRepository = $this->createMock(IntegrationRepository::class);
+        $api = $this->createMock(UiTPASApiInterface::class);
+        $context = new ClientCredentialsContext(
+            Environment::Testing,
+            'https://test.publiq.be/',
+            '123',
+            'secret',
+            'uitid'
+        );
+        $handler = new ActivateUiTPASClientHandler(
+            $udbOrganizerRepository,
+            $integrationRepository,
+            $api,
+            $context
+        );
+
+        $id = Uuid::uuid4();
+        $integrationId = Uuid::uuid4();
+        $organizerId = 'organizer-123';
+        $clientId = 'keycloak-client-id';
+
+        $udbOrganizer = new UdbOrganizer($id, $integrationId, $organizerId);
+
+        $integration = $this->givenThereIsAnIntegration($integrationId)
+            ->withKeycloakClients(
+                new Client(Uuid::uuid4(), Uuid::uuid4(), $clientId, 'client-id-1', Environment::Testing),
+            );
+
+        $udbOrganizerRepository->expects($this->once())
+            ->method('getById')
+            ->with($id)
+            ->willReturn($udbOrganizer);
+
+        $integrationRepository->expects($this->once())
+            ->method('getById')
+            ->with($integrationId)
+            ->willReturn($integration);
+
+        $api->expects($this->once())
+            ->method('addPermissions')
+            ->with(
+                $context,
+                $organizerId,
+                $clientId
+            );
+
+        $udbOrganizerRepository->expects($this->once())
+            ->method('save')
+            ->with($this->callback(function ($actual) {
+                // check that status is updated to Approved
+                return $actual instanceof UdbOrganizer &&
+                    $actual->status === UdbOrganizerStatus::Approved;
+            }));
+
+        $handler->handle(new ActivateUiTPASClient($id));
+    }
+}
