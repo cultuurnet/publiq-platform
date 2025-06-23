56a127cd3652d334ba2a775e5d04b7ba77ee3da7
Koen Eelen <koen.eelenpubliq.be>
2025-06-23 13:39:26 +0200

    modify uitpas interface

--- app/UiTPAS/UiTPASApiInterface.php
+++ app/UiTPAS/UiTPASApiInterface.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\UiTPAS;
 
 use App\Api\ClientCredentialsContext;
-use App\UiTPAS\Dto\UiTPASPermissions;
+use App\UiTPAS\Dto\UiTPASPermission;
 
 interface UiTPASApiInterface
 {
     public function addPermissions(ClientCredentialsContext $context, string $organizerId, string $clientId): bool;
 
-    public function fetchPermissions(ClientCredentialsContext $context, string $organisationId, string $clientId): UiTPASPermissions;
+    public function fetchPermissions(ClientCredentialsContext $context, string $organisationId, string $clientId): ?UiTPASPermission;
 }
