ded67693294428426ac0808b7f72fb9f99504588
Koen Eelen <koen.eelenpubliq.be>
2025-06-23 17:35:34 +0200

    fix wrong config values

--- config/uitpas.php
+++ config/uitpas.php
@@ -9,13 +9,13 @@ return [
     'test' => [
         'organisation' => env('UITPAS_TEST_ORG', '0ce87cbc-9299-4528-8d35-92225dc9489f'),
         'api_endpoint' => env('UITPAS_TEST_ENDPOINT', ''),
-        'oath_token_url' => env('UITPAS_TEST_OAUTH_TOKEN_URL', ''),
+        'oauth_token_url' => env('UITPAS_TEST_OAUTH_TOKEN_URL', ''),
         'client_id' => env('UITPAS_TEST_CLIENT_ID', ''),
         'client_secret' => env('UITPAS_TEST_CLIENT_SECRET', ''),
     ],
     'prod' => [
         'api_endpoint' => env('UITPAS_PROD_ENDPOINT', ''),
-        'oath_token_url' => env('UITPAS_PROD_OATH_TOKEN_URL', ''),
+        'oauth_token_url' => env('UITPAS_PROD_OAUTH_TOKEN_URL', ''),
         'client_id' => env('UITPAS_PROD_CLIENT_ID', ''),
         'client_secret' => env('UITPAS_PROD_CLIENT_SECRET', ''),
     ],
