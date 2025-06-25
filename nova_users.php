3b05607727cbb4755beecb745b05ed63c2f67720
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-06-25 14:27:53 +0200

    set nova_users.php in gitignore

--- nova_users.php
+++ nova_users.php
@@ -11,11 +11,9 @@ return [
     'jonas@cultuurnet.be',
     'karen.schilders@cultuurnet.be',
     'koen.eelen@publiq.be',
-    'simon.debruijn@publiq.be',
     'ben.gabriels@publiq.be',
     // PLATFORM & SERVICES
     'kristof@publiq.be',
-    'paul@herbosch.be',
     // E2E
     'dev+e2etest-admin@publiq.be',
     // madewithlove
