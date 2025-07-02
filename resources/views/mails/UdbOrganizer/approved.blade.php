4650d766cf8ce136a3e396776ab9e20cc53d40b1
Koen Eelen <koen.eelenpubliq.be>
2025-07-02 14:50:27 +0200

    add mail templates

---
+++ resources/views/mails/UdbOrganizer/approved.blade.php
@@ -0,0 +1,19 @@
+@include('mails.partials.header')
+
+<p>Je integratie met naam {{ $integrationName }} is geactiveerd voor de organisator <strong>{{ $organizerName }}</strong>.</p>
+
+<p>Je toegangssleutels voor de liveomgeving vind je op je integratiepagina:
+    <a href="{{ $integrationDetailpage }}">{{ $integrationDetailpage }}</a>.</p>
+
+<p>We horen ook graag van je hoe voor jou de integratie met onze API’s verliep. Wil je daarom onderstaande vragenlijst
+    invullen?</p>
+
+<p><a href="https://tally.so/r/wg5adm" target="_blank">Naar de vragenlijst</a></p>
+
+<p>Het invullen duurt slechts 5 minuten en helpt ons enorm onze producten en services verder te optimaliseren.</p>
+
+<p>Bedankt!</p>
+
+<p>Het publiq platform team.</p>
+
+@include('mails.partials.footer')
---
+++ resources/views/mails/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<p style="border-top:solid 1px #9ec6db;font-size:1px;margin:10px auto;width:100%;">&nbsp;</p>
+
+<table width="100%">
+    <tr>
+        <td>
+            <p>Publiq Platform<br />
+                <a href="mailto:technical-support@publiq.be" target="_blank">technical-support@publiq.be</a></p>
+        </td>
+        <td style="text-align: right;">
+            <img alt="Publiq VZW" src="https://assets.uit.be/uitdatabank/entry_api/logo.png" style="float: right;" />
+        </td>
+    </tr>
+</table>
+
+<p style="text-align:center;margin:10px 0;margin-top:10px;margin-bottom:10px"><span
+        style="font-family:Verdana;font-size:16px;line-height:22px">publiq vzw - Henegouwenkaai 41-43 1080 Molenbeek BE</span>
+</p>
+</div>
