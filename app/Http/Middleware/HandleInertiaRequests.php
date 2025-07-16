5c7f3cf59daa6b7e13e7e14fdcfde3b379c463ec
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-07-16 14:08:32 +0200

    use correct nesting

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,9 +35,9 @@ final class HandleInertiaRequests extends Middleware
                     'dsn' => config('sentry.dsn'),
                     'enabled' => config('app.sentry.enabled'),
                 ],
-            ],
-            'coupons' => [
-                'enabled' => config('coupons.enabled'),
+                'coupons' => [
+                    'enabled' => config('coupons.enabled'),
+                ],
             ],
             'widgetConfig' => [
                 'url' => config('uitidwidget.url'),
