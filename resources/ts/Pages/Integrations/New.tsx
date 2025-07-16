3b47e9d045523325b276d06fa6173035629a275f
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-07-16 10:41:01 +0200

    undo frontend changes

--- resources/ts/Pages/Integrations/New.tsx
+++ resources/ts/Pages/Integrations/New.tsx
@@ -83,7 +83,10 @@ const New = ({ subscriptions }: Props) => {
     });
   }
 
-  const isCouponFieldVisible = false;
+  const isCouponFieldVisible =
+      (activeType === IntegrationType.SearchApi ||
+          activeType === IntegrationType.Widgets) &&
+      data.subscriptionId === basicPricingPlan?.id;
 
   const isPricingInfoVisible = (
     [IntegrationType.SearchApi, IntegrationType.Widgets] as IntegrationType[]
