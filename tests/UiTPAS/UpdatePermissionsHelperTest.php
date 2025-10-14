887ef9459c819d97fc05f2fa24100a5ccfeaa672
Koen Eelen <koen.eelenpubliq.be>
2025-10-14 16:13:28 +0200

    cleanup test

--- tests/UiTPAS/UpdatePermissionsHelperTest.php
+++ tests/UiTPAS/UpdatePermissionsHelperTest.php
@@ -12,64 +12,55 @@ use Tests\TestCase;
 final class UpdatePermissionsHelperTest extends TestCase
 {
     private UpdatePermissionsHelper $helper;
+    private UdbUuid $organizerId;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->helper = new UpdatePermissionsHelper();
+        $this->organizerId = new UdbUuid(Uuid::uuid4()->toString());
     }
 
     public function test_merge_adds_new_permission_set_when_organizer_not_present(): void
     {
-        $organizerId = new UdbUuid(Uuid::uuid4()->toString());
-        $emptyPermissions = [];
-
-        $result = $this->helper->merge($emptyPermissions, $organizerId);
+        $result = $this->helper->merge([], $this->organizerId);
 
         $this->assertCount(1, $result);
-        $this->assertEquals($organizerId->toString(), $result[0]['organizer']['id']);
+        $this->assertEquals($this->organizerId->toString(), $result[0]['organizer']['id']);
         $this->assertGreaterThan(0, count($result[0]['permissionDetails']));
     }
 
     public function test_merge_adds_only_missing_permissions_when_organizer_exists(): void
     {
-        $organizerId = new UdbUuid(Uuid::uuid4()->toString());
-        $existingPermissions = [
+        $result = $this->helper->merge([
             [
-                'organizer' => ['id' => $organizerId->toString()],
+                'organizer' => ['id' => $this->organizerId->toString()],
                 'permissionDetails' => [
                     ['id' => 'CHECKINS_WRITE'],
                     ['id' => 'EVENTS_READ'],
                 ],
             ],
-        ];
-        $expectedPermissionCount = count(UpdatePermissionsHelper::PERMISSION_LIST);
-
-        $result = $this->helper->merge($existingPermissions, $organizerId);
+        ], $this->organizerId);
 
         $resultPermissionIds = array_column($result[0]['permissionDetails'], 'id');
         $this->assertContains('CHECKINS_WRITE', $resultPermissionIds);
         $this->assertContains('EVENTS_READ', $resultPermissionIds);
         $this->assertContains('REWARDS_PASSHOLDERS_READ', $resultPermissionIds);
-        $this->assertCount($expectedPermissionCount, $resultPermissionIds);
+        $this->assertCount(count(UpdatePermissionsHelper::PERMISSION_LIST), $resultPermissionIds);
     }
 
     public function test_merge_does_not_duplicate_permissions(): void
     {
-        $organizerId = new UdbUuid(Uuid::uuid4()->toString());
         $allPermissionDetails = array_map(fn ($id) => ['id' => $id], UpdatePermissionsHelper::PERMISSION_LIST);
-        $permissionsWithAllDetails = [
+        $result = $this->helper->merge([
             [
-                'organizer' => ['id' => $organizerId->toString()],
+                'organizer' => ['id' => $this->organizerId->toString()],
                 'permissionDetails' => $allPermissionDetails,
             ],
-        ];
-        $expectedPermissionCount = 15;
-
-        $result = $this->helper->merge($permissionsWithAllDetails, $organizerId);
+        ], $this->organizerId);
 
         $resultPermissionIds = array_column($result[0]['permissionDetails'], 'id');
-        $this->assertCount($expectedPermissionCount, $resultPermissionIds);
+        $this->assertCount(count(UpdatePermissionsHelper::PERMISSION_LIST), $resultPermissionIds);
         $this->assertEqualsCanonicalizing(
             UpdatePermissionsHelper::PERMISSION_LIST,
             $resultPermissionIds
