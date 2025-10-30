7114f15d1672c1bd6c82a2e42bdc7fc0e5cdff42
Luc Wollants <lucwollants@gmail.com>
2025-10-30 14:54:10 +0100

    Move initialisation to init-database.sh

--- docker/mysql/init-database.sh
+++ docker/mysql/init-database.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS platform;
     CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
 EOSQL
