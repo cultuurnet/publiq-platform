97e2afba3e4f6e109cb2b80e5ce5d29eb3cb6d51
Luc Wollants <lucwollants@gmail.com>
2025-11-04 17:20:30 +0100

    Run only activate

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         storageState: "playwright/.auth/user.json",
       },
       dependencies: ["setup"],
-      testMatch: "tests/integrations/admin/*.test.ts",
+      testMatch: "tests/integrations/admin/activate-integration.test.ts",
     },
   ],
 });
