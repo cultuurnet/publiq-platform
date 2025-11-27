abdd0642f8f37fb6392616519e3ace0068849256
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-11-27 15:35:51 +0100

    Add var showContentCheck to template

--- resources/views/mails/integration/requested.blade.php
+++ resources/views/mails/integration/requested.blade.php
@@ -3,10 +3,12 @@
 <p>We hebben je aanvraag tot activatie voor de integratie met naam <strong>{{ $integrationName }}</strong> goed
     ontvangen.</p>
 
+@if($showContentCheck ?? true)
 <p>Om de integratie te activeren stuur je via de integratie 5 testevenementen zoals beschreven in de documentatie.
     Gelieve deze evenementen, met hun respectievelijke identificatienummers, te sturen naar
     <a href="mailto:technical-support@publiq.be">technical-support@publiq.be</a> met als onderwerp <em>"content
         check"</em>.</p>
+@endif
 
 <p>Ons team zal je aanvraag zo spoedig mogelijk verwerken en de testevenementen beoordelen.
     Bij activatie ontvang je een bevestigingsmail met je toegang tot de live omgeving.</p>
